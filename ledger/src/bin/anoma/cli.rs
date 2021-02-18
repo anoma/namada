78fcdea548dbbc8f3c48ec2dbb46ba96de02a903
Tomáš Zemanovič <tzemanovic@gmail.com>
2021-02-18 13:05:42 +0100

    [ledger] pass env. variables to sub-commands

--- ledger/src/bin/anoma/cli.rs
+++ ledger/src/bin/anoma/cli.rs
@@ -9,7 +9,8 @@ use clap::{Clap, IntoApp};
 
 pub fn main() {
     let args = env::args();
-    // TODO use https://github.com/rust-lang/cargo/blob/ab64d1393b5b77c66b6534ef5023a1b89ee7bf64/src/cargo/util/process_builder.rs to correctly set env, etc.
+    let env_vars = env::vars_os();
+    // TODO use https://github.com/rust-lang/cargo/blob/ab64d1393b5b77c66b6534ef5023a1b89ee7bf64/src/cargo/util/process_builder.rs to correctly set working dir, etc.
     match AnomaOpts::try_parse() {
         Err(_err) => {
             AnomaOpts::into_app().print_help().unwrap();
@@ -17,19 +18,37 @@ pub fn main() {
         Ok(opts) => match opts {
             AnomaOpts::Node(_) => {
                 let args = args.skip(2);
-                Command::new("anomad").args(args).status().unwrap();
+                // TODO when `anoma` is ran with `cargo run`, the sub-commands
+                // should also run from cargo
+                Command::new("anomad")
+                    .args(args)
+                    .envs(env_vars)
+                    .status()
+                    .unwrap();
             }
             AnomaOpts::InlinedNode(_) => {
                 let args = args.skip(1);
-                Command::new("anomad").args(args).status().unwrap();
+                Command::new("anomad")
+                    .args(args)
+                    .envs(env_vars)
+                    .status()
+                    .unwrap();
             }
             AnomaOpts::Client(_) => {
                 let args = args.skip(2);
-                Command::new("anomac").args(args).status().unwrap();
+                Command::new("anomac")
+                    .args(args)
+                    .envs(env_vars)
+                    .status()
+                    .unwrap();
             }
             AnomaOpts::InlinedClient(_) => {
                 let args = args.skip(1);
-                Command::new("anomac").args(args).status().unwrap();
+                Command::new("anomac")
+                    .args(args)
+                    .envs(env_vars)
+                    .status()
+                    .unwrap();
             }
         },
     }
