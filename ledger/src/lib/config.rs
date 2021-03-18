f20171dbfd22632fca2dfcd1f4f828ec58577783
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2021-03-18 13:46:16 +0100

    [feat]: p2p config refactor

--- ledger/src/lib/config.rs
+++ ledger/src/lib/config.rs
@@ -23,7 +23,9 @@ pub struct Tendermint {
 #[derive(Debug, Deserialize)]
 pub struct Gossip {
     pub host: String,
-    pub port: String
+    pub port: String,
+    pub peers: Vec<String>,
+    pub topics: Vec<String>
 }
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -47,6 +49,8 @@ impl Config {
         
         s.set_default("p2p.host", "127.0.0.1")?;
         s.set_default("p2p.port", 20201)?;
+        s.set_default("p2p.peers", Vec::<String>::new())?;
+        s.set_default("p2p.topics", Vec::<String>::new())?;
 
         s.merge(config::File::with_name(&format!("{}/{}", home, "settings.toml")).required(false))?;
 
@@ -54,14 +58,14 @@ impl Config {
     }
 
     pub fn tendermint_home_dir(&self) -> PathBuf {
-        self.node.home.join(self.node.tendermint_path.clone().into_os_string().into_string().unwrap())
+        self.node.home.join(self.node.tendermint_path.clone())
     }
 
     pub fn gossip_home_dir(&self) -> PathBuf {
-        self.node.home.join(self.node.libp2p_path.clone().into_os_string().into_string().unwrap())
+        self.node.home.join(self.node.libp2p_path.clone())
     }
 
     pub fn db_home_dir(&self) -> PathBuf {
-        self.node.home.join(self.node.db_path.clone().into_os_string().into_string().unwrap())
+        self.node.home.join(self.node.db_path.clone())
     }
 }
