787944554c8b972eff93eb123db869d7cfcf00c7
Sylvain Ribstein <sylvain.ribstein@gmail.com>
2021-04-15 13:50:26 +0200

    [config] if config does not exists or failed to gen use default conf

--- ledger/src/bin/anoma-node/cli.rs
+++ ledger/src/bin/anoma-node/cli.rs
@@ -1,7 +1,7 @@
 //! The docstrings on types and their fields with `derive(Clap)` are displayed
 //! in the CLI `--help`.
-use anoma::cli;
 use anoma::config::Config;
+use anoma::{cli, config};
 use eyre::{Context, Result};
 
 use crate::{gossip, shell};
@@ -15,27 +15,57 @@ pub fn main() -> Result<()> {
     // because it has a default
     let home = cli::parse_string_req(&matches, cli::BASE_ARG);
 
+    // for dev purpose this is useful
+    let read_gen_cfg = |home: String| -> Config {
+        match Config::read(&home) {
+            Ok(config) => config,
+            Err(err) => {
+                log::debug!(
+                    "Tried to read config in {} but failed with: {}",
+                    home,
+                    err
+                );
+                match config::Config::generate(&home) {
+                    Ok(config) => {
+                        log::debug!("Generated default config in {}", home,);
+                        config
+                    }
+                    Err(err) => {
+                        log::debug!(
+                            "Tried to generate config in {} but failed with: \
+                             {}",
+                            home,
+                            err
+                        );
+                        config::Config::default()
+                    }
+                }
+            }
+        }
+    };
     match matches.subcommand() {
         Some((cli::RUN_GOSSIP_COMMAND, args)) => {
-            let config = Config::read(home).expect("error config");
+            let config = read_gen_cfg(home);
             let mut gossip_cfg = config.gossip.unwrap_or_default();
             cli::update_gossip_config(args, &mut gossip_cfg)
                 .expect("failed to update config with cli option");
             gossip::run(gossip_cfg).wrap_err("Failed to run gossip service")
         }
         Some((cli::RUN_LEDGER_COMMAND, _)) => {
-            let config = Config::read(home).expect("error config");
+            let config = read_gen_cfg(home);
             let ledger_cfg = config.ledger.unwrap_or_default();
             shell::run(ledger_cfg).wrap_err("Failed to run Anoma node")
         }
         Some((cli::RESET_LEDGER_COMMAND, _)) => {
-            let config = Config::read(home).expect("error config");
+            let config = read_gen_cfg(home);
             let ledger_cfg = config.ledger.unwrap_or_default();
             shell::reset(ledger_cfg).wrap_err("Failed to reset Anoma node")
         }
         Some((cli::GENERATE_CONFIG_COMMAND, _args)) => {
-            anoma::config::Config::generate(home)
-                .wrap_err("failed to generate default config")
+            let gen_config = config::Config::generate(&home)
+                .wrap_err("failed to generate default config")?;
+            log::debug!("generated config {:?}", gen_config);
+            Ok(())
         }
         _ => app.print_help().wrap_err("Can't display help."),
     }
