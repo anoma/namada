bb8c005869b55faf99dd64e8eaa922a0e135ca5d
Tomáš Zemanovič <tzemanovic@gmail.com>
2021-05-12 17:27:54 +0200

    fix logging initialization

---
+++ apps/src/lib/logging.rs
@@ -0,0 +1,29 @@
+//! A module for anything related to logging
+use std::env;
+
+use color_eyre::eyre::Result;
+use eyre::WrapErr;
+use tracing_subscriber::filter::{Directive, EnvFilter};
+use tracing_subscriber::fmt::Subscriber;
+
+pub const ENV_KEY: &str = "ANOMA_LOG";
+
+pub fn init_from_env_or(default: impl Into<Directive>) -> Result<()> {
+    let filter = filter_from_env_or(default);
+    let my_collector = Subscriber::builder().with_env_filter(filter).finish();
+    tracing::subscriber::set_global_default(my_collector)
+        .wrap_err("Failed to set log subscriber")
+}
+
+pub fn filter_from_env_or(default: impl Into<Directive>) -> EnvFilter {
+    match env::var(ENV_KEY) {
+        Ok(log_env_var) => EnvFilter::new(log_env_var),
+        Err(_) => EnvFilter::default().add_directive(default.into()),
+    }
+}
+
+pub fn set_subscriber(filter: EnvFilter) -> Result<()> {
+    let my_collector = Subscriber::builder().with_env_filter(filter).finish();
+    tracing::subscriber::set_global_default(my_collector)
+        .wrap_err("Failed to set log subscriber")
+}
