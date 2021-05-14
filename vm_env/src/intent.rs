71ffb7e90313cebebc345630c01bf451c8461ecf
Tomáš Zemanovič <tzemanovic@gmail.com>
2021-05-14 13:17:39 +0200

    [vm_env] use `HashSet` for invalid intents

--- vm_env/src/intent.rs
+++ vm_env/src/intent.rs
@@ -1,26 +1,26 @@
+use std::collections::HashSet;
+
 use anoma_shared::types::intent;
 use anoma_shared::types::intent::Intent;
 use anoma_shared::types::key::ed25519::{Signature, Signed};
 
-// TODO It would be nicer to use a Set<Sig> but Signature does not imply Hash.
-// TODO if we don't use a set, only push if sig not already part ?
 pub fn invalidate_intent(intent: &Signed<Intent>) {
     use crate::imports::tx;
     let key = intent::invalid_intent_key(&intent.data.addr);
-    let mut invalid_intent: Vec<Signature> =
+    let mut invalid_intent: HashSet<Signature> =
         tx::read(&key.to_string()).unwrap_or_default();
-    invalid_intent.push(intent.sig.clone());
-    tx::write(&key.to_string(), invalid_intent)
+    invalid_intent.insert(intent.sig.clone());
+    tx::write(&key.to_string(), &invalid_intent)
 }
 
 pub fn vp(intent: &Signed<Intent>) -> bool {
     use crate::imports::vp;
     let key = intent::invalid_intent_key(&intent.data.addr);
 
-    let invalid_intent_pre: Vec<Signature> =
+    let invalid_intent_pre: HashSet<Signature> =
         vp::read_pre(&key.to_string()).unwrap_or_default();
-    let invalid_intent_post: Vec<Signature> =
+    let invalid_intent_post: HashSet<Signature> =
         vp::read_post(&key.to_string()).unwrap_or_default();
-    !invalid_intent_pre.iter().any(|sig| sig == &intent.sig)
-        && invalid_intent_post.iter().any(|sig| sig == &intent.sig)
+    !invalid_intent_pre.contains(&intent.sig)
+        && invalid_intent_post.contains(&intent.sig)
 }
