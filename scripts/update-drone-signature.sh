3b0c484706cb7132ebe950b381f127d3447de869
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2021-05-25 17:17:26 +0200

    [feat]: update drone configuration signature

---
+++ scripts/update-drone-signature.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROFILE=$1
+DRONE_URL=${2:-ci.heliax.dev}
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. AWS Profile and Drone url needed."
+    exit 1
+fi
+
+TOKEN=$(aws ssm get-parameter --name "drone_machine_secret" --with-decryption --region eu-west-1 --profile $1 | jq -r '.Parameter.Value')
+
+export DRONE_TOKEN=$TOKEN
+export DRONE_SERVER=https://$DRONE_URL
+
+drone sign --save heliaxdev/anoma-prototype
