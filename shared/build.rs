14a62446ed5ac24872f3551c60ad05ae8102bc3a
Tomáš Zemanovič <tzemanovic@gmail.com>
2021-06-11 21:36:20 +0200

    avoid compiling proto file in docker

--- shared/build.rs
+++ shared/build.rs
@@ -7,6 +7,13 @@ const PROTO_SRC: &str = "../proto";
 const RUSTFMT_TOOLCHAIN: &str = "nightly-2021-03-09";
 
 fn main() {
+    if let Ok(val) = env::var("COMPILE_PROTO") {
+        if val.to_ascii_lowercase() == "false" {
+            // Skip compiling proto files
+            return;
+        }
+    }
+
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed={}", PROTO_SRC);
 
