19d16f0c55495eeb55ad8b4ab0de4cca385f552a
Tomáš Zemanovič <tzemanovic@gmail.com>
2021-06-15 15:03:52 +0200

    add more wasms for tests

--- wasm_for_tests/txs_and_vps/src/lib.rs
+++ wasm_for_tests/txs_and_vps/src/lib.rs
@@ -1,3 +1,4 @@
+/// A tx that doesn't do anything.
 #[cfg(feature = "tx_no_op")]
 pub mod main {
     use anoma_vm_env::tx_prelude::*;
@@ -6,13 +7,13 @@ pub mod main {
     fn apply_tx(_tx_data: Vec<u8>) {}
 }
 
+/// A tx that allocates a memory of size given from the `tx_data: usize`.
 #[cfg(feature = "tx_memory_limit")]
 pub mod main {
     use anoma_vm_env::tx_prelude::*;
 
     #[transaction]
     fn apply_tx(tx_data: Vec<u8>) {
-        // Allocates a memory of size given from the `tx_data (usize)`
         let len = usize::try_from_slice(&tx_data[..]).unwrap();
         log_string(format!("allocate len {}", len));
         let bytes: Vec<u8> = vec![6_u8; len];
@@ -21,10 +22,23 @@ pub mod main {
     }
 }
 
+/// A tx that attempts to read the given key from storage.
+#[cfg(feature = "tx_read_storage_key")]
+pub mod main {
+    use anoma_vm_env::tx_prelude::*;
+
+    #[transaction]
+    fn apply_tx(tx_data: Vec<u8>) {
+        // Allocates a memory of size given from the `tx_data (usize)`
+        let key = storage::Key::try_from_slice(&tx_data[..]).unwrap();
+        log_string(format!("key {}", key));
+        let _result: Vec<u8> = read(key.to_string()).unwrap();
+    }
+}
+
+/// A VP that always returns `true`.
 #[cfg(feature = "vp_always_true")]
 pub mod main {
-    use std::collections::HashSet;
-
     use anoma_vm_env::vp_prelude::*;
 
     #[validity_predicate]
@@ -38,10 +52,9 @@ pub mod main {
     }
 }
 
+/// A VP that always returns `false`.
 #[cfg(feature = "vp_always_false")]
 pub mod main {
-    use std::collections::HashSet;
-
     use anoma_vm_env::vp_prelude::*;
 
     #[validity_predicate]
@@ -55,7 +68,30 @@ pub mod main {
     }
 }
 
+// A VP that allocates a memory of size given from the `tx_data: usize`.
 #[cfg(feature = "vp_memory_limit")]
+pub mod main {
+    use anoma_vm_env::vp_prelude::*;
+
+    #[validity_predicate]
+    fn validate_tx(
+        tx_data: Vec<u8>,
+        _addr: Address,
+        _keys_changed: Vec<storage::Key>,
+        _verifiers: HashSet<Address>,
+    ) -> bool {
+        let len = usize::try_from_slice(&tx_data[..]).unwrap();
+        log_string(format!("allocate len {}", len));
+        let bytes: Vec<u8> = vec![6_u8; len];
+        // use the variable to prevent it from compiler optimizing it away
+        log_string(format!("{:?}", &bytes[..8]));
+        true
+    }
+}
+
+/// A VP that attempts to read the given key from storage (state prior to tx
+/// execution).
+#[cfg(feature = "vp_read_storage_key")]
 pub mod main {
     use std::collections::HashSet;
 
@@ -69,11 +105,9 @@ pub mod main {
         _verifiers: HashSet<Address>,
     ) -> bool {
         // Allocates a memory of size given from the `tx_data (usize)`
-        let len = usize::try_from_slice(&tx_data[..]).unwrap();
-        log_string(format!("allocate len {}", len));
-        let bytes: Vec<u8> = vec![6_u8; len];
-        // use the variable to prevent it from compiler optimizing it away
-        log_string(format!("{:?}", &bytes[..8]));
+        let key = storage::Key::try_from_slice(&tx_data[..]).unwrap();
+        log_string(format!("key {}", key));
+        let _result: Vec<u8> = read_pre(key.to_string()).unwrap();
         true
     }
 }
