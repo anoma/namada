471dd538affe5e21628091f70579f1b00b5175ed
Tomáš Zemanovič <tzemanovic@gmail.com>
2021-06-29 19:01:34 +0200

    [gossip] reduce deps logging

--- apps/src/bin/anoma-node/main.rs
+++ apps/src/bin/anoma-node/main.rs
@@ -5,23 +5,44 @@ use std::env;
 use anoma::logging;
 use color_eyre::eyre::Result;
 use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::EnvFilter;
 
-const ABCI_MOD: &str = "tendermint_abci";
 fn main() -> Result<()> {
     // init error reporting
     color_eyre::install()?;
 
     // init logging
-    let abci_level = format!("{}=warn", ABCI_MOD).parse()?;
     let filter = logging::filter_from_env_or(LevelFilter::INFO);
-    let filter = match env::var(logging::ENV_KEY) {
-        // reduce the logging from ABCI unless it's explicitly set
-        Ok(log_env_var) if log_env_var.contains(ABCI_MOD) => filter,
-        _ => filter.add_directive(abci_level),
-    };
+    let log_env_var = env::var(logging::ENV_KEY);
+
+    // Ledger dependencies
+    let filter = reduce_log_level("tendermint_abci", filter, &log_env_var)?;
+    // Gossip dependencies
+    let filter = reduce_log_level("regalloc", filter, &log_env_var)?;
+    let filter =
+        reduce_log_level("wasmer_compiler_cranelift", filter, &log_env_var)?;
+
     logging::set_subscriber(filter)?;
     logging::init_log_tracer()?;
 
     // run the CLI
     cli::main()
+}
+
+/// Reduce the logging from given crate unless it's explicitly set.
+fn reduce_log_level(
+    crate_name: &str,
+    filter: EnvFilter,
+    log_env_var: &Result<String, std::env::VarError>,
+) -> Result<EnvFilter> {
+    let contains_crate = if let Ok(log_env_var) = log_env_var {
+        log_env_var.contains(crate_name)
+    } else {
+        false
+    };
+    Ok(if !contains_crate {
+        filter.add_directive(format!("{}=warn", crate_name).parse()?)
+    } else {
+        filter
+    })
 }
