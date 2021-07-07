a0eadd61d4c78ed6130619e30239d2bde3cb7290
yito88 <yuji@phact-columba.com>
2021-07-07 13:26:04 +0900

    fix wasm build

--- shared/src/ledger/mod.rs
+++ shared/src/ledger/mod.rs
@@ -1,8 +1,11 @@
 //! The ledger modules
 
 pub mod gas;
+#[cfg(feature = "wasm-runtime")]
 pub mod ibc;
+#[cfg(feature = "wasm-runtime")]
 pub mod native_vp;
+#[cfg(feature = "wasm-runtime")]
 pub mod pos;
 pub mod storage;
 pub mod vp_env;
