5733178455e2f8b8faa26bade2bae1c2b22b774b
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2021-07-21 15:35:25 +0200

    [feat]: cargo audit as cron

--- scripts/ci/audit.py
+++ scripts/ci/audit.py
@@ -1,8 +1,60 @@
+import os
 import json
 import subprocess
+import urllib
+import urllib.request
+from urllib.request import urlopen
+
+GH_TOKEN = os.environ['GITHUB_TOKEN']
+
+GET_ISSUES_URL = 'https://api.github.com/repos/{}/{}/issues'.format('anoma', 'anoma')
+CREATE_ISSUE_URL = 'https://api.github.com/repos/{}/{}/issues'.format('anoma', 'anoma')
+MODIFY_ISSUE = 'https://api.github.com/repos/{}/{}/issues/{}'.format('anoma', 'anoma', '{}')
+HEADERS = {
+    'Content-Type': 'application/json', 
+    'Accept': 'application/vnd.github.v3+json', 
+    'Authorization': 'token {}'.format(GH_TOKEN)
+}
+
+
+# 0 - not exist,2 already exist, else issue number
+def check_issue_status(body: str) -> int:
+    params = {'labels':'audit','state':'open'}
+    params_encoded = urllib.parse.urlencode(params)
+    req = urllib.request.Request("{}?{}".format(CREATE_ISSUE_URL, params_encoded), headers=HEADERS)
+
+    with urlopen(req) as response:
+        issues = json.load(response)
+        for issue in issues:
+            title_check = issue['title'] == 'Cargo Audit' 
+            label_check = 'audit' in list(map(lambda label: label['name'], issue['labels']))
+            if title_check and label_check:
+                if issue['body'] == body:
+                    return 2
+                return issue['number']
+        return 0
+
+
+def modify_issue(issue_number: int, body: str):
+    body = {"body": body}
+    encoded_body = json.dumps(body).encode('ascii')
+    req = urllib.request.Request(MODIFY_ISSUE.format(issue_number), data=encoded_body, headers=HEADERS)
+    req.get_method = lambda: 'PATCH'
+
+    with urlopen(req) as response:
+        json.load(response)
+
+
+def create_issue(body: str):
+    body = {"title": "Cargo Audit", "body": body, "labels": ["audit"]}
+    encoded_body = json.dumps(body).encode('ascii')
+    req = urllib.request.Request(CREATE_ISSUE_URL, data=encoded_body, headers=HEADERS)
+
+    with urlopen(req) as response:
+        json.load(response)
+
 
 table_header = '| Id  | Package  | Title  | Date  |\n|----:|---------:|-------:|------:|'
-
 table_row = '|{}|{}|{}|{}|'
 
 table = [table_header]
@@ -12,7 +64,12 @@ p = subprocess.Popen(command, stdout=subprocess.PIPE)
 output = p.stdout.read()
 retcode = p.wait()
 
-for vulnerability in json.loads(output)['vulnerabilities']['list']:
+vulnerabilities = json.loads(output)['vulnerabilities']
+if int(vulnerabilities['count']) == 0:
+    print("No vulnerabilities found.")
+    exit(0)
+
+for vulnerability in vulnerabilities['list']:
     vuln_description = vulnerability['advisory']
     vuln_id = vuln_description['id']
     vuln_title = vuln_description['title']
@@ -21,4 +78,15 @@ for vulnerability in json.loads(output)['vulnerabilities']['list']:
     new_table_row = table_row.format(vuln_id, vuln_package, vuln_title, vuln_date)
     table.append(new_table_row)
 
-print('\n'.join(table))
+table_rendered = '\n'.join(table)
+
+issue_status = check_issue_status(table_rendered)
+
+if issue_status == 0:
+    print("Create new issue.")
+    create_issue(table_rendered)
+elif issue_status == 2:
+    print("Issue already created.")
+else:
+    print("Issue updated.")
+    modify_issue(issue_status, table_rendered)
