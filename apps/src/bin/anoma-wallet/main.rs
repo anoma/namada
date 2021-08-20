7ff7437b8371a656901dc3af8f0fa5efb5826979
Pablo Hildo <pablohildo@gmail.com>
2021-08-20 18:17:26 -0300

    feat: implements keypair generation

--- apps/src/bin/anoma-wallet/main.rs
+++ apps/src/bin/anoma-wallet/main.rs
@@ -1,13 +1,15 @@
 use std::collections::{HashMap, HashSet};
 
-use anoma::types::key::ed25519::{Keypair, PublicKey};
-use anoma::types::address::Address;
+use anoma::types::{
+    address::Address,
+    key::ed25519::{Keypair, PublicKey},
+};
 
 pub type Alias = String;
 
 pub struct Store {
     pub keys: HashMap<Alias, Keypair>,
-    pub addresses: HashSet<Address>
+    pub addresses: HashSet<Address>,
 }
 
 impl Store {
@@ -15,12 +17,30 @@ impl Store {
         self.keys.get(&alias)
     }
 
-    pub fn fetch_by_public_key(&self, public_key: PublicKey) -> Option<&Keypair> {
-        self.keys.values().find(|keypair| {
-           public_key.is_same_key(keypair.public) 
-        })
+    pub fn fetch_by_public_key(
+        &self,
+        public_key: PublicKey,
+    ) -> Option<&Keypair> {
+        self.keys
+            .values()
+            .find(|keypair| public_key.is_same_key(keypair.public))
+    }
+
+    pub fn insert_keypair(
+        &mut self,
+        alias: Alias,
+        keypair: Keypair,
+    ) -> Option<Keypair> {
+        self.keys.insert(alias, keypair)
     }
 }
 
-pub fn main() {
+fn generate_keypair(alias: Alias, store: &mut Store) -> Option<Keypair> {
+    use rand::rngs::OsRng;
+
+    let mut csprng = OsRng {};
+    let keypair = Keypair::generate(&mut csprng);
+    store.insert_keypair(alias, keypair)
 }
+
+pub fn main() {}
