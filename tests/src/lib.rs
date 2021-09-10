8fd78786247bc7d95846743747bfda423c9c7b91
Tomáš Zemanovič <tzemanovic@gmail.com>
2021-09-10 17:35:03 +0200

    [tests] export logging helper

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -4,3 +4,10 @@
 mod vm_host_env;
 pub use vm_host_env::{tx, vp};
 mod e2e;
+
+/// Using this import requires `tracing` and `tracing-subscriber` dependencies.
+/// Set env var `RUST_LOG=info` to see the logs from a test run (and
+/// `--nocapture` if the test is not failing).
+pub mod log {
+    pub use test_env_log::test;
+}
