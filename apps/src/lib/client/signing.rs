2809d846dfd95e61d53c51be7f43daf108965119
Tomáš Zemanovič <tzemanovic@gmail.com>
2021-09-27 13:08:07 +0200

    [client] allow to sign with implicit account

--- apps/src/lib/client/signing.rs
+++ apps/src/lib/client/signing.rs
@@ -3,7 +3,7 @@
 
 use std::rc::Rc;
 
-use anoma::types::address::Address;
+use anoma::types::address::{Address, ImplicitAddress};
 use anoma::types::key::ed25519::Keypair;
 
 use super::rpc;
@@ -17,22 +17,47 @@ pub async fn find_keypair(
     addr: &Address,
     ledger_address: tendermint::net::Address,
 ) -> Rc<Keypair> {
-    println!(
-        "Looking-up public key of {} from the ledger...",
-        addr.encode()
-    );
-    let public_key = rpc::get_public_key(addr, ledger_address)
-        .await
-        .unwrap_or_else(|| {
-            eprintln!("No public key found for the address {}", addr.encode());
-            cli::safe_exit(1);
-        });
-    wallet.find_key_by_pk(&public_key).unwrap_or_else(|err| {
-        eprintln!(
-            "Unable to load the keypair from the wallet for public key {}. \
-             Failed with: {}",
-            public_key, err
-        );
-        cli::safe_exit(1)
-    })
+    match addr {
+        Address::Established(_) => {
+            println!(
+                "Looking-up public key of {} from the ledger...",
+                addr.encode()
+            );
+            let public_key = rpc::get_public_key(addr, ledger_address)
+                .await
+                .unwrap_or_else(|| {
+                    eprintln!(
+                        "No public key found for the address {}",
+                        addr.encode()
+                    );
+                    cli::safe_exit(1);
+                });
+            wallet.find_key_by_pk(&public_key).unwrap_or_else(|err| {
+                eprintln!(
+                    "Unable to load the keypair from the wallet for public \
+                     key {}. Failed with: {}",
+                    public_key, err
+                );
+                cli::safe_exit(1)
+            })
+        }
+        Address::Implicit(ImplicitAddress::Ed25519(pkh)) => {
+            wallet.find_key_by_pkh(pkh).unwrap_or_else(|err| {
+                eprintln!(
+                    "Unable to load the keypair from the wallet for the \
+                     implicit address {}. Failed with: {}",
+                    addr.encode(),
+                    err
+                );
+                cli::safe_exit(1)
+            })
+        }
+        Address::Internal(_) => {
+            eprintln!(
+                "Internal address {} doesn't have any signing keys.",
+                addr
+            );
+            cli::safe_exit(1)
+        }
+    }
 }
