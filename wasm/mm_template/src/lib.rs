e79d8404ed299b7d953d65a217f0318143572a98
Tomáš Zemanovič <tzemanovic@gmail.com>
2021-09-28 13:17:23 +0200

    make fmt

--- wasm/mm_template/src/lib.rs
+++ wasm/mm_template/src/lib.rs
@@ -1,6 +1,10 @@
 use anoma_vm_env::matchmaker_prelude::*;
 
 #[matchmaker]
-fn add_intent(_last_state: Vec<u8>, _intent_id: Vec<u8>, _intent_data: Vec<u8>) -> bool {
+fn add_intent(
+    _last_state: Vec<u8>,
+    _intent_id: Vec<u8>,
+    _intent_data: Vec<u8>,
+) -> bool {
     true
 }
