50a2821b9e02e6a7247d308e3412fd5b5ec5de22
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2021-09-29 17:36:21 +0200

    [fix]: checksums.py minor bug

--- wasm/checksums.py
+++ wasm/checksums.py
@@ -8,6 +8,6 @@ for wasm in sorted(glob.glob("wasm/*.wasm")):
     basename = os.path.basename(wasm)
     file_name = os.path.splitext(basename)[0] if wasm.count(".") == 1 else os.path.splitext(basename)[0].split('.')[0]
     checksums["{}.wasm".format(file_name)] = "{}.{}.wasm".format(file_name, hashlib.sha256(open(wasm, "rb").read()).hexdigest())
-    os.rename(wasm, checksums["wasm/{}.wasm".format(file_name)])
+    os.rename(wasm, 'wasm/{}'.format(checksums["{}.wasm".format(file_name)]))
 
 json.dump(checksums, open("wasm/checksums.json", "w"))
