44101a6d36bec588eb158ad5962085298997afc1
Tomáš Zemanovič <tomas@heliax.dev>
2021-11-17 11:16:39 +0100

    [apps/node/ledger] refactor ledger's threads usage
    Run the shell in a dedicated thread "ledger-shell", receiving and processing
    message from AbciService.
    
    Added env vars ANOMA_TOKIO_THREADS and ANOMA_RAYON_THREADS, both defaulting to
    1/2 CPUs (logical).
    
    Refactored tendermint_node and wasm_loader to take advantage of async
    and removed the manual failure handling and instead let the errors propagate
    from async tasks.

--- apps/src/lib/node/ledger/shims/abcipp_shim.rs
+++ apps/src/lib/node/ledger/shims/abcipp_shim.rs
@@ -19,35 +19,146 @@ use crate::node::ledger::shims::abcipp_shim_types::shim::request::{
     BeginBlock, ProcessedTx,
 };
 
-/// The shim wraps the shell, which implements ABCI++
-/// The shim makes a crude translation between the ABCI
-/// interface currently used by tendermint and the shell's
-/// interface
+/// The shim wraps the shell, which implements ABCI++.
+/// The shim makes a crude translation between the ABCI interface currently used
+/// by tendermint and the shell's interface.
+#[derive(Debug)]
 pub struct AbcippShim {
     service: Shell,
     begin_block_request: Option<BeginBlock>,
     block_txs: Vec<ProcessedTx>,
+    shell_recv: tokio::sync::mpsc::Receiver<(
+        Req,
+        tokio::sync::oneshot::Sender<Result<Resp, BoxError>>,
+    )>,
 }
 
 impl AbcippShim {
+    /// Create a shell with a ABCI service that passes messages to and from the
+    /// shell.
     pub fn new(
         base_dir: PathBuf,
         db_path: impl AsRef<Path>,
         chain_id: ChainId,
         wasm_dir: PathBuf,
-    ) -> Self {
-        Self {
-            service: Shell::new(base_dir, db_path, chain_id, wasm_dir),
-            begin_block_request: None,
-            block_txs: vec![],
+    ) -> (Self, AbciService) {
+        let (shell_send, shell_recv) = tokio::sync::mpsc::channel(1024);
+        (
+            Self {
+                service: Shell::new(base_dir, db_path, chain_id, wasm_dir),
+                begin_block_request: None,
+                block_txs: vec![],
+                shell_recv,
+            },
+            AbciService { shell_send },
+        )
+    }
+
+    /// Run the shell's blocking loop that receives messages from the
+    /// [`AbciService`].
+    pub fn run(mut self) {
+        while let Some((req, resp_sender)) = self.shell_recv.blocking_recv() {
+            let resp = match req {
+                Req::BeginBlock(block) => {
+                    // we save this data to be forwarded to finalize later
+                    self.begin_block_request =
+                        Some(block.try_into().unwrap_or_else(|_| {
+                            panic!("Could not read begin block request");
+                        }));
+                    Ok(Resp::BeginBlock(Default::default()))
+                }
+                Req::DeliverTx(deliver_tx) => {
+                    // We call [`process_proposal`] to report back the validity
+                    // of the tx to tendermint.
+                    // Invariant: The service call with
+                    // `Request::ProcessProposal`
+                    // must always return `Response::ProcessProposal`
+                    self.service
+                        .call(Request::ProcessProposal(
+                            #[cfg(not(feature = "ABCI"))]
+                            deliver_tx.tx.clone().into(),
+                            #[cfg(feature = "ABCI")]
+                            deliver_tx.tx.into(),
+                        ))
+                        .map_err(Error::from)
+                        .and_then(|res| match res {
+                            Response::ProcessProposal(resp) => {
+                                self.block_txs.push(ProcessedTx {
+                                    #[cfg(not(feature = "ABCI"))]
+                                    tx: deliver_tx.tx,
+                                    #[cfg(feature = "ABCI")]
+                                    tx: resp.tx,
+                                    result: resp.result,
+                                });
+                                Ok(Resp::DeliverTx(Default::default()))
+                            }
+                            _ => unreachable!(),
+                        })
+                }
+                Req::EndBlock(end) => {
+                    BlockHeight::try_from(end.height).unwrap_or_else(|_| {
+                        panic!("Unexpected block height {}", end.height)
+                    });
+                    let mut txs = vec![];
+                    std::mem::swap(&mut txs, &mut self.block_txs);
+                    // If the wrapper txs were not properly submitted, reject
+                    // all txs
+                    let out_of_order =
+                        txs.iter().any(|tx| tx.result.code > 3u32);
+                    if out_of_order {
+                        // The wrapper txs will need to be decrypted again
+                        // and included in the proposed block after the current
+                        self.service.reset_queue();
+                    }
+                    let begin_block_request =
+                        self.begin_block_request.take().unwrap();
+                    self.service
+                        .call(Request::FinalizeBlock(request::FinalizeBlock {
+                            hash: begin_block_request.hash,
+                            header: begin_block_request.header,
+                            byzantine_validators: begin_block_request
+                                .byzantine_validators,
+                            txs,
+                            reject_all_decrypted: out_of_order,
+                        }))
+                        .map_err(Error::from)
+                        .and_then(|res| match res {
+                            Response::FinalizeBlock(resp) => {
+                                let x = Resp::EndBlock(resp.into());
+                                Ok(x)
+                            }
+                            _ => Err(Error::ConvertResp(res)),
+                        })
+                }
+                _ => match Request::try_from(req.clone()) {
+                    Ok(request) => self
+                        .service
+                        .call(request)
+                        .map(Resp::try_from)
+                        .map_err(Error::Shell)
+                        .and_then(|inner| inner),
+                    Err(err) => Err(err),
+                },
+            };
+            let resp = resp.map_err(|e| e.into());
+            if resp_sender.send(resp).is_err() {
+                tracing::info!("ABCI response channel is closed")
+            }
         }
     }
 }
 
-/// This is the actual tower service that we run for now.
-/// It provides the translation between tendermints interface
-/// and the interface of the shell service.
-impl Service<Req> for AbcippShim {
+#[derive(Debug)]
+pub struct AbciService {
+    shell_send: tokio::sync::mpsc::Sender<(
+        Req,
+        tokio::sync::oneshot::Sender<Result<Resp, BoxError>>,
+    )>,
+}
+
+/// The ABCI tower service implementation sends and receives messages to and
+/// from the [`AbcippShim`] for requests from Tendermint.
+impl Service<Req> for AbciService {
     type Error = BoxError;
     type Future =
         Pin<Box<dyn Future<Output = Result<Resp, BoxError>> + Send + 'static>>;
@@ -57,90 +168,30 @@ impl Service<Req> for AbcippShim {
         &mut self,
         _cx: &mut Context<'_>,
     ) -> Poll<Result<(), Self::Error>> {
+        // First, check if the shell is still alive
+        if self.shell_send.is_closed() {
+            return Poll::Ready(Err("The shell is closed".into()));
+        }
         Poll::Ready(Ok(()))
     }
 
     fn call(&mut self, req: Req) -> Self::Future {
-        let rsp = match req {
-            Req::BeginBlock(block) => {
-                // we save this data to be forwarded to finalize later
-                self.begin_block_request =
-                    Some(block.try_into().unwrap_or_else(|_| {
-                        panic!("Could not read begin block request");
-                    }));
-                Ok(Resp::BeginBlock(Default::default()))
-            }
-            Req::DeliverTx(deliver_tx) => {
-                // We call [`process_proposal`] to report back the validity
-                // of the tx to tendermint.
-                // Invariant: The service call with `Request::ProcessProposal`
-                // must always return `Response::ProcessProposal`
-                self.service
-                    .call(Request::ProcessProposal(
-                        #[cfg(not(feature = "ABCI"))]
-                        deliver_tx.tx.clone().into(),
-                        #[cfg(feature = "ABCI")]
-                        deliver_tx.tx.into(),
-                    ))
-                    .map_err(Error::from)
-                    .and_then(|res| match res {
-                        Response::ProcessProposal(resp) => {
-                            self.block_txs.push(ProcessedTx {
-                                #[cfg(not(feature = "ABCI"))]
-                                tx: deliver_tx.tx,
-                                #[cfg(feature = "ABCI")]
-                                tx: resp.tx,
-                                result: resp.result,
-                            });
-                            Ok(Resp::DeliverTx(Default::default()))
-                        }
-                        _ => unreachable!(),
-                    })
-            }
-            Req::EndBlock(end) => {
-                BlockHeight::try_from(end.height).unwrap_or_else(|_| {
-                    panic!("Unexpected block height {}", end.height)
-                });
-                let mut txs = vec![];
-                std::mem::swap(&mut txs, &mut self.block_txs);
-                // If the wrapper txs were not properly submitted, reject all
-                // txs
-                let out_of_order = txs.iter().any(|tx| tx.result.code > 3u32);
-                if out_of_order {
-                    // The wrapper txs will need to be decrypted again
-                    // and included in the proposed block after the current
-                    self.service.reset_queue();
+        let sender = self.shell_send.clone();
+        Box::pin(
+            async move {
+                let (resp_send, recv) = tokio::sync::oneshot::channel();
+                if sender.send((req, resp_send)).await.is_err() {
+                    return Err("The shell is closed".into());
                 }
-                let begin_block_request =
-                    self.begin_block_request.take().unwrap();
-                self.service
-                    .call(Request::FinalizeBlock(request::FinalizeBlock {
-                        hash: begin_block_request.hash,
-                        header: begin_block_request.header,
-                        byzantine_validators: begin_block_request
-                            .byzantine_validators,
-                        txs,
-                        reject_all_decrypted: out_of_order,
-                    }))
-                    .map_err(Error::from)
-                    .and_then(|res| match res {
-                        Response::FinalizeBlock(resp) => {
-                            let x = Resp::EndBlock(resp.into());
-                            Ok(x)
-                        }
-                        _ => Err(Error::ConvertResp(res)),
-                    })
+                match recv.await {
+                    Ok(resp) => resp,
+                    Err(err) => {
+                        tracing::info!("ABCI response channel didn't respond");
+                        Err(err.into())
+                    }
+                }
             }
-            _ => match Request::try_from(req.clone()) {
-                Ok(request) => self
-                    .service
-                    .call(request)
-                    .map(Resp::try_from)
-                    .map_err(Error::Shell)
-                    .and_then(|inner| inner),
-                Err(err) => Err(err),
-            },
-        };
-        Box::pin(async move { rsp.map_err(|e| e.into()) }.boxed())
+            .boxed(),
+        )
     }
 }
