2cc21948f0f07f7f5e095e03331c3b0fc53785b3
Tomáš Zemanovič <tomas@heliax.dev>
2021-12-08 15:56:27 +0100

    wasm: add tx_prelude and vp_prelude
    We need to separate the tx and VP preludes into crates, because we
    cannot declare a macro with the same name in one crate (fails with
    error[E0428]: the name `debug_log` is defined multiple times).

---
+++ vp_prelude/src/lib.rs
@@ -0,0 +1,19 @@
+//! This crate contains library code for validity predicate WASM. Most of the
+//! code is re-exported from the `anoma_vm_env` crate.
+
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(rustdoc::private_intra_doc_links)]
+
+pub use anoma_vm_env::vp_prelude::*;
+
+/// Log a string in a debug build. The message will be printed at the
+/// `tracing::Level::Info`. Any `debug_log!` statements are only enabled in
+/// non optimized builds by default. An optimized build will not execute
+/// `debug_log!` statements unless `-C debug-assertions` is passed to the
+/// compiler.
+#[macro_export]
+macro_rules! debug_log {
+    ($($arg:tt)*) => {{
+        (if cfg!(debug_assertions) { log_string(format!($($arg)*)) })
+    }}
+}
