02a95b77cf048e92f05e652ac53d36b4bc8e75ea
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2021-12-27 13:47:14 +0100

    [feat]: update drone signature script

--- scripts/ci/update-drone-config.py
+++ scripts/ci/update-drone-config.py
@@ -13,29 +13,6 @@ DRONE_FILE: str = ".drone"
 DRONE_FILE_SUFFIX: str = ".yml"
 REPOSITORY: str = "anoma/anoma"
 
-STEP_NAME = 'check-scripts-integrity'
-
-files_to_check = [
-    'Makefile',
-    'wasm/wasm_source/Makefile',
-    'wasm/vp_template/Makefile',
-    'wasm/tx_template/Makefile',
-    'docs/Makefile',
-    'scripts/ci/pre-run.sh',
-    'scripts/ci/release.sh',
-    'scripts/ci/build-and-publish-docs.sh',
-    'scripts/ci/audit.py',
-    'scripts/ci/udeps.py',
-    'wasm/checksums.py'
-]
-
-scripts_to_run = [
-    'scripts/ci/pre-run.sh'
-]
-
-check_command_template = 'echo "{}  {}" | sha256sum -c -'
-run_command_template = 'sh {}'
-
 boto_config = Config(
     region_name='eu-west-1',
     retries={
