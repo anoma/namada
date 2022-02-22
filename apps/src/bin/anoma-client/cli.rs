junkicide/anoma

[feat]: initial nft implementation

apps/src/bin/anoma-client/cli.rs
@@ -27,6 +27,12 @@ pub async fn main() -> Result<()> {
                 Sub::TxInitValidator(TxInitValidator(args)) => {
                     tx::submit_init_validator(ctx, args).await;
                 }
+                Sub::TxNftCreate(TxNftCreate(args)) => {
+                    tx::submit_create_nft(ctx, args).await;
+                }
+                Sub::TxNftMint(TxNftMint(args)) => {
+                    tx::submit_mint_nft(ctx, args).await;
+                }
                 Sub::Bond(Bond(args)) => {
                     tx::submit_bond(ctx, args).await;
                 }

apps/src/bin/anoma/cli.rs
@@ -49,6 +49,8 @@ fn handle_command(cmd: cli::cmds::Anoma, raw_sub_cmd: String) -> Result<()> {
         | cli::cmds::Anoma::TxCustom(_)
         | cli::cmds::Anoma::TxTransfer(_)
         | cli::cmds::Anoma::TxUpdateVp(_)
+        | cli::cmds::Anoma::TxNftCreate(_)
+        | cli::cmds::Anoma::TxNftMint(_)
         | cli::cmds::Anoma::Intent(_) => handle_subcommand("anomac", sub_args),
         cli::cmds::Anoma::Wallet(_) => handle_subcommand("anomaw", sub_args),
     }

apps/src/lib/cli.rs
@@ -48,6 +48,8 @@ pub mod cmds {
         TxCustom(TxCustom),
         TxTransfer(TxTransfer),
         TxUpdateVp(TxUpdateVp),
+        TxNftCreate(TxNftCreate),
+        TxNftMint(TxNftMint),
         Intent(Intent),
     }
 
@@ -62,6 +64,8 @@ pub mod cmds {
                 .subcommand(TxCustom::def())
                 .subcommand(TxTransfer::def())
                 .subcommand(TxUpdateVp::def())
+                .subcommand(TxNftCreate::def())
+                .subcommand(TxNftMint::def())
                 .subcommand(Intent::def())
         }
 
@@ -75,6 +79,8 @@ pub mod cmds {
             let tx_custom = SubCmd::parse(matches).map(Self::TxCustom);
             let tx_transfer = SubCmd::parse(matches).map(Self::TxTransfer);
             let tx_update_vp = SubCmd::parse(matches).map(Self::TxUpdateVp);
+            let tx_nft_create = SubCmd::parse(matches).map(Self::TxNftCreate);
+            let tx_nft_mint = SubCmd::parse(matches).map(Self::TxNftMint);
             let intent = SubCmd::parse(matches).map(Self::Intent);
             node.or(client)
                 .or(wallet)
@@ -84,6 +90,8 @@ pub mod cmds {
                 .or(tx_custom)
                 .or(tx_transfer)
                 .or(tx_update_vp)
+                .or(tx_nft_create)
+                .or(tx_nft_mint)
                 .or(intent)
         }
     }
@@ -155,6 +163,9 @@ pub mod cmds {
                 .subcommand(TxUpdateVp::def().display_order(1))
                 .subcommand(TxInitAccount::def().display_order(1))
                 .subcommand(TxInitValidator::def().display_order(1))
+                // Nft transactions
+                .subcommand(TxNftCreate::def().display_order(1))
+                .subcommand(TxNftMint::def().display_order(1))
                 // PoS transactions
                 .subcommand(Bond::def().display_order(2))
                 .subcommand(Unbond::def().display_order(2))
@@ -181,6 +192,8 @@ pub mod cmds {
             let tx_init_account = Self::parse_with_ctx(matches, TxInitAccount);
             let tx_init_validator =
                 Self::parse_with_ctx(matches, TxInitValidator);
+            let tx_nft_create = Self::parse_with_ctx(matches, TxNftCreate);
+            let tx_nft_mint = Self::parse_with_ctx(matches, TxNftMint);
             let bond = Self::parse_with_ctx(matches, Bond);
             let unbond = Self::parse_with_ctx(matches, Unbond);
             let withdraw = Self::parse_with_ctx(matches, Withdraw);
@@ -199,6 +212,8 @@ pub mod cmds {
                 .or(tx_update_vp)
                 .or(tx_init_account)
                 .or(tx_init_validator)
+                .or(tx_nft_create)
+                .or(tx_nft_mint)
                 .or(bond)
                 .or(unbond)
                 .or(withdraw)
@@ -252,6 +267,8 @@ pub mod cmds {
         TxUpdateVp(TxUpdateVp),
         TxInitAccount(TxInitAccount),
         TxInitValidator(TxInitValidator),
+        TxNftCreate(TxNftCreate),
+        TxNftMint(TxNftMint),
         Bond(Bond),
         Unbond(Unbond),
         Withdraw(Withdraw),
@@ -988,6 +1005,50 @@ pub mod cmds {
     }
 
     #[derive(Clone, Debug)]
+    pub struct TxNftCreate(pub args::NftCreate);
+
+    impl SubCmd for TxNftCreate {
+        const CMD: &'static str = "nft-create";
+
+        fn parse(matches: &ArgMatches) -> Option<Self>
+        where
+            Self: Sized,
+        {
+            matches
+                .subcommand_matches(Self::CMD)
+                .map(|matches| TxNftCreate(args::NftCreate::parse(matches)))
+        }
+
+        fn def() -> App {
+            App::new(Self::CMD)
+                .about("Create a new NFT.")
+                .add_args::<args::NftCreate>()
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct TxNftMint(pub args::NftMint);
+
+    impl SubCmd for TxNftMint {
+        const CMD: &'static str = "nft-mint";
+
+        fn parse(matches: &ArgMatches) -> Option<Self>
+        where
+            Self: Sized,
+        {
+            matches
+                .subcommand_matches(Self::CMD)
+                .map(|matches| TxNftMint(args::NftMint::parse(matches)))
+        }
+
+        fn def() -> App {
+            App::new(Self::CMD)
+                .about("Mint new NFT tokens.")
+                .add_args::<args::NftMint>()
+        }
+    }
+
+    #[derive(Clone, Debug)]
     pub struct Intent(pub args::Intent);
 
     impl SubCmd for Intent {
@@ -1213,6 +1274,7 @@ pub mod args {
     const MULTIADDR_OPT: ArgOpt<Multiaddr> = arg_opt("address");
     const NODE_OPT: ArgOpt<String> = arg_opt("node");
     const NODE: Arg<String> = arg("node");
+    const NFT_ADDRESS: Arg<Address> = arg("nft-address");
     const OWNER: ArgOpt<WalletAddress> = arg_opt("owner");
     const PUBLIC_KEY: Arg<WalletPublicKey> = arg("public-key");
     const RAW_ADDRESS: Arg<Address> = arg("address");
@@ -1666,6 +1728,66 @@ pub mod args {
                      withdrawing from self-bonds, the validator is also the \
                      source.",
                 ))
+        }
+    }
+
+    // Transaction to create a new nft
+    #[derive(Clone, Debug)]
+    pub struct NftCreate {
+        /// Common tx argumentsips
+        pub tx: Tx,
+        /// Path to the nft file description
+        pub nft_data: PathBuf,
+    }
+
+    impl Args for NftCreate {
+        fn parse(matches: &ArgMatches) -> Self {
+            let tx = Tx::parse(matches);
+            let data_path = DATA_PATH.parse(matches);
+
+            Self {
+                tx,
+                nft_data: data_path,
+            }
+        }
+
+        fn def(app: App) -> App {
+            app.add_args::<Tx>()
+                .arg(DATA_PATH.def().about("The path nft description file."))
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct NftMint {
+        /// Common tx arguments
+        pub tx: Tx,
+        /// The nft address
+        pub nft_address: Address,
+        /// The nft token description
+        pub nft_data: PathBuf,
+    }
+
+    impl Args for NftMint {
+        fn parse(matches: &ArgMatches) -> Self {
+            let tx = Tx::parse(matches);
+            let nft_address = NFT_ADDRESS.parse(matches);
+            let data_path = DATA_PATH.parse(matches);
+
+            Self {
+                tx,
+                nft_address,
+                nft_data: data_path,
+            }
+        }
+
+        fn def(app: App) -> App {
+            app.add_args::<Tx>()
+                .arg(NFT_ADDRESS.def().about("The nft address."))
+                .arg(
+                    DATA_PATH.def().about(
+                        "The data path file that describes the nft tokens.",
+                    ),
+                )
         }
     }
 

vm_env/src/nft.rs
@@ -1,0 +1,192 @@
+use anoma::types::nft;
+
+/// Tx imports and functions.
+pub mod tx {
+    use anoma::types::address::Address;
+    use anoma::types::nft::NftToken;
+    use anoma::types::transaction::nft::{CreateNft, MintNft};
+
+    use super::*;
+    use crate::imports::tx;
+    pub fn init_nft(nft: CreateNft) -> Address {
+        let address = tx::init_account(&nft.vp_code);
+
+        // write tag
+        let tag_key = nft::get_tag_key(&address);
+        tx::write(&tag_key.to_string(), &nft.tag);
+
+        // write creator
+        let creator_key = nft::get_creator_key(&address);
+        tx::write(&creator_key.to_string(), &nft.creator);
+
+        // write keys
+        let keys_key = nft::get_keys_key(&address);
+        tx::write(&keys_key.to_string(), &nft.keys);
+
+        // write optional keys
+        let optional_keys_key = nft::get_optional_keys_key(&address);
+        tx::write(&optional_keys_key.to_string(), nft.opt_keys);
+
+        // mint tokens
+        aux_mint_token(&address, &nft.creator, nft.tokens, &nft.creator);
+
+        tx::insert_verifier(&nft.creator);
+
+        address
+    }
+
+    pub fn mint_tokens(nft: MintNft) {
+        aux_mint_token(&nft.address, &nft.creator, nft.tokens, &nft.creator);
+    }
+
+    fn aux_mint_token(
+        nft_address: &Address,
+        creator_address: &Address,
+        tokens: Vec<NftToken>,
+        verifier: &Address,
+    ) {
+        for token in tokens {
+            // write token metadata
+            let metadata_key =
+                nft::get_token_metadata_key(nft_address, &token.id.to_string());
+            tx::write(&metadata_key.to_string(), &token.metadata);
+
+            // write current owner token as creator
+            let current_owner_key = nft::get_token_current_owner_key(
+                nft_address,
+                &token.id.to_string(),
+            );
+            tx::write(
+                &current_owner_key.to_string(),
+                &token
+                    .current_owner
+                    .unwrap_or_else(|| creator_address.clone()),
+            );
+
+            // write value key
+            let value_key =
+                nft::get_token_value_key(nft_address, &token.id.to_string());
+            tx::write(&value_key.to_string(), &token.values);
+
+            // write optional value keys
+            let optional_value_key = nft::get_token_optional_value_key(
+                nft_address,
+                &token.id.to_string(),
+            );
+            tx::write(&optional_value_key.to_string(), &token.opt_values);
+
+            // write burnt propriety
+            let burnt_key =
+                nft::get_token_burnt_key(nft_address, &token.id.to_string());
+            tx::write(&burnt_key.to_string(), token.burnt);
+
+            // write approval addresses
+            let approval_key =
+                nft::get_token_approval_key(nft_address, &token.id.to_string());
+            tx::write(&approval_key.to_string(), &token.approvals);
+        }
+        tx::insert_verifier(verifier);
+    }
+}
+
+/// A Nft validity predicate
+pub mod vp {
+    use std::collections::HashSet;
+
+    use anoma::types::address::Address;
+    pub use anoma::types::nft::*;
+    use anoma::types::storage::Key;
+
+    use crate::imports::tx;
+    use crate::imports::vp::{self};
+
+    enum KeyType {
+        Metadata(Address, String),
+        Approval(Address, String),
+        CurrentOwner(Address, String),
+        Creator(Address),
+        PastOwners(Address, String),
+        Unknown,
+    }
+
+    pub fn vp(
+        _tx_da_ta: Vec<u8>,
+        nft_address: &Address,
+        keys_changed: &HashSet<Key>,
+        verifiers: &HashSet<Address>,
+    ) -> bool {
+        keys_changed
+            .iter()
+            .all(|key| match get_key_type(key, nft_address) {
+                KeyType::Creator(_creator_addr) => {
+                    vp::log_string("creator cannot be changed.".to_string());
+                    false
+                }
+                KeyType::Approval(nft_address, token_id) => {
+                    vp::log_string(format!(
+                        "nft vp, checking approvals with token id: {}",
+                        token_id
+                    ));
+
+                    is_creator(&nft_address, verifiers)
+                        || is_approved(
+                            &nft_address,
+                            token_id.as_ref(),
+                            verifiers,
+                        )
+                }
+                KeyType::Metadata(nft_address, token_id) => {
+                    vp::log_string(format!(
+                        "nft vp, checking if metadata changed: {}",
+                        token_id
+                    ));
+                    is_creator(&nft_address, verifiers)
+                }
+                _ => is_creator(nft_address, verifiers),
+            })
+    }
+
+    fn is_approved(
+        nft_address: &Address,
+        nft_token_id: &str,
+        verifiers: &HashSet<Address>,
+    ) -> bool {
+        let approvals_key =
+            get_token_approval_key(nft_address, nft_token_id).to_string();
+        let approval_addresses: Vec<Address> =
+            vp::read_pre(approvals_key).unwrap();
+        return approval_addresses
+            .iter()
+            .any(|addr| verifiers.contains(addr));
+    }
+
+    fn is_creator(nft_address: &Address, verifiers: &HashSet<Address>) -> bool {
+        let creator_key = get_creator_key(nft_address).to_string();
+        let creator_address: Address = tx::read(creator_key).unwrap();
+        verifiers.contains(&creator_address)
+    }
+
+    fn get_key_type(key: &Key, nft_address: &Address) -> KeyType {
+        let is_creator_key = is_nft_creator_key(key, nft_address);
+        let is_metadata_key = is_nft_metadata_key(key, nft_address);
+        let is_approval_key = is_nft_approval_key(key, nft_address);
+        let is_current_owner_key = is_nft_current_owner_key(key, nft_address);
+        let is_past_owner_key = is_nft_past_owners_key(key, nft_address);
+        if let Some(nft_address) = is_creator_key {
+            return KeyType::Creator(nft_address);
+        }
+        if let Some((nft_address, token_id)) = is_metadata_key {
+            return KeyType::Metadata(nft_address, token_id);
+        }
+        if let Some((nft_address, token_id)) = is_approval_key {
+            return KeyType::Approval(nft_address, token_id);
+        }
+        if let Some((nft_address, token_id)) = is_current_owner_key {
+            return KeyType::CurrentOwner(nft_address, token_id);
+        }
+        if let Some((nft_address, token_id)) = is_past_owner_key {
+            return KeyType::PastOwners(nft_address, token_id);
+        }
+        KeyType::Unknown
+    }
+}

anoma/namada

apps/src/bin/anoma-client/cli.rs
@@ -27,6 +27,12 @@ pub async fn main() -> Result<()> {
                 Sub::TxInitValidator(TxInitValidator(args)) => {
                     tx::submit_init_validator(ctx, args).await;
                 }
+                Sub::TxInitNft(TxInitNft(args)) => {
+                    tx::submit_init_nft(ctx, args).await;
+                }
+                Sub::TxMintNft(TxMintNft(args)) => {
+                    tx::submit_mint_nft(ctx, args).await;
+                }
                 Sub::Bond(Bond(args)) => {
                     tx::submit_bond(ctx, args).await;
                 }

apps/src/bin/anoma/cli.rs
@@ -49,6 +49,8 @@ fn handle_command(cmd: cli::cmds::Anoma, raw_sub_cmd: String) -> Result<()> {
         | cli::cmds::Anoma::TxCustom(_)
         | cli::cmds::Anoma::TxTransfer(_)
         | cli::cmds::Anoma::TxUpdateVp(_)
+        | cli::cmds::Anoma::TxInitNft(_)
+        | cli::cmds::Anoma::TxMintNft(_)
         | cli::cmds::Anoma::Intent(_) => handle_subcommand("anomac", sub_args),
         cli::cmds::Anoma::Wallet(_) => handle_subcommand("anomaw", sub_args),
     }

apps/src/lib/cli.rs
@@ -48,6 +48,8 @@ pub mod cmds {
         TxCustom(TxCustom),
         TxTransfer(TxTransfer),
         TxUpdateVp(TxUpdateVp),
+        TxInitNft(TxInitNft),
+        TxMintNft(TxMintNft),
         Intent(Intent),
     }
 
@@ -62,6 +64,8 @@ pub mod cmds {
                 .subcommand(TxCustom::def())
                 .subcommand(TxTransfer::def())
                 .subcommand(TxUpdateVp::def())
+                .subcommand(TxInitNft::def())
+                .subcommand(TxMintNft::def())
                 .subcommand(Intent::def())
         }
 
@@ -75,6 +79,8 @@ pub mod cmds {
             let tx_custom = SubCmd::parse(matches).map(Self::TxCustom);
             let tx_transfer = SubCmd::parse(matches).map(Self::TxTransfer);
             let tx_update_vp = SubCmd::parse(matches).map(Self::TxUpdateVp);
+            let tx_nft_create = SubCmd::parse(matches).map(Self::TxInitNft);
+            let tx_nft_mint = SubCmd::parse(matches).map(Self::TxMintNft);
             let intent = SubCmd::parse(matches).map(Self::Intent);
             node.or(client)
                 .or(wallet)
@@ -84,6 +90,8 @@ pub mod cmds {
                 .or(tx_custom)
                 .or(tx_transfer)
                 .or(tx_update_vp)
+                .or(tx_nft_create)
+                .or(tx_nft_mint)
                 .or(intent)
         }
     }
@@ -155,6 +163,9 @@ pub mod cmds {
                 .subcommand(TxUpdateVp::def().display_order(1))
                 .subcommand(TxInitAccount::def().display_order(1))
                 .subcommand(TxInitValidator::def().display_order(1))
+                // Nft transactions
+                .subcommand(TxInitNft::def().display_order(1))
+                .subcommand(TxMintNft::def().display_order(1))
                 // PoS transactions
                 .subcommand(Bond::def().display_order(2))
                 .subcommand(Unbond::def().display_order(2))
@@ -181,6 +192,8 @@ pub mod cmds {
             let tx_init_account = Self::parse_with_ctx(matches, TxInitAccount);
             let tx_init_validator =
                 Self::parse_with_ctx(matches, TxInitValidator);
+            let tx_nft_create = Self::parse_with_ctx(matches, TxInitNft);
+            let tx_nft_mint = Self::parse_with_ctx(matches, TxMintNft);
             let bond = Self::parse_with_ctx(matches, Bond);
             let unbond = Self::parse_with_ctx(matches, Unbond);
             let withdraw = Self::parse_with_ctx(matches, Withdraw);
@@ -199,6 +212,8 @@ pub mod cmds {
                 .or(tx_update_vp)
                 .or(tx_init_account)
                 .or(tx_init_validator)
+                .or(tx_nft_create)
+                .or(tx_nft_mint)
                 .or(bond)
                 .or(unbond)
                 .or(withdraw)
@@ -252,6 +267,8 @@ pub mod cmds {
         TxUpdateVp(TxUpdateVp),
         TxInitAccount(TxInitAccount),
         TxInitValidator(TxInitValidator),
+        TxInitNft(TxInitNft),
+        TxMintNft(TxMintNft),
         Bond(Bond),
         Unbond(Unbond),
         Withdraw(Withdraw),
@@ -988,6 +1005,50 @@ pub mod cmds {
     }
 
     #[derive(Clone, Debug)]
+    pub struct TxInitNft(pub args::NftCreate);
+
+    impl SubCmd for TxInitNft {
+        const CMD: &'static str = "init-nft";
+
+        fn parse(matches: &ArgMatches) -> Option<Self>
+        where
+            Self: Sized,
+        {
+            matches
+                .subcommand_matches(Self::CMD)
+                .map(|matches| TxInitNft(args::NftCreate::parse(matches)))
+        }
+
+        fn def() -> App {
+            App::new(Self::CMD)
+                .about("Create a new NFT.")
+                .add_args::<args::NftCreate>()
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct TxMintNft(pub args::NftMint);
+
+    impl SubCmd for TxMintNft {
+        const CMD: &'static str = "mint-nft";
+
+        fn parse(matches: &ArgMatches) -> Option<Self>
+        where
+            Self: Sized,
+        {
+            matches
+                .subcommand_matches(Self::CMD)
+                .map(|matches| TxMintNft(args::NftMint::parse(matches)))
+        }
+
+        fn def() -> App {
+            App::new(Self::CMD)
+                .about("Mint new NFT tokens.")
+                .add_args::<args::NftMint>()
+        }
+    }
+
+    #[derive(Clone, Debug)]
     pub struct Intent(pub args::Intent);
 
     impl SubCmd for Intent {
@@ -1213,6 +1274,7 @@ pub mod args {
     const MULTIADDR_OPT: ArgOpt<Multiaddr> = arg_opt("address");
     const NODE_OPT: ArgOpt<String> = arg_opt("node");
     const NODE: Arg<String> = arg("node");
+    const NFT_ADDRESS: Arg<Address> = arg("nft-address");
     const OWNER: ArgOpt<WalletAddress> = arg_opt("owner");
     const PUBLIC_KEY: Arg<WalletPublicKey> = arg("public-key");
     const RAW_ADDRESS: Arg<Address> = arg("address");
@@ -1666,6 +1728,66 @@ pub mod args {
                      withdrawing from self-bonds, the validator is also the \
                      source.",
                 ))
+        }
+    }
+
+    // Transaction to create a new nft
+    #[derive(Clone, Debug)]
+    pub struct NftCreate {
+        /// Common tx argumentsips
+        pub tx: Tx,
+        /// Path to the nft file description
+        pub nft_data: PathBuf,
+    }
+
+    impl Args for NftCreate {
+        fn parse(matches: &ArgMatches) -> Self {
+            let tx = Tx::parse(matches);
+            let data_path = DATA_PATH.parse(matches);
+
+            Self {
+                tx,
+                nft_data: data_path,
+            }
+        }
+
+        fn def(app: App) -> App {
+            app.add_args::<Tx>()
+                .arg(DATA_PATH.def().about("The path nft description file."))
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct NftMint {
+        /// Common tx arguments
+        pub tx: Tx,
+        /// The nft address
+        pub nft_address: Address,
+        /// The nft token description
+        pub nft_data: PathBuf,
+    }
+
+    impl Args for NftMint {
+        fn parse(matches: &ArgMatches) -> Self {
+            let tx = Tx::parse(matches);
+            let nft_address = NFT_ADDRESS.parse(matches);
+            let data_path = DATA_PATH.parse(matches);
+
+            Self {
+                tx,
+                nft_address,
+                nft_data: data_path,
+            }
+        }
+
+        fn def(app: App) -> App {
+            app.add_args::<Tx>()
+                .arg(NFT_ADDRESS.def().about("The nft address."))
+                .arg(
+                    DATA_PATH.def().about(
+                        "The data path file that describes the nft tokens.",
+                    ),
+                )
         }
     }
 

vm_env/src/nft.rs
@@ -1,0 +1,192 @@
+use anoma::types::nft;
+
+/// Tx imports and functions.
+pub mod tx {
+    use anoma::types::address::Address;
+    use anoma::types::nft::NftToken;
+    use anoma::types::transaction::nft::{CreateNft, MintNft};
+
+    use super::*;
+    use crate::imports::tx;
+    pub fn init_nft(nft: CreateNft) -> Address {
+        let address = tx::init_account(&nft.vp_code);
+
+        // write tag
+        let tag_key = nft::get_tag_key(&address);
+        tx::write(&tag_key.to_string(), &nft.tag);
+
+        // write creator
+        let creator_key = nft::get_creator_key(&address);
+        tx::write(&creator_key.to_string(), &nft.creator);
+
+        // write keys
+        let keys_key = nft::get_keys_key(&address);
+        tx::write(&keys_key.to_string(), &nft.keys);
+
+        // write optional keys
+        let optional_keys_key = nft::get_optional_keys_key(&address);
+        tx::write(&optional_keys_key.to_string(), nft.opt_keys);
+
+        // mint tokens
+        aux_mint_token(&address, &nft.creator, nft.tokens, &nft.creator);
+
+        tx::insert_verifier(&nft.creator);
+
+        address
+    }
+
+    pub fn mint_tokens(nft: MintNft) {
+        aux_mint_token(&nft.address, &nft.creator, nft.tokens, &nft.creator);
+    }
+
+    fn aux_mint_token(
+        nft_address: &Address,
+        creator_address: &Address,
+        tokens: Vec<NftToken>,
+        verifier: &Address,
+    ) {
+        for token in tokens {
+            // write token metadata
+            let metadata_key =
+                nft::get_token_metadata_key(nft_address, &token.id.to_string());
+            tx::write(&metadata_key.to_string(), &token.metadata);
+
+            // write current owner token as creator
+            let current_owner_key = nft::get_token_current_owner_key(
+                nft_address,
+                &token.id.to_string(),
+            );
+            tx::write(
+                &current_owner_key.to_string(),
+                &token
+                    .current_owner
+                    .unwrap_or_else(|| creator_address.clone()),
+            );
+
+            // write value key
+            let value_key =
+                nft::get_token_value_key(nft_address, &token.id.to_string());
+            tx::write(&value_key.to_string(), &token.values);
+
+            // write optional value keys
+            let optional_value_key = nft::get_token_optional_value_key(
+                nft_address,
+                &token.id.to_string(),
+            );
+            tx::write(&optional_value_key.to_string(), &token.opt_values);
+
+            // write approval addresses
+            let approval_key =
+                nft::get_token_approval_key(nft_address, &token.id.to_string());
+            tx::write(&approval_key.to_string(), &token.approvals);
+
+            // write burnt propriety
+            let burnt_key =
+                nft::get_token_burnt_key(nft_address, &token.id.to_string());
+            tx::write(&burnt_key.to_string(), token.burnt);
+        }
+        tx::insert_verifier(verifier);
+    }
+}
+
+/// A Nft validity predicate
+pub mod vp {
+    use std::collections::HashSet;
+
+    use anoma::types::address::Address;
+    pub use anoma::types::nft::*;
+    use anoma::types::storage::Key;
+
+    use crate::imports::tx;
+    use crate::imports::vp::{self};
+
+    enum KeyType {
+        Metadata(Address, String),
+        Approval(Address, String),
+        CurrentOwner(Address, String),
+        Creator(Address),
+        PastOwners(Address, String),
+        Unknown,
+    }
+
+    pub fn vp(
+        _tx_da_ta: Vec<u8>,
+        nft_address: &Address,
+        keys_changed: &HashSet<Key>,
+        verifiers: &HashSet<Address>,
+    ) -> bool {
+        keys_changed
+            .iter()
+            .all(|key| match get_key_type(key, nft_address) {
+                KeyType::Creator(_creator_addr) => {
+                    vp::log_string("creator cannot be changed.");
+                    false
+                }
+                KeyType::Approval(nft_address, token_id) => {
+                    vp::log_string(format!(
+                        "nft vp, checking approvals with token id: {}",
+                        token_id
+                    ));
+
+                    is_creator(&nft_address, verifiers)
+                        || is_approved(
+                            &nft_address,
+                            token_id.as_ref(),
+                            verifiers,
+                        )
+                }
+                KeyType::Metadata(nft_address, token_id) => {
+                    vp::log_string(format!(
+                        "nft vp, checking if metadata changed: {}",
+                        token_id
+                    ));
+                    is_creator(&nft_address, verifiers)
+                }
+                _ => is_creator(nft_address, verifiers),
+            })
+    }
+
+    fn is_approved(
+        nft_address: &Address,
+        nft_token_id: &str,
+        verifiers: &HashSet<Address>,
+    ) -> bool {
+        let approvals_key =
+            get_token_approval_key(nft_address, nft_token_id).to_string();
+        let approval_addresses: Vec<Address> =
+            vp::read_pre(approvals_key).unwrap_or_default();
+        return approval_addresses
+            .iter()
+            .any(|addr| verifiers.contains(addr));
+    }
+
+    fn is_creator(nft_address: &Address, verifiers: &HashSet<Address>) -> bool {
+        let creator_key = get_creator_key(nft_address).to_string();
+        let creator_address: Address = tx::read(creator_key).unwrap();
+        verifiers.contains(&creator_address)
+    }
+
+    fn get_key_type(key: &Key, nft_address: &Address) -> KeyType {
+        let is_creator_key = is_nft_creator_key(key, nft_address);
+        let is_metadata_key = is_nft_metadata_key(key, nft_address);
+        let is_approval_key = is_nft_approval_key(key, nft_address);
+        let is_current_owner_key = is_nft_current_owner_key(key, nft_address);
+        let is_past_owner_key = is_nft_past_owners_key(key, nft_address);
+        if let Some(nft_address) = is_creator_key {
+            return KeyType::Creator(nft_address);
+        }
+        if let Some((nft_address, token_id)) = is_metadata_key {
+            return KeyType::Metadata(nft_address, token_id);
+        }
+        if let Some((nft_address, token_id)) = is_approval_key {
+            return KeyType::Approval(nft_address, token_id);
+        }
+        if let Some((nft_address, token_id)) = is_current_owner_key {
+            return KeyType::CurrentOwner(nft_address, token_id);
+        }
+        if let Some((nft_address, token_id)) = is_past_owner_key {
+            return KeyType::PastOwners(nft_address, token_id);
+        }
+        KeyType::Unknown
+    }
+}