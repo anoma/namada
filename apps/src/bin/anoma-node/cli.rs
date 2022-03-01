46e0eb0422927069e0c3caf120945d2fde67c8a4
R2D2 <jacob.turner870@gmail.com>
2022-03-01 15:56:10 +0100

    [fix]: Changed how the ledger is passed in

--- apps/src/bin/anoma-node/cli.rs
+++ apps/src/bin/anoma-node/cli.rs
@@ -6,7 +6,9 @@ use eyre::{Context, Result};
 
 pub fn main() -> Result<()> {
     let (cmd, mut ctx) = cli::anoma_node_cli();
-    ctx.config.ledger.tendermint.tendermint_mode = ctx.global_args.mode.clone();
+    if let Some( mode )= ctx.global_args.mode.clone() {
+        ctx.config.ledger.tendermint.tendermint_mode = mode;
+    }
     match cmd {
         cmds::AnomaNode::Ledger(sub) => match sub {
             cmds::Ledger::Run(_) => {
