389badc2bef33af5e285ece9a9764bc54697d79b
Tomáš Zemanovič <tomas@heliax.dev>
2022-03-22 18:18:08 +0100

    tests/e2e: init-network for tests with `--allow-duplicate-ip`

--- tests/src/e2e/setup.rs
+++ tests/src/e2e/setup.rs
@@ -148,6 +148,7 @@ pub fn network(
         "e2e-test",
         "--localhost",
         "--dont-archive",
+        "--allow-duplicate-ip",
         "--wasm-checksums-path",
         &checksums_path,
     ];
