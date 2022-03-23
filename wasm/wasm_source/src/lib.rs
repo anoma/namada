30680245a02d111a9b790589eeca26cf503ff6df
Marco Granelli <marco.granelli@ymail.com>
2022-03-23 12:57:50 +0100

    Fixes wrong features

--- wasm/wasm_source/src/lib.rs
+++ wasm/wasm_source/src/lib.rs
@@ -6,9 +6,9 @@ pub mod tx_from_intent;
 pub mod tx_ibc;
 #[cfg(feature = "tx_init_account")]
 pub mod tx_init_account;
-#[cfg(feature = "tx_init_nft")]
-pub mod tx_init_proposal;
 #[cfg(feature = "tx_init_proposal")]
+pub mod tx_init_proposal;
+#[cfg(feature = "tx_init_nft")]
 pub mod tx_init_nft;
 #[cfg(feature = "tx_init_validator")]
 pub mod tx_init_validator;
