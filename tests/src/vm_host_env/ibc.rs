fb6de4e945cafd1b20c6b27716cd6a59d3d46c88
yito88 <yuji@phact-columba.com>
2022-05-13 17:38:48 +0200

    fix coding for ack and ftt

--- tests/src/vm_host_env/ibc.rs
+++ tests/src/vm_host_env/ibc.rs
@@ -64,7 +64,7 @@ use namada::ledger::tx_env::TxEnv;
 use namada::proto::Tx;
 use namada::tendermint_proto::Protobuf;
 use namada::types::address::{self, Address, InternalAddress};
-use namada::types::ibc::data::FungibleTokenPacketData;
+use namada::types::ibc::data::{FungibleTokenPacketData, PacketAck};
 use namada::types::storage::{self, BlockHash, BlockHeight};
 use namada::types::token::{self, Amount};
 use namada::vm::{wasm, WasmCacheRwAccess};
@@ -584,7 +584,7 @@ pub fn msg_packet_recv(packet: Packet) -> MsgRecvPacket {
 pub fn msg_packet_ack(packet: Packet) -> MsgAcknowledgement {
     MsgAcknowledgement {
         packet,
-        acknowledgement: vec![0].into(),
+        acknowledgement: PacketAck::result_success().encode_to_vec().into(),
         proofs: dummy_proofs(),
         signer: Signer::new("test"),
     }
@@ -601,7 +601,7 @@ pub fn received_packet(
     let timeout_timestamp =
         (Timestamp::now() + Duration::from_secs(100)).unwrap();
     let data = FungibleTokenPacketData {
-        denomination: token,
+        denom: token,
         amount: 100u64.to_string(),
         sender: address::testing::gen_established_address().to_string(),
         receiver: receiver.to_string(),
