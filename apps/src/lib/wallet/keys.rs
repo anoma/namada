5ca829cd191eac895056cd6759716854094b91c7
leontiad <leontiad@gmail.com>
2022-06-27 10:00:14 +0200

    wallet: increase keys encryption password iterations

--- apps/src/lib/wallet/keys.rs
+++ apps/src/lib/wallet/keys.rs
@@ -235,6 +235,6 @@ fn encryption_salt() -> kdf::Salt {
 /// Make encryption secret key from a password.
 fn encryption_key(salt: &kdf::Salt, password: String) -> kdf::SecretKey {
     kdf::Password::from_slice(password.as_bytes())
-        .and_then(|password| kdf::derive_key(&password, salt, 3, 1 << 16, 32))
+        .and_then(|password| kdf::derive_key(&password, salt, 3, 1 << 17, 32))
         .expect("Generation of encryption secret key shouldn't fail")
 }
