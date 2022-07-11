796ade232b7a48ef6aa0b35c0a0a80192350427c
James Hiew <james@chainfork.com>
2022-07-11 12:15:42 +0100

    Fixing things to compile

--- apps/src/lib/node/ledger/protocol/transactions/ethereum_events.rs
+++ apps/src/lib/node/ledger/protocol/transactions/ethereum_events.rs
@@ -1,13 +1,20 @@
 use anoma::types::address::Address;
-use anoma::types::ethereum_events::{EthereumEvent, MultiSignedEthEvent};
+use anoma::types::ethereum_events::vote_extensions::{
+    FractionalVotingPower, MultiSignedEthEvent,
+};
+use anoma::types::ethereum_events::EthereumEvent;
 use borsh::{BorshDeserialize, BorshSerialize};
 use num_rational::Ratio;
+
+fn threshold() -> Ratio<u64> {
+    Ratio::new(2, 3)
+}
 
 #[derive(Debug, PartialEq, Eq, BorshSerialize, BorshDeserialize)]
 pub(crate) struct EthMsg {
     body: EthereumEvent,
     seen_by: Vec<Address>,
-    voting_power: (u64, u64),
+    voting_power: FractionalVotingPower,
     seen: bool,
 }
 
@@ -16,22 +23,20 @@ pub(crate) fn calculate_eth_msgs_state(
 ) -> Vec<EthMsg> {
     let mut eth_msgs = Vec::with_capacity(multisigneds.len());
     for multisigned in multisigneds {
-        let body = multisigned.event.data;
-        let mut total_voting_power = 0;
+        let (body, _) = multisigned.event.data;
+        let mut total_voting_power = FractionalVotingPower::zero();
         let mut seen_by = vec![];
         for (signer, voting_power) in multisigned.signers {
             seen_by.push(signer);
-            total_voting_power += voting_power; // TODO: overflow checks?
+            total_voting_power =
+                (*voting_power + *total_voting_power).try_into().unwrap();
         }
-        const DENOMINATOR: u64 = 1000; // TODO: what is the denominator?
 
-        let voting_power = (total_voting_power, DENOMINATOR);
-        let seen =
-            Ratio::new(voting_power.0, voting_power.1) > Ratio::new(2, 3);
+        let seen = *total_voting_power > threshold();
         eth_msgs.push(EthMsg {
             body,
             seen_by,
-            voting_power,
+            voting_power: total_voting_power,
             seen,
         });
     }
@@ -42,7 +47,11 @@ pub(crate) fn calculate_eth_msgs_state(
 mod test {
     use anoma::proto::MultiSigned;
     use anoma::types::address;
-    use anoma::types::ethereum_events::{EthereumEvent, MultiSignedEthEvent};
+    use anoma::types::ethereum_events::vote_extensions::{
+        FractionalVotingPower, MultiSignedEthEvent,
+    };
+    use anoma::types::ethereum_events::EthereumEvent;
+    use anoma::types::storage::BlockHeight;
 
     use super::calculate_eth_msgs_state;
     use crate::node::ledger::protocol::transactions::ethereum_events::EthMsg;
@@ -50,20 +59,32 @@ mod test {
     #[test]
     fn test_calculate_eth_msgs_state() {
         let est1 = address::testing::established_address_1();
-        let event = EthereumEvent::TransfersToNamada(vec![]);
+        let event = EthereumEvent::TransfersToNamada {
+            nonce: 0.into(),
+            transfers: vec![],
+        };
         assert_eq!(calculate_eth_msgs_state(vec![]), vec![]);
         assert_eq!(
             calculate_eth_msgs_state(vec![MultiSignedEthEvent {
-                signers: vec![(est1.clone(), 1)],
+                signers: vec![(
+                    est1.clone(),
+                    FractionalVotingPower::new(1, 3).unwrap()
+                )],
                 event: MultiSigned {
-                    data: EthereumEvent::TransfersToNamada(vec![]),
+                    data: (
+                        EthereumEvent::TransfersToNamada {
+                            nonce: 0.into(),
+                            transfers: vec![]
+                        },
+                        BlockHeight(100)
+                    ),
                     sigs: vec![]
                 }
             }]),
             vec![EthMsg {
                 body: event,
                 seen_by: vec![est1.clone()],
-                voting_power: (1, 1000),
+                voting_power: FractionalVotingPower::new(1, 3).unwrap(),
                 seen: false,
             }]
         );
--- shared/src/types/ethereum_events.rs
+++ shared/src/types/ethereum_events.rs
@@ -1,55 +1,172 @@
-//! Types to do with interfacing with the Ethereum blockchain
-use std::fmt::Debug;
+//! Types representing data intended for Anoma via Ethereum events
 
 use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
+use ethabi::Uint as ethUint;
 
-use crate::proto::MultiSigned;
 use crate::types::address::Address;
+use crate::types::hash::Hash;
 use crate::types::token::Amount;
 
-/// An Ethereum event to be processed by the Anoma ledger
+/// Anoma native type to replace the ethabi::Uint type
 #[derive(
-    Debug, PartialEq, Eq, Clone, BorshSerialize, BorshDeserialize, BorshSchema,
+    Clone, Debug, PartialEq, Eq, BorshSerialize, BorshDeserialize, BorshSchema,
 )]
-pub enum EthereumEvent {
-    /// Event transferring batches of ether from Ethereum to wrapped ETH on
-    /// Anoma
-    TransfersToNamada(Vec<TransferToNamada>),
+pub struct Uint(pub [u64; 4]);
+
+impl From<ethUint> for Uint {
+    fn from(value: ethUint) -> Self {
+        Self(value.0)
+    }
+}
+
+impl From<Uint> for ethUint {
+    fn from(value: Uint) -> Self {
+        Self(value.0)
+    }
+}
+
+impl From<u64> for Uint {
+    fn from(value: u64) -> Self {
+        ethUint::from(value).into()
+    }
 }
 
 /// Representation of address on Ethereum
 #[derive(
-    Clone, PartialEq, Eq, Debug, BorshSerialize, BorshDeserialize, BorshSchema,
+    Clone, Debug, PartialEq, Eq, BorshSerialize, BorshDeserialize, BorshSchema,
 )]
 pub struct EthAddress(pub [u8; 20]);
 
+/// A Keccak hash
+#[derive(
+    Clone, Debug, PartialEq, Eq, BorshSerialize, BorshDeserialize, BorshSchema,
+)]
+pub struct KeccakHash(pub [u8; 32]);
+
+/// An Ethereum event to be processed by the Anoma ledger
+#[derive(
+    Clone, Debug, PartialEq, Eq, BorshSerialize, BorshDeserialize, BorshSchema,
+)]
+pub enum EthereumEvent {
+    /// Event transferring batches of ether or Ethereum based ERC20 tokens
+    /// from Ethereum to wrapped assets on Anoma
+    TransfersToNamada {
+        /// Monotonically increasing nonce
+        #[allow(dead_code)]
+        nonce: Uint,
+        /// The batch of transfers
+        #[allow(dead_code)]
+        transfers: Vec<TransferToNamada>,
+    },
+    /// A confirmation event that a batch of transfers have been made
+    /// from Anoma to Ethereum
+    TransfersToEthereum {
+        /// Monotonically increasing nonce
+        #[allow(dead_code)]
+        nonce: Uint,
+        /// The batch of transfers
+        #[allow(dead_code)]
+        transfers: Vec<TransferToEthereum>,
+    },
+    /// Event indication that the validator set has been updated
+    /// in the governance contract
+    ValidatorSetUpdate {
+        /// Monotonically increasing nonce
+        #[allow(dead_code)]
+        nonce: Uint,
+        /// Hash of the validators in the bridge contract
+        #[allow(dead_code)]
+        bridge_validator_hash: KeccakHash,
+        /// Hash of the validators in the governance contract
+        #[allow(dead_code)]
+        governance_validator_hash: KeccakHash,
+    },
+    /// Event indication that a new smart contract has been
+    /// deployed
+    NewContract {
+        /// Name of the contract
+        #[allow(dead_code)]
+        name: String,
+        /// Address of the contract on Ethereum
+        #[allow(dead_code)]
+        address: EthAddress,
+    },
+    /// Event indicating that a smart contract has been updated
+    UpgradedContract {
+        /// Name of the contract
+        #[allow(dead_code)]
+        name: String,
+        /// Address of the contract on Ethereum
+        #[allow(dead_code)]
+        address: EthAddress,
+    },
+    /// Event indication a new Ethereum based token has been whitelisted for
+    /// transfer across the bridge
+    UpdateBridgeWhitelist {
+        /// Monotonically increasing nonce
+        #[allow(dead_code)]
+        nonce: Uint,
+        /// Tokens to be allowed to be transferred across the bridge
+        #[allow(dead_code)]
+        whitelist: Vec<TokenWhitelist>,
+    },
+}
+
+impl EthereumEvent {
+    /// SHA256 of the Borsh serialization of the [`EthereumEvent`].
+    #[allow(dead_code)]
+    fn hash(&self) -> Result<Hash, std::io::Error> {
+        let bytes = self.try_to_vec()?;
+        Ok(Hash::sha256(&bytes))
+    }
+}
+
 /// An event transferring some kind of value from Ethereum to Anoma
 #[derive(
-    Debug, PartialEq, Eq, Clone, BorshSerialize, BorshDeserialize, BorshSchema,
+    Clone, Debug, PartialEq, Eq, BorshSerialize, BorshDeserialize, BorshSchema,
 )]
 pub struct TransferToNamada {
-    /// Quantity of ether in the transfer
+    /// Quantity of the ERC20 token in the transfer
     pub amount: Amount,
-    /// Address on Ethereum of the asset
-    pub asset: EthereumAsset,
-    /// The Namada address receiving wrapped assets on Anoma
+    /// Address of the smart contract issuing the token
+    pub asset: EthAddress,
+    /// The address receiving wrapped assets on Anoma
     pub receiver: Address,
 }
 
-/// Represents Ethereum assets on the Ethereum blockchain
+/// An event transferring some kind of value from Anoma to Ethereum
 #[derive(
-    Debug, PartialEq, Eq, Clone, BorshSerialize, BorshDeserialize, BorshSchema,
+    Clone, Debug, PartialEq, Eq, BorshSerialize, BorshDeserialize, BorshSchema,
 )]
-pub enum EthereumAsset {
-    /// Native ETH
-    Eth,
-    /// An ERC20 token and the address of its contract
-    ERC20(EthAddress),
+pub struct TransferToEthereum {
+    /// Quantity of wrapped Asset in the transfer
+    pub amount: Amount,
+    /// Address of the smart contract issuing the token
+    pub asset: EthAddress,
+    /// The address receiving assets on Ethereum
+    pub receiver: EthAddress,
+}
+
+/// struct for whitelisting a token from Ethereum.
+/// Includes the address of issuing contract and
+/// a cap on the max amount of this token allowed to be
+/// held by the bridge.
+#[derive(
+    Clone, Debug, PartialEq, Eq, BorshSerialize, BorshDeserialize, BorshSchema,
+)]
+#[allow(dead_code)]
+pub struct TokenWhitelist {
+    /// Address of Ethereum smart contract issuing token
+    pub token: EthAddress,
+    /// Maximum amount of token allowed on the bridge
+    pub cap: Amount,
 }
 
 /// Contains types necessary for processing Ethereum events
 /// in vote extensions
 pub mod vote_extensions {
+    use std::ops::Deref;
+
     use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
     use eyre::{eyre, Result};
     use num_rational::Ratio;
@@ -79,11 +196,33 @@ pub mod vote_extensions {
             }
             Ok(Self(ratio))
         }
+
+        /// Convenience function for returning zero [`FractionalVotingPower`]
+        pub fn zero() -> Self {
+            Self(Ratio::new(0, 1))
+        }
+    }
+
+    impl Deref for FractionalVotingPower {
+        type Target = Ratio<u64>;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
     }
 
     impl From<&FractionalVotingPower> for (u64, u64) {
         fn from(ratio: &FractionalVotingPower) -> Self {
             (ratio.0.numer().to_owned(), ratio.0.denom().to_owned())
+        }
+    }
+
+    impl TryFrom<Ratio<u64>> for FractionalVotingPower {
+        type Error = eyre::Report;
+
+        fn try_from(value: Ratio<u64>) -> Result<Self, Self::Error> {
+            // TODO: check ratio is > 0 and <= 1
+            Ok(Self(value))
         }
     }
 
@@ -135,3 +274,62 @@ pub mod vote_extensions {
         /// Events as signed by validators
         pub event: MultiSigned<(EthereumEvent, BlockHeight)>,
     }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use crate::types::ethereum_events::Uint;
+        use crate::types::hash::Hash;
+
+        /// Test the hashing of an Ethereum event
+        #[test]
+        fn test_ethereum_event_hash() {
+            let nonce = Uint::from(123u64);
+            let event = EthereumEvent::TransfersToNamada {
+                nonce,
+                transfers: vec![],
+            };
+            let hash = event.hash().unwrap();
+
+            assert_eq!(
+                hash,
+                Hash([
+                    94, 131, 116, 129, 41, 204, 178, 144, 24, 8, 185, 16, 103,
+                    236, 209, 191, 20, 89, 145, 17, 41, 233, 31, 98, 185, 6,
+                    217, 204, 80, 38, 224, 23
+                ])
+            );
+        }
+
+        /// This test is ultimately just exercising the underlying
+        /// library we use for fractions, we want to make sure
+        /// operators work as expected with our FractionalVotingPower
+        /// type itself
+        #[test]
+        fn test_fractional_voting_power_ord_eq() {
+            assert!(
+                FractionalVotingPower::new(2, 3).unwrap()
+                    > FractionalVotingPower::new(1, 4).unwrap()
+            );
+            assert!(
+                FractionalVotingPower::new(1, 3).unwrap()
+                    > FractionalVotingPower::new(1, 4).unwrap()
+            );
+            assert!(
+                FractionalVotingPower::new(1, 3).unwrap()
+                    == FractionalVotingPower::new(2, 6).unwrap()
+            );
+        }
+
+        /// Test error handling on the FractionalVotingPower type
+        #[test]
+        fn test_fractional_voting_power_valid_fractions() {
+            assert!(FractionalVotingPower::new(0, 0).is_err());
+            assert!(FractionalVotingPower::new(1, 0).is_err());
+            assert!(FractionalVotingPower::new(0, 1).is_ok());
+            assert!(FractionalVotingPower::new(1, 1).is_ok());
+            assert!(FractionalVotingPower::new(1, 2).is_ok());
+            assert!(FractionalVotingPower::new(3, 2).is_err());
+        }
+    }
+}
