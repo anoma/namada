aca9665b7dd3d5780c8dde7181fef11dfe8671db
James Hiew <james@chainfork.com>
2022-07-14 18:10:54 +0100

    Deserialize tx_data

--- vm_env/src/eth_bridge_tx.rs
+++ vm_env/src/eth_bridge_tx.rs
@@ -1,5 +1,9 @@
 //! code that should be executed within a transaction
+use std::error::Error;
+
 use anoma::ledger::eth_bridge::storage;
+use anoma::proto::Tx;
+use borsh::BorshDeserialize;
 
 use crate::imports::tx::log_string;
 
@@ -10,10 +14,17 @@ fn log(msg: &str) {
 }
 
 pub fn apply(tx_data: Vec<u8>) {
-    // tx_data shouldn't be signed, as any full node should be able to apply
-    // this transaction to their storage out-of-band
+    if let Err(err) = apply_aux(tx_data) {
+        log(&format!("ERROR: {:?}", err));
+        panic!("{:?}", err)
+    }
+}
+
+pub fn apply_aux(tx_data: Vec<u8>) -> Result<(), Box<dyn Error>> {
     log(&format!("got data - {} bytes", tx_data.len()));
-    log(&format!("/eth_msgs key - {}", storage::eth_msgs_key()))
+    log(&format!("/eth_msgs key - {}", storage::eth_msgs_key()));
+    Tx::try_from_slice(&tx_data)?;
+    Ok(())
 
     // TODO: extract Vec<EthMsgDiffs>
     // TODO: look at /eth_msgs storage, calculate new state
