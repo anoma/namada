c1e9c61c9009e612858d3b7fd9e8463cddd492c7
James Hiew <james@chainfork.com>
2022-07-22 12:05:04 +0100

    VoteExtension: comment validator_addr field with TODO

--- shared/src/types/ethereum_events/vote_extensions.rs
+++ shared/src/types/ethereum_events/vote_extensions.rs
@@ -19,7 +19,7 @@ use crate::types::storage::BlockHeight;
 pub struct VoteExtension {
     /// The block height for which this [`VoteExtension`] was made.
     pub block_height: BlockHeight,
-    /// The validator's address is temporarily being included
+    /// TODO: the validator's address is temporarily being included
     /// until we're able to map a Tendermint address to a validator
     /// address (see https://github.com/anoma/namada/issues/200)
     pub validator_addr: Address,
