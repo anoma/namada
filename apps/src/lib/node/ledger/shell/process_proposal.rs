59387b18bdc847f0009704a4847556721c52ad51
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-07-27 15:19:03 +0100

    Test if we reject proposals with invalid validators

--- apps/src/lib/node/ledger/shell/process_proposal.rs
+++ apps/src/lib/node/ledger/shell/process_proposal.rs
@@ -563,6 +563,70 @@ mod test_process_proposal {
         );
     }
 
+    /// Test that if a proposal contains vote extensions with
+    /// invalid validators, we reject it.
+    #[test]
+    fn test_drop_vext_digest_with_invalid_validators() {
+        const LAST_HEIGHT: BlockHeight = BlockHeight(2);
+        let (mut shell, _, _) = test_utils::setup();
+        shell.storage.last_height = LAST_HEIGHT;
+        let (addr, protocol_key) = {
+            let bertha_key = wallet::defaults::bertha_keypair();
+            let bertha_addr = wallet::defaults::bertha_address();
+            (bertha_addr, bertha_key)
+        };
+        let vote_extension_digest = {
+            let event = EthereumEvent::TransfersToNamada {
+                nonce: 1u64.into(),
+                transfers: vec![],
+            };
+            let ext = {
+                let ext = VoteExtension {
+                    validator_addr: addr.clone(),
+                    block_height: LAST_HEIGHT,
+                    ethereum_events: vec![event.clone()],
+                }
+                .sign(&protocol_key);
+                assert!(ext.verify(&protocol_key.ref_to()).is_ok());
+                ext
+            };
+            VoteExtensionDigest {
+                signatures: {
+                    let mut s = HashMap::new();
+                    s.insert(addr.clone(), ext.sig);
+                    s
+                },
+                events: vec![MultiSignedEthEvent {
+                    event,
+                    signers: {
+                        let mut s = HashSet::new();
+                        s.insert(addr);
+                        s
+                    },
+                }],
+            }
+        };
+        let tx = ProtocolTxType::EthereumEvents(vote_extension_digest)
+            .sign(&protocol_key)
+            .to_bytes();
+        let request = ProcessProposal { txs: vec![tx] };
+        let response = if let Err(TestError::RejectProposal(resp)) =
+            shell.process_proposal(request)
+        {
+            if let [resp] = resp.as_slice() {
+                resp.clone()
+            } else {
+                panic!("Test failed")
+            }
+        } else {
+            panic!("Test failed")
+        };
+        assert_eq!(
+            response.result.code,
+            u32::from(ErrorCodes::InvalidVoteExntension)
+        );
+    }
+
     /// Test that if a wrapper tx is not signed, it is rejected
     /// by [`process_proposal`].
     #[test]
