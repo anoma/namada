0fd17fca276b0e5445e6c519a5f385721e86aaab
Gianmarco Fraccaroli <>
2022-07-28 15:09:24 +0200

    [ci] minor improvements ci scripts

--- .github/workflows/scripts/update-wasm.py
+++ .github/workflows/scripts/update-wasm.py
@@ -55,34 +55,36 @@ comment_event = loads(environ['GITHUB_CONTEXT'])
 pr_comment = comment_event['event']['comment']['body']
 pr_number = comment_event['event']['issue']['number']
 
-if pr_comment == PR_COMMENT:
-    pr_info = api.pulls.get(pr_number)
-    head_sha = pr_info['head']['sha']
+if pr_comment != PR_COMMENT:
+    exit(0)
 
-    artifacts = api.actions.list_artifacts_for_repo(per_page=ARTIFACT_PER_PAGE)
+pr_info = api.pulls.get(pr_number)
+head_sha = pr_info['head']['sha']
 
-    for artifact in artifacts['artifacts']:
-        if 'wasm' in artifact['name'] and artifact['workflow_run']['head_sha'] == head_sha and not artifact['expired']:
-            artifact_download_url = artifact['archive_download_url']
+artifacts = api.actions.list_artifacts_for_repo(per_page=ARTIFACT_PER_PAGE)
 
-            log("Downloading artifacts...")
-            curl_command_outcome = download_artifact(
-                artifact_download_url, TMP_DIRECTORY, TOKEN)
-            if curl_command_outcome.returncode != 0:
-                exit(1)
+for artifact in artifacts['artifacts']:
+    if 'wasm' in artifact['name'] and artifact['workflow_run']['head_sha'] == head_sha and not artifact['expired']:
+        artifact_download_url = artifact['archive_download_url']
 
-            unzip_command_outcome = unzip(TMP_DIRECTORY)
-            if unzip_command_outcome.returncode != 0:
-                exit(1)
+        log("Downloading artifacts...")
+        curl_command_outcome = download_artifact(
+            artifact_download_url, TMP_DIRECTORY, TOKEN)
+        if curl_command_outcome.returncode != 0:
+            exit(1)
 
-            log("Replacing checksums.json...")
-            replace_command_outcome = replace_checksums(TMP_DIRECTORY)
-            if replace_command_outcome.returncode != 0:
-                exit(1)
+        unzip_command_outcome = unzip(TMP_DIRECTORY)
+        if unzip_command_outcome.returncode != 0:
+            exit(1)
 
-            log("Pushing new checksums.json...")
-            commit_and_push_command_outcome = commit_and_push()
-            if commit_and_push_command_outcome.returncode != 0:
-                exit(1)
+        log("Replacing checksums.json...")
+        replace_command_outcome = replace_checksums(TMP_DIRECTORY)
+        if replace_command_outcome.returncode != 0:
+            exit(1)
 
-            log("Done!")
+        log("Pushing new checksums.json...")
+        commit_and_push_command_outcome = commit_and_push()
+        if commit_and_push_command_outcome.returncode != 0:
+            exit(1)
+
+        log("Done!")
