a3d53b671910d05253b1610a6abdda2808279ef0
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-08-02 15:03:01 +0100

    VoteExtension type TODO

--- shared/src/types/vote_extensions.rs
+++ shared/src/types/vote_extensions.rs
@@ -1,3 +1,12 @@
 //! This module contains types necessary for processing vote extensions.
 
 pub mod ethereum_events;
+
+// TODO: add a `VoteExtension` type
+//
+// ```ignore
+// pub struct VoteExtension {
+//     ethereum_events: SignedEthEventsVext,
+//     validator_set_update: Option<SignedValidatorSetUpdateVext>,
+// }
+// ```
