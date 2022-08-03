8eb6e03be8af2bd1a097b0b09817e67fef37f504
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-08-03 16:45:38 +0100

    Add a TODO

--- shared/src/types/vote_extensions/validator_set_update.rs
+++ shared/src/types/vote_extensions/validator_set_update.rs
@@ -1,2 +1,16 @@
 //! Contains types necessary for processing validator set updates
 //! in vote extensions.
+
+// TODO: finish signed vote extension
+// ```ignore
+// struct Vext {
+//     ...?
+// }
+// struct SignedVext {
+//     signature: EthereumSignature,
+//     data: Vext,
+// }
+// ```
+// we derive a keccak hash from the `Vext` data
+// in `SignedVext`, which we can sign with an
+// Ethereum key. that is the content of `signature`
