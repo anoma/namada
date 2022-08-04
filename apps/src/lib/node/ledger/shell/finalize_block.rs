IdaTucker/anoma

Now record which transactions were accepted in a block using a bit vector.

apps/src/lib/node/ledger/shell/finalize_block.rs
@@ -9,7 +9,7 @@ use anoma::ledger::storage::types::encode;
 use anoma::ledger::treasury::ADDRESS as treasury_address;
 use anoma::types::address::{xan as m1t, Address};
 use anoma::types::governance::TallyResult;
-use anoma::types::storage::{BlockHash, Epoch, Header};
+use anoma::types::storage::{BlockHash, BlockResults, Epoch, Header, TxIndex};
 #[cfg(not(feature = "ABCI"))]
 use tendermint_proto::abci::Misbehavior as Evidence;
 #[cfg(not(feature = "ABCI"))]
@@ -227,6 +227,8 @@ where
             }
         }
 
+        // Tracks the accepted transactions
+        self.storage.block.results = BlockResults::with_len(req.txs.len());
         for (tx_index, processed_tx) in req.txs.iter().enumerate() {
             let tx = if let Ok(tx) = Tx::try_from(processed_tx.tx.as_ref()) {
                 tx
@@ -364,6 +366,7 @@ where
                         self.write_log.commit_tx();
                         if !tx_event.contains_key("code") {
                             tx_event["code"] = ErrorCodes::Ok.into();
+                            self.storage.block.results.accept(tx_index);
                         }
                         if let Some(ibc_event) = &result.ibc_event {
                             // Add the IBC event besides the tx_event

apps/src/lib/node/ledger/shell/mod.rs
@@ -671,7 +671,7 @@ mod test_utils {
     use anoma::types::chain::ChainId;
     use anoma::types::hash::Hash;
     use anoma::types::key::*;
-    use anoma::types::storage::{BlockHash, Epoch, Header};
+    use anoma::types::storage::{BlockHash, BlockResults, Epoch, Header};
     use anoma::types::transaction::Fee;
     use tempfile::tempdir;
     #[cfg(not(feature = "ABCI"))]
@@ -937,6 +937,7 @@ mod test_utils {
                 next_epoch_min_start_height: BlockHeight(3),
                 next_epoch_min_start_time: DateTimeUtc::now(),
                 address_gen: &address_gen,
+                results: &BlockResults::default(),
                 tx_queue: &shell.storage.tx_queue,
             })
             .expect("Test failed");

apps/src/lib/node/ledger/shell/queries.rs
@@ -7,7 +7,7 @@ use anoma::ledger::storage::types;
 use anoma::types::address::Address;
 use anoma::types::key;
 use anoma::types::key::dkg_session_keys::DkgPublicKey;
-use anoma::types::storage::{Key, PrefixValue};
+use anoma::types::storage::{BlockResults, Key, PrefixValue};
 use anoma::types::token::{self, Amount};
 use borsh::{BorshDeserialize, BorshSerialize};
 use ferveo_common::TendermintValidator;
@@ -64,6 +64,7 @@ where
                         ..Default::default()
                     }
                 }
+                Path::Results => self.read_results(),
                 Path::Conversion(asset_type) => {
                     self.read_conversion(&asset_type)
                 }
@@ -80,6 +81,28 @@ where
                 info: format!("RPC error: {}", err),
                 ..Default::default()
             },
+        }
+    }
+
+    /// Query to read block results from storage
+    pub fn read_results(&self) -> response::Query {
+        let (iter, _gas) = self.storage.iter_results();
+        let mut results = vec![
+            BlockResults::default();
+            self.storage.block.height.0 as usize + 1
+        ];
+        iter.for_each(|(key, value, _gas)| {
+            let key = key
+                .parse::<usize>()
+                .expect("expected integer for block height");
+            let value = BlockResults::try_from_slice(&value)
+                .expect("expected BlockResults bytes");
+            results[key] = value;
+        });
+        let value = anoma::ledger::storage::types::encode(&results);
+        response::Query {
+            value,
+            ..Default::default()
         }
     }
 

anoma/namada

apps/src/lib/node/ledger/shell/finalize_block.rs
@@ -9,7 +9,7 @@ use namada::ledger::storage::types::encode;
 use namada::ledger::treasury::ADDRESS as treasury_address;
 use namada::types::address::{xan as m1t, Address};
 use namada::types::governance::TallyResult;
-use namada::types::storage::{BlockHash, Epoch, Header};
+use namada::types::storage::{BlockHash, BlockResults, Epoch, Header, TxIndex};
 use tendermint_proto::abci::Misbehavior as Evidence;
 use tendermint_proto::crypto::PublicKey as TendermintPublicKey;
 
@@ -224,6 +224,8 @@ where
             }
         }
 
+        // Tracks the accepted transactions
+        self.storage.block.results = BlockResults::with_len(req.txs.len());
         for (tx_index, processed_tx) in req.txs.iter().enumerate() {
             let tx = if let Ok(tx) = Tx::try_from(processed_tx.tx.as_ref()) {
                 tx
@@ -357,6 +359,7 @@ where
                         self.write_log.commit_tx();
                         if !tx_event.contains_key("code") {
                             tx_event["code"] = ErrorCodes::Ok.into();
+                            self.storage.block.results.accept(tx_index);
                         }
                         if let Some(ibc_event) = &result.ibc_event {
                             // Add the IBC event besides the tx_event

apps/src/lib/node/ledger/shell/mod.rs
@@ -642,7 +642,7 @@ mod test_utils {
     use namada::types::chain::ChainId;
     use namada::types::hash::Hash;
     use namada::types::key::*;
-    use namada::types::storage::{BlockHash, Epoch, Header};
+    use namada::types::storage::{BlockHash, BlockResults, Epoch, Header};
     use namada::types::transaction::Fee;
     use tempfile::tempdir;
     use tendermint_proto::abci::{RequestInitChain, RequestProcessProposal};
@@ -885,6 +885,7 @@ mod test_utils {
                 next_epoch_min_start_height: BlockHeight(3),
                 next_epoch_min_start_time: DateTimeUtc::now(),
                 address_gen: &address_gen,
+                results: &BlockResults::default(),
                 tx_queue: &shell.storage.tx_queue,
             })
             .expect("Test failed");

apps/src/lib/node/ledger/shell/queries.rs
@@ -10,7 +10,7 @@ use namada::ledger::storage::types;
 use namada::types::address::Address;
 use namada::types::key;
 use namada::types::key::dkg_session_keys::DkgPublicKey;
-use namada::types::storage::{Key, PrefixValue};
+use namada::types::storage::{BlockResults, Key, PrefixValue};
 use namada::types::token::{self, Amount};
 use tendermint_proto::crypto::{ProofOp, ProofOps};
 use tendermint_proto::google::protobuf;
@@ -55,6 +55,7 @@ where
                         ..Default::default()
                     }
                 }
+                Path::Results => self.read_results(),
                 Path::Conversion(asset_type) => {
                     self.read_conversion(&asset_type)
                 }
@@ -71,6 +72,28 @@ where
                 info: format!("RPC error: {}", err),
                 ..Default::default()
             },
+        }
+    }
+
+    /// Query to read block results from storage
+    pub fn read_results(&self) -> response::Query {
+        let (iter, _gas) = self.storage.iter_results();
+        let mut results = vec![
+            BlockResults::default();
+            self.storage.block.height.0 as usize + 1
+        ];
+        iter.for_each(|(key, value, _gas)| {
+            let key = key
+                .parse::<usize>()
+                .expect("expected integer for block height");
+            let value = BlockResults::try_from_slice(&value)
+                .expect("expected BlockResults bytes");
+            results[key] = value;
+        });
+        let value = namada::ledger::storage::types::encode(&results);
+        response::Query {
+            value,
+            ..Default::default()
         }
     }
 