c7f940fa7e79a7c374a021fd647690a68499f601
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-08-09 09:51:02 +0100

    Revert "Add VoteExtensionDigest::get_protocol_txs()"
    This reverts commit a5a2cdea6b6a73fb7e89b581b6e9f260dfaca6a4.

--- shared/src/types/vote_extensions.rs
+++ shared/src/types/vote_extensions.rs
@@ -4,7 +4,6 @@ pub mod ethereum_events;
 pub mod validator_set_update;
 
 use crate::proto::Signed;
-use crate::types::transaction::protocol::ProtocolTxType;
 
 /// This type represents the data we pass to the extension of
 /// a vote at the PreCommit phase of Tendermint.
@@ -19,7 +18,7 @@ pub struct VoteExtension {
 /// in [`VoteExtension`] instances.
 ///
 /// From a [`VoteExtensionDigest`] we yield two signed
-/// [`ProtocolTxType`] transactions:
+/// [`crate::types::transaction::protocol::ProtocolTxType`] transactions:
 ///   - A `ProtocolTxType::EthereumEvents` tx, and
 ///   - A `ProtocolTxType::ValidatorSetUpdate` tx
 pub struct VoteExtensionDigest {
@@ -27,18 +26,4 @@ pub struct VoteExtensionDigest {
     pub ethereum_events: ethereum_events::VextDigest,
     /// The digest of validator set updates vote extension signatures.
     pub validator_set_update: Option<validator_set_update::VextDigest>,
-}
-
-impl VoteExtensionDigest {
-    /// Yields an iterator over the [`ProtocolTxType`] transactions
-    /// in this [`VoteExtensionDigest`].
-    pub fn into_protocol_txs(self) -> impl Iterator<Item = ProtocolTxType> {
-        [
-            Some(ProtocolTxType::EthereumEvents(self.ethereum_events)),
-            self.validator_set_update
-                .map(ProtocolTxType::ValidatorSetUpdate),
-        ]
-        .into_iter()
-        .flat_map(|tx| tx)
-    }
 }
