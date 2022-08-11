anoma/anoma

shell: remove TM consensus evidence parameters

apps/src/lib/node/ledger/shell/mod.rs
@@ -29,7 +29,7 @@ use anoma::ledger::storage::write_log::WriteLog;
 use anoma::ledger::storage::{
     DBIter, Sha256Hasher, Storage, StorageHasher, DB,
 };
-use anoma::ledger::{ibc, parameters, pos};
+use anoma::ledger::{ibc, pos};
 use anoma::proto::{self, Tx};
 use anoma::types::chain::ChainId;
 use anoma::types::key::*;
@@ -54,10 +54,6 @@ use tendermint_proto::abci::{
 };
 #[cfg(not(feature = "ABCI"))]
 use tendermint_proto::crypto::public_key;
-#[cfg(not(feature = "ABCI"))]
-use tendermint_proto::types::ConsensusParams;
-#[cfg(feature = "ABCI")]
-use tendermint_proto_abci::abci::ConsensusParams;
 #[cfg(feature = "ABCI")]
 use tendermint_proto_abci::abci::{Evidence, EvidenceType, ValidatorUpdate};
 #[cfg(feature = "ABCI")]

anoma/namada

apps/src/lib/node/ledger/shell/mod.rs
@@ -29,7 +29,7 @@ use namada::ledger::storage::write_log::WriteLog;
 use namada::ledger::storage::{
     DBIter, Sha256Hasher, Storage, StorageHasher, DB,
 };
-use namada::ledger::{ibc, parameters, pos};
+use namada::ledger::{ibc, pos};
 use namada::proto::{self, Tx};
 use namada::types::chain::ChainId;
 use namada::types::key::*;
@@ -50,7 +50,6 @@ use tendermint_proto::abci::{
     RequestPrepareProposal, ValidatorUpdate,
 };
 use tendermint_proto::crypto::public_key;
-use tendermint_proto::types::ConsensusParams;
 use thiserror::Error;
 use tokio::sync::mpsc::UnboundedSender;
 use tower_abci::{request, response};