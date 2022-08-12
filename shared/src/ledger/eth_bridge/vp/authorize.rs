62f899bbf18df1af4e9c812fb9246cb604c3b3d8
James <james@chainfork.com>
2022-08-12 17:59:04 +0100

    Stub out test for is_authorized

--- shared/src/ledger/eth_bridge/vp/authorize.rs
+++ shared/src/ledger/eth_bridge/vp/authorize.rs
@@ -6,9 +6,28 @@ use super::store;
 use crate::types::address::Address;
 
 pub(super) fn is_authorized(
-    reader: impl store::Reader,
-    tx_data: &[u8],
-    owner: &Address,
+    _reader: impl store::Reader,
+    _tx_data: &[u8],
+    _owner: &Address,
 ) -> Result<bool> {
-    Ok(false)
+    // TODO: integrate with how `anomac transfer` transactions will work from <https://github.com/anoma/namada/pull/297>
+    Ok(true)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::types::address;
+
+    #[test]
+    fn test_is_authorized_established_address() -> Result<()> {
+        let reader = store::testing::FakeReader::default();
+        let tx_data = vec![];
+        let owner = address::testing::established_address_1();
+
+        let authorized = is_authorized(reader, &tx_data, &owner)?;
+
+        assert!(authorized);
+        Ok(())
+    }
 }
