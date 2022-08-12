64e1d4916e8d6e19b9dd7c1b5b59d19f1b2095f1
James <james@chainfork.com>
2022-08-12 12:35:36 +0100

    Add is_eth_bridge_key utility function

--- shared/src/ledger/eth_bridge/storage/mod.rs
+++ shared/src/ledger/eth_bridge/storage/mod.rs
@@ -9,3 +9,24 @@ pub mod wrapped_erc20s;
 pub fn prefix() -> Key {
     Key::from(ADDRESS.to_db_key())
 }
+
+/// Returns whether a key belongs to this account or not
+pub fn is_eth_bridge_key(key: &Key) -> bool {
+    key.segments[0] == ADDRESS.to_db_key()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_is_eth_bridge_key() {
+        let key = Key::from(super::ADDRESS.to_db_key());
+        assert!(is_eth_bridge_key(&key));
+
+        let key = Key::from(super::ADDRESS.to_db_key())
+            .push(&"arbitrary key segment".to_owned())
+            .expect("Could not set up test");
+        assert!(is_eth_bridge_key(&key));
+    }
+}
