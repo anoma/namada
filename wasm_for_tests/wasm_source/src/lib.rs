anoma/anoma

wasm: update for VM API changes

wasm_for_tests/wasm_source/src/lib.rs
@@ -1,66 +1,71 @@
 /// A tx that doesn't do anything.
 #[cfg(feature = "tx_no_op")]
 pub mod main {
-    use anoma_vm_env::tx_prelude::*;
+    use anoma_tx_prelude::*;
 
     #[transaction]
-    fn apply_tx(_tx_data: Vec<u8>) {}
+    fn apply_tx(_ctx: &mut Ctx, _tx_data: Vec<u8>) -> TxResult {
+        Ok(())
+    }
 }
 
 /// A tx that allocates a memory of size given from the `tx_data: usize`.
 #[cfg(feature = "tx_memory_limit")]
 pub mod main {
-    use anoma_vm_env::tx_prelude::*;
+    use anoma_tx_prelude::*;
 
     #[transaction]
-    fn apply_tx(tx_data: Vec<u8>) {
+    fn apply_tx(_ctx: &mut Ctx, tx_data: Vec<u8>) -> TxResult {
         let len = usize::try_from_slice(&tx_data[..]).unwrap();
         log_string(format!("allocate len {}", len));
         let bytes: Vec<u8> = vec![6_u8; len];
         // use the variable to prevent it from compiler optimizing it away
         log_string(format!("{:?}", &bytes[..8]));
+        Ok(())
     }
 }
 
 /// A tx to be used as proposal_code
 #[cfg(feature = "tx_proposal_code")]
 pub mod main {
-    use anoma_vm_env::tx_prelude::*;
+    use anoma_tx_prelude::*;
 
     #[transaction]
-    fn apply_tx(_tx_data: Vec<u8>) {
+    fn apply_tx(ctx: &mut Ctx, _tx_data: Vec<u8>) -> TxResult {
         // governance
-        let target_key = storage::get_min_proposal_grace_epoch_key();
-        write(&target_key.to_string(), 9_u64);
+        let target_key = gov_storage::get_min_proposal_grace_epoch_key();
+        ctx.write(&target_key, 9_u64)?;
 
         // treasury
         let target_key = treasury_storage::get_max_transferable_fund_key();
-        write(&target_key.to_string(), token::Amount::whole(20_000));
+        ctx.write(&target_key, token::Amount::whole(20_000))?;
 
         // parameters
         let target_key = parameters_storage::get_tx_whitelist_storage_key();
-        write(&target_key.to_string(), vec!["hash"]);
+        ctx.write(&target_key, vec!["hash"])?;
+        Ok(())
     }
 }
 
 /// A tx that attempts to read the given key from storage.
 #[cfg(feature = "tx_read_storage_key")]
 pub mod main {
-    use anoma_vm_env::tx_prelude::*;
+    use anoma_tx_prelude::*;
 
     #[transaction]
-    fn apply_tx(tx_data: Vec<u8>) {
+    fn apply_tx(ctx: &mut Ctx, tx_data: Vec<u8>) -> TxResult {
         // Allocates a memory of size given from the `tx_data (usize)`
-        let key = Key::try_from_slice(&tx_data[..]).unwrap();
+        let key = storage::Key::try_from_slice(&tx_data[..]).unwrap();
         log_string(format!("key {}", key));
-        let _result: Vec<u8> = read(key.to_string()).unwrap();
+        let _result: Vec<u8> = ctx.read(&key)?.unwrap();
+        Ok(())
     }
 }
 
 /// A tx that attempts to write arbitrary data to the given key
 #[cfg(feature = "tx_write_storage_key")]
 pub mod main {
-    use anoma_vm_env::tx_prelude::*;
+    use anoma_tx_prelude::*;
 
     const TX_NAME: &str = "tx_write";
 
@@ -81,7 +86,7 @@ pub mod main {
     }
 
     #[transaction]
-    fn apply_tx(tx_data: Vec<u8>) {
+    fn apply_tx(ctx: &mut Ctx, tx_data: Vec<u8>) -> TxResult {
         let signed = match SignedTxData::try_from_slice(&tx_data[..]) {
             Ok(signed) => {
                 log("got signed data");
@@ -100,15 +105,15 @@ pub mod main {
         };
         let key = match String::from_utf8(data) {
             Ok(key) => {
+                let key = storage::Key::parse(key).unwrap();
                 log(&format!("parsed key from data: {}", key));
                 key
             }
             Err(error) => fatal("getting key", error),
         };
-        let val: Option<Vec<u8>> = read(key.as_str());
+        let val: Option<String> = ctx.read(&key)?;
         match val {
             Some(val) => {
-                let val = String::from_utf8(val).unwrap();
                 log(&format!("preexisting val is {}", val));
             }
             None => {
@@ -119,7 +124,7 @@ pub mod main {
             "attempting to write new value {} to key {}",
             ARBITRARY_VALUE, key
         ));
-        write(key.as_str(), ARBITRARY_VALUE);
+        ctx.write(&key, ARBITRARY_VALUE)
     }
 }
 
@@ -128,10 +133,10 @@ pub mod main {
 /// token's VP.
 #[cfg(feature = "tx_mint_tokens")]
 pub mod main {
-    use anoma_vm_env::tx_prelude::*;
+    use anoma_tx_prelude::*;
 
     #[transaction]
-    fn apply_tx(tx_data: Vec<u8>) {
+    fn apply_tx(ctx: &mut Ctx, tx_data: Vec<u8>) -> TxResult {
         let signed = SignedTxData::try_from_slice(&tx_data[..]).unwrap();
         let transfer =
             token::Transfer::try_from_slice(&signed.data.unwrap()[..]).unwrap();
@@ -144,41 +149,43 @@ pub mod main {
         } = transfer;
         let target_key = token::balance_key(&token, &target);
         let mut target_bal: token::Amount =
-            read(&target_key.to_string()).unwrap_or_default();
+            ctx.read(&target_key)?.unwrap_or_default();
         target_bal.receive(&amount);
-        write(&target_key.to_string(), target_bal);
+        ctx.write(&target_key, target_bal)
     }
 }
 
 /// A VP that always returns `true`.
 #[cfg(feature = "vp_always_true")]
 pub mod main {
-    use anoma_vm_env::vp_prelude::*;
+    use anoma_vp_prelude::*;
 
     #[validity_predicate]
     fn validate_tx(
+        _ctx: &Ctx,
         _tx_data: Vec<u8>,
         _addr: Address,
         _keys_changed: BTreeSet<storage::Key>,
         _verifiers: BTreeSet<Address>,
-    ) -> bool {
-        true
+    ) -> VpResult {
+        accept()
     }
 }
 
 /// A VP that always returns `false`.
 #[cfg(feature = "vp_always_false")]
 pub mod main {
-    use anoma_vm_env::vp_prelude::*;
+    use anoma_vp_prelude::*;
 
     #[validity_predicate]
     fn validate_tx(
+        _ctx: &Ctx,
         _tx_data: Vec<u8>,
         _addr: Address,
         _keys_changed: BTreeSet<storage::Key>,
         _verifiers: BTreeSet<Address>,
-    ) -> bool {
-        false
+    ) -> VpResult {
+        reject()
     }
 }
 
@@ -186,19 +193,20 @@ pub mod main {
 /// of `eval`.
 #[cfg(feature = "vp_eval")]
 pub mod main {
-    use anoma_vm_env::vp_prelude::*;
+    use anoma_vp_prelude::*;
 
     #[validity_predicate]
     fn validate_tx(
+        ctx: &Ctx,
         tx_data: Vec<u8>,
         _addr: Address,
         _keys_changed: BTreeSet<storage::Key>,
         _verifiers: BTreeSet<Address>,
-    ) -> bool {
+    ) -> VpResult {
         use validity_predicate::EvalVp;
         let EvalVp { vp_code, input }: EvalVp =
             EvalVp::try_from_slice(&tx_data[..]).unwrap();
-        eval(vp_code, input)
+        ctx.eval(vp_code, input)
     }
 }
 
@@ -206,21 +214,22 @@ pub mod main {
 // Returns `true`, if the allocation is within memory limits.
 #[cfg(feature = "vp_memory_limit")]
 pub mod main {
-    use anoma_vm_env::vp_prelude::*;
+    use anoma_vp_prelude::*;
 
     #[validity_predicate]
     fn validate_tx(
+        _ctx: &Ctx,
         tx_data: Vec<u8>,
         _addr: Address,
         _keys_changed: BTreeSet<storage::Key>,
         _verifiers: BTreeSet<Address>,
-    ) -> bool {
+    ) -> VpResult {
         let len = usize::try_from_slice(&tx_data[..]).unwrap();
         log_string(format!("allocate len {}", len));
         let bytes: Vec<u8> = vec![6_u8; len];
         // use the variable to prevent it from compiler optimizing it away
         log_string(format!("{:?}", &bytes[..8]));
-        true
+        accept()
     }
 }
 
@@ -228,19 +237,20 @@ pub mod main {
 /// execution). Returns `true`, if the allocation is within memory limits.
 #[cfg(feature = "vp_read_storage_key")]
 pub mod main {
-    use anoma_vm_env::vp_prelude::*;
+    use anoma_vp_prelude::*;
 
     #[validity_predicate]
     fn validate_tx(
+        ctx: &Ctx,
         tx_data: Vec<u8>,
         _addr: Address,
         _keys_changed: BTreeSet<storage::Key>,
         _verifiers: BTreeSet<Address>,
-    ) -> bool {
+    ) -> VpResult {
         // Allocates a memory of size given from the `tx_data (usize)`
-        let key = Key::try_from_slice(&tx_data[..]).unwrap();
+        let key = storage::Key::try_from_slice(&tx_data[..]).unwrap();
         log_string(format!("key {}", key));
-        let _result: Vec<u8> = read_pre(key.to_string()).unwrap();
-        true
+        let _result: Vec<u8> = ctx.read_pre(&key)?.unwrap();
+        accept()
     }
 }

anoma/namada

wasm_for_tests/wasm_source/src/lib.rs
@@ -1,66 +1,71 @@
 /// A tx that doesn't do anything.
 #[cfg(feature = "tx_no_op")]
 pub mod main {
-    use namada_vm_env::tx_prelude::*;
+    use namada_tx_prelude::*;
 
     #[transaction]
-    fn apply_tx(_tx_data: Vec<u8>) {}
+    fn apply_tx(_ctx: &mut Ctx, _tx_data: Vec<u8>) -> TxResult {
+        Ok(())
+    }
 }
 
 /// A tx that allocates a memory of size given from the `tx_data: usize`.
 #[cfg(feature = "tx_memory_limit")]
 pub mod main {
-    use namada_vm_env::tx_prelude::*;
+    use namada_tx_prelude::*;
 
     #[transaction]
-    fn apply_tx(tx_data: Vec<u8>) {
+    fn apply_tx(_ctx: &mut Ctx, tx_data: Vec<u8>) -> TxResult {
         let len = usize::try_from_slice(&tx_data[..]).unwrap();
         log_string(format!("allocate len {}", len));
         let bytes: Vec<u8> = vec![6_u8; len];
         // use the variable to prevent it from compiler optimizing it away
         log_string(format!("{:?}", &bytes[..8]));
+        Ok(())
     }
 }
 
 /// A tx to be used as proposal_code
 #[cfg(feature = "tx_proposal_code")]
 pub mod main {
-    use namada_vm_env::tx_prelude::*;
+    use namada_tx_prelude::*;
 
     #[transaction]
-    fn apply_tx(_tx_data: Vec<u8>) {
+    fn apply_tx(ctx: &mut Ctx, _tx_data: Vec<u8>) -> TxResult {
         // governance
-        let target_key = storage::get_min_proposal_grace_epoch_key();
-        write(&target_key.to_string(), 9_u64);
+        let target_key = gov_storage::get_min_proposal_grace_epoch_key();
+        ctx.write(&target_key, 9_u64)?;
 
         // treasury
         let target_key = treasury_storage::get_max_transferable_fund_key();
-        write(&target_key.to_string(), token::Amount::whole(20_000));
+        ctx.write(&target_key, token::Amount::whole(20_000))?;
 
         // parameters
         let target_key = parameters_storage::get_tx_whitelist_storage_key();
-        write(&target_key.to_string(), vec!["hash"]);
+        ctx.write(&target_key, vec!["hash"])?;
+        Ok(())
     }
 }
 
 /// A tx that attempts to read the given key from storage.
 #[cfg(feature = "tx_read_storage_key")]
 pub mod main {
-    use namada_vm_env::tx_prelude::*;
+    use namada_tx_prelude::*;
 
     #[transaction]
-    fn apply_tx(tx_data: Vec<u8>) {
+    fn apply_tx(ctx: &mut Ctx, tx_data: Vec<u8>) -> TxResult {
         // Allocates a memory of size given from the `tx_data (usize)`
-        let key = Key::try_from_slice(&tx_data[..]).unwrap();
+        let key = storage::Key::try_from_slice(&tx_data[..]).unwrap();
         log_string(format!("key {}", key));
-        let _result: Vec<u8> = read(key.to_string()).unwrap();
+        let _result: Vec<u8> = ctx.read(&key)?.unwrap();
+        Ok(())
     }
 }
 
 /// A tx that attempts to write arbitrary data to the given key
 #[cfg(feature = "tx_write_storage_key")]
 pub mod main {
-    use namada_vm_env::tx_prelude::*;
+    use namada_tx_prelude::*;
 
     const TX_NAME: &str = "tx_write";
 
@@ -81,7 +86,7 @@ pub mod main {
     }
 
     #[transaction]
-    fn apply_tx(tx_data: Vec<u8>) {
+    fn apply_tx(ctx: &mut Ctx, tx_data: Vec<u8>) -> TxResult {
         let signed = match SignedTxData::try_from_slice(&tx_data[..]) {
             Ok(signed) => {
                 log("got signed data");
@@ -100,15 +105,15 @@ pub mod main {
         };
         let key = match String::from_utf8(data) {
             Ok(key) => {
+                let key = storage::Key::parse(key).unwrap();
                 log(&format!("parsed key from data: {}", key));
                 key
             }
             Err(error) => fatal("getting key", error),
         };
-        let val: Option<Vec<u8>> = read(key.as_str());
+        let val: Option<String> = ctx.read(&key)?;
         match val {
             Some(val) => {
-                let val = String::from_utf8(val).unwrap();
                 log(&format!("preexisting val is {}", val));
             }
             None => {
@@ -119,7 +124,7 @@ pub mod main {
             "attempting to write new value {} to key {}",
             ARBITRARY_VALUE, key
         ));
-        write(key.as_str(), ARBITRARY_VALUE);
+        ctx.write(&key, ARBITRARY_VALUE)
     }
 }
 
@@ -128,10 +133,10 @@ pub mod main {
 /// token's VP.
 #[cfg(feature = "tx_mint_tokens")]
 pub mod main {
-    use namada_vm_env::tx_prelude::*;
+    use namada_tx_prelude::*;
 
     #[transaction]
-    fn apply_tx(tx_data: Vec<u8>) {
+    fn apply_tx(ctx: &mut Ctx, tx_data: Vec<u8>) -> TxResult {
         let signed = SignedTxData::try_from_slice(&tx_data[..]).unwrap();
         let transfer =
             token::Transfer::try_from_slice(&signed.data.unwrap()[..]).unwrap();
@@ -144,41 +149,43 @@ pub mod main {
         } = transfer;
         let target_key = token::balance_key(&token, &target);
         let mut target_bal: token::Amount =
-            read(&target_key.to_string()).unwrap_or_default();
+            ctx.read(&target_key)?.unwrap_or_default();
         target_bal.receive(&amount);
-        write(&target_key.to_string(), target_bal);
+        ctx.write(&target_key, target_bal)
     }
 }
 
 /// A VP that always returns `true`.
 #[cfg(feature = "vp_always_true")]
 pub mod main {
-    use namada_vm_env::vp_prelude::*;
+    use namada_vp_prelude::*;
 
     #[validity_predicate]
     fn validate_tx(
+        _ctx: &Ctx,
         _tx_data: Vec<u8>,
         _addr: Address,
         _keys_changed: BTreeSet<storage::Key>,
         _verifiers: BTreeSet<Address>,
-    ) -> bool {
-        true
+    ) -> VpResult {
+        accept()
     }
 }
 
 /// A VP that always returns `false`.
 #[cfg(feature = "vp_always_false")]
 pub mod main {
-    use namada_vm_env::vp_prelude::*;
+    use namada_vp_prelude::*;
 
     #[validity_predicate]
     fn validate_tx(
+        _ctx: &Ctx,
         _tx_data: Vec<u8>,
         _addr: Address,
         _keys_changed: BTreeSet<storage::Key>,
         _verifiers: BTreeSet<Address>,
-    ) -> bool {
-        false
+    ) -> VpResult {
+        reject()
     }
 }
 
@@ -186,19 +193,20 @@ pub mod main {
 /// of `eval`.
 #[cfg(feature = "vp_eval")]
 pub mod main {
-    use namada_vm_env::vp_prelude::*;
+    use namada_vp_prelude::*;
 
     #[validity_predicate]
     fn validate_tx(
+        ctx: &Ctx,
         tx_data: Vec<u8>,
         _addr: Address,
         _keys_changed: BTreeSet<storage::Key>,
         _verifiers: BTreeSet<Address>,
-    ) -> bool {
+    ) -> VpResult {
         use validity_predicate::EvalVp;
         let EvalVp { vp_code, input }: EvalVp =
             EvalVp::try_from_slice(&tx_data[..]).unwrap();
-        eval(vp_code, input)
+        ctx.eval(vp_code, input)
     }
 }
 
@@ -206,21 +214,22 @@ pub mod main {
 // Returns `true`, if the allocation is within memory limits.
 #[cfg(feature = "vp_memory_limit")]
 pub mod main {
-    use namada_vm_env::vp_prelude::*;
+    use namada_vp_prelude::*;
 
     #[validity_predicate]
     fn validate_tx(
+        _ctx: &Ctx,
         tx_data: Vec<u8>,
         _addr: Address,
         _keys_changed: BTreeSet<storage::Key>,
         _verifiers: BTreeSet<Address>,
-    ) -> bool {
+    ) -> VpResult {
         let len = usize::try_from_slice(&tx_data[..]).unwrap();
         log_string(format!("allocate len {}", len));
         let bytes: Vec<u8> = vec![6_u8; len];
         // use the variable to prevent it from compiler optimizing it away
         log_string(format!("{:?}", &bytes[..8]));
-        true
+        accept()
     }
 }
 
@@ -228,19 +237,20 @@ pub mod main {
 /// execution). Returns `true`, if the allocation is within memory limits.
 #[cfg(feature = "vp_read_storage_key")]
 pub mod main {
-    use namada_vm_env::vp_prelude::*;
+    use namada_vp_prelude::*;
 
     #[validity_predicate]
     fn validate_tx(
+        ctx: &Ctx,
         tx_data: Vec<u8>,
         _addr: Address,
         _keys_changed: BTreeSet<storage::Key>,
         _verifiers: BTreeSet<Address>,
-    ) -> bool {
+    ) -> VpResult {
         // Allocates a memory of size given from the `tx_data (usize)`
-        let key = Key::try_from_slice(&tx_data[..]).unwrap();
+        let key = storage::Key::try_from_slice(&tx_data[..]).unwrap();
         log_string(format!("key {}", key));
-        let _result: Vec<u8> = read_pre(key.to_string()).unwrap();
-        true
+        let _result: Vec<u8> = ctx.read_pre(&key)?.unwrap();
+        accept()
     }
 }