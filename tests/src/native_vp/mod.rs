anoma/anoma

tests: update for VM API changes

tests/src/native_vp/mod.rs
@@ -1,47 +1,38 @@
 mod pos;
 
+use std::collections::BTreeSet;
+
 use anoma::ledger::native_vp::{Ctx, NativeVp};
 use anoma::ledger::storage::mockdb::MockDB;
 use anoma::ledger::storage::Sha256Hasher;
-use anoma::vm::wasm::compilation_cache;
-use anoma::vm::wasm::compilation_cache::common::Cache;
-use anoma::vm::{wasm, WasmCacheRwAccess};
-use tempfile::TempDir;
+use anoma::types::address::Address;
+use anoma::types::storage;
+use anoma::vm::WasmCacheRwAccess;
 
 use crate::tx::TestTxEnv;
 
 type NativeVpCtx<'a> = Ctx<'a, MockDB, Sha256Hasher, WasmCacheRwAccess>;
-type VpCache = Cache<compilation_cache::vp::Name, WasmCacheRwAccess>;
 
 #[derive(Debug)]
 pub struct TestNativeVpEnv {
-    pub vp_cache_dir: TempDir,
-    pub vp_wasm_cache: VpCache,
     pub tx_env: TestTxEnv,
+    pub address: Address,
+    pub verifiers: BTreeSet<Address>,
+    pub keys_changed: BTreeSet<storage::Key>,
 }
 
 impl TestNativeVpEnv {
-    pub fn new(tx_env: TestTxEnv) -> Self {
-        let (vp_wasm_cache, vp_cache_dir) =
-            wasm::compilation_cache::common::testing::cache();
+    pub fn from_tx_env(tx_env: TestTxEnv, address: Address) -> Self {
+        // Find the tx verifiers and keys_changes the same way as protocol would
+        let verifiers = tx_env.get_verifiers();
+
+        let keys_changed = tx_env.all_touched_storage_keys();
 
         Self {
-            vp_cache_dir,
-            vp_wasm_cache,
+            address,
             tx_env,
-        }
-    }
-}
-
-impl Default for TestNativeVpEnv {
-    fn default() -> Self {
-        let (vp_wasm_cache, vp_cache_dir) =
-            wasm::compilation_cache::common::testing::cache();
-
-        Self {
-            vp_cache_dir,
-            vp_wasm_cache,
-            tx_env: TestTxEnv::default(),
+            verifiers,
+            keys_changed,
         }
     }
 }
@@ -51,20 +42,10 @@ impl TestNativeVpEnv {
     pub fn validate_tx<'a, T>(
         &'a self,
         init_native_vp: impl Fn(NativeVpCtx<'a>) -> T,
-        // The function is applied on the `tx_data` when called
-        mut apply_tx: impl FnMut(&[u8]),
     ) -> Result<bool, <T as NativeVp>::Error>
     where
         T: NativeVp,
     {
-        let tx_data = self.tx_env.tx.data.as_ref().cloned().unwrap_or_default();
-        apply_tx(&tx_data);
-
-        // Find the tx verifiers and keys_changes the same way as protocol would
-        let verifiers = self.tx_env.get_verifiers();
-
-        let keys_changed = self.tx_env.all_touched_storage_keys();
-
         let ctx = Ctx {
             iterators: Default::default(),
             gas_meter: Default::default(),
@@ -72,10 +53,13 @@ impl TestNativeVpEnv {
             write_log: &self.tx_env.write_log,
             tx: &self.tx_env.tx,
             vp_wasm_cache: self.tx_env.vp_wasm_cache.clone(),
+            address: &self.address,
+            keys_changed: &self.keys_changed,
+            verifiers: &self.verifiers,
         };
         let tx_data = self.tx_env.tx.data.as_ref().cloned().unwrap_or_default();
         let native_vp = init_native_vp(ctx);
 
-        native_vp.validate_tx(&tx_data, &keys_changed, &verifiers)
+        native_vp.validate_tx(&tx_data, &self.keys_changed, &self.verifiers)
     }
 }

tests/src/vm_host_env/tx.rs
@@ -14,17 +14,26 @@ use anoma::types::{key, token};
 use anoma::vm::prefix_iter::PrefixIterators;
 use anoma::vm::wasm::{self, TxCache, VpCache};
 use anoma::vm::{self, WasmCacheRwAccess};
-use anoma_vm_env::tx_prelude::BorshSerialize;
+use anoma_tx_prelude::{BorshSerialize, Ctx};
 use derivative::Derivative;
 use tempfile::TempDir;
+
+/// Tx execution context provides access to host env functions
+static mut CTX: Ctx = unsafe { Ctx::new() };
+
+/// Tx execution context provides access to host env functions
+pub fn ctx() -> &'static mut Ctx {
+    unsafe { &mut CTX }
+}
 
 /// This module combines the native host function implementations from
 /// `native_tx_host_env` with the functions exposed to the tx wasm
 /// that will call to the native functions, instead of interfacing via a
 /// wasm runtime. It can be used for host environment integration tests.
 pub mod tx_host_env {
-    pub use anoma_vm_env::tx_prelude::*;
+    pub use anoma_tx_prelude::*;
 
+    pub use super::ctx;
     pub use super::native_tx_host_env::*;
 }
 

tests/src/vm_host_env/vp.rs
@@ -10,17 +10,27 @@ use anoma::types::storage::{self, Key};
 use anoma::vm::prefix_iter::PrefixIterators;
 use anoma::vm::wasm::{self, VpCache};
 use anoma::vm::{self, WasmCacheRwAccess};
+use anoma_vp_prelude::Ctx;
 use tempfile::TempDir;
 
 use crate::tx::{tx_host_env, TestTxEnv};
+
+/// VP execution context provides access to host env functions
+pub static CTX: Ctx = unsafe { Ctx::new() };
+
+/// VP execution context provides access to host env functions
+pub fn ctx() -> &'static Ctx {
+    &CTX
+}
 
 /// This module combines the native host function implementations from
 /// `native_vp_host_env` with the functions exposed to the vp wasm
 /// that will call to the native functions, instead of interfacing via a
 /// wasm runtime. It can be used for host environment integration tests.
 pub mod vp_host_env {
-    pub use anoma_vm_env::vp_prelude::*;
+    pub use anoma_vp_prelude::*;
 
+    pub use super::ctx;
     pub use super::native_vp_host_env::*;
 }
 
@@ -160,7 +170,7 @@ mod native_vp_host_env {
     /// Initialize the VP host environment in [`ENV`] by running a transaction.
     /// The transaction is expected to modify the storage sub-space of the given
     /// address `addr` or to add it to the set of verifiers using
-    /// [`tx_host_env::insert_verifier`].
+    /// `ctx.insert_verifier`.
     pub fn init_from_tx(
         addr: Address,
         mut tx_env: TestTxEnv,

anoma/namada

tests/src/native_vp/mod.rs
@@ -1,47 +1,38 @@
 mod pos;
 
+use std::collections::BTreeSet;
+
 use namada::ledger::native_vp::{Ctx, NativeVp};
 use namada::ledger::storage::mockdb::MockDB;
 use namada::ledger::storage::Sha256Hasher;
-use namada::vm::wasm::compilation_cache;
-use namada::vm::wasm::compilation_cache::common::Cache;
-use namada::vm::{wasm, WasmCacheRwAccess};
-use tempfile::TempDir;
+use namada::types::address::Address;
+use namada::types::storage;
+use namada::vm::WasmCacheRwAccess;
 
 use crate::tx::TestTxEnv;
 
 type NativeVpCtx<'a> = Ctx<'a, MockDB, Sha256Hasher, WasmCacheRwAccess>;
-type VpCache = Cache<compilation_cache::vp::Name, WasmCacheRwAccess>;
 
 #[derive(Debug)]
 pub struct TestNativeVpEnv {
-    pub vp_cache_dir: TempDir,
-    pub vp_wasm_cache: VpCache,
     pub tx_env: TestTxEnv,
+    pub address: Address,
+    pub verifiers: BTreeSet<Address>,
+    pub keys_changed: BTreeSet<storage::Key>,
 }
 
 impl TestNativeVpEnv {
-    pub fn new(tx_env: TestTxEnv) -> Self {
-        let (vp_wasm_cache, vp_cache_dir) =
-            wasm::compilation_cache::common::testing::cache();
+    pub fn from_tx_env(tx_env: TestTxEnv, address: Address) -> Self {
+        // Find the tx verifiers and keys_changes the same way as protocol would
+        let verifiers = tx_env.get_verifiers();
+
+        let keys_changed = tx_env.all_touched_storage_keys();
 
         Self {
-            vp_cache_dir,
-            vp_wasm_cache,
+            address,
             tx_env,
-        }
-    }
-}
-
-impl Default for TestNativeVpEnv {
-    fn default() -> Self {
-        let (vp_wasm_cache, vp_cache_dir) =
-            wasm::compilation_cache::common::testing::cache();
-
-        Self {
-            vp_cache_dir,
-            vp_wasm_cache,
-            tx_env: TestTxEnv::default(),
+            verifiers,
+            keys_changed,
         }
     }
 }
@@ -51,20 +42,10 @@ impl TestNativeVpEnv {
     pub fn validate_tx<'a, T>(
         &'a self,
         init_native_vp: impl Fn(NativeVpCtx<'a>) -> T,
-        // The function is applied on the `tx_data` when called
-        mut apply_tx: impl FnMut(&[u8]),
     ) -> Result<bool, <T as NativeVp>::Error>
     where
         T: NativeVp,
     {
-        let tx_data = self.tx_env.tx.data.as_ref().cloned().unwrap_or_default();
-        apply_tx(&tx_data);
-
-        // Find the tx verifiers and keys_changes the same way as protocol would
-        let verifiers = self.tx_env.get_verifiers();
-
-        let keys_changed = self.tx_env.all_touched_storage_keys();
-
         let ctx = Ctx {
             iterators: Default::default(),
             gas_meter: Default::default(),
@@ -72,10 +53,13 @@ impl TestNativeVpEnv {
             write_log: &self.tx_env.write_log,
             tx: &self.tx_env.tx,
             vp_wasm_cache: self.tx_env.vp_wasm_cache.clone(),
+            address: &self.address,
+            keys_changed: &self.keys_changed,
+            verifiers: &self.verifiers,
         };
         let tx_data = self.tx_env.tx.data.as_ref().cloned().unwrap_or_default();
         let native_vp = init_native_vp(ctx);
 
-        native_vp.validate_tx(&tx_data, &keys_changed, &verifiers)
+        native_vp.validate_tx(&tx_data, &self.keys_changed, &self.verifiers)
     }
 }

tests/src/vm_host_env/tx.rs
@@ -15,16 +15,25 @@ use namada::types::{key, token};
 use namada::vm::prefix_iter::PrefixIterators;
 use namada::vm::wasm::{self, TxCache, VpCache};
 use namada::vm::{self, WasmCacheRwAccess};
-use namada_vm_env::tx_prelude::BorshSerialize;
+use namada_tx_prelude::{BorshSerialize, Ctx};
 use tempfile::TempDir;
+
+/// Tx execution context provides access to host env functions
+static mut CTX: Ctx = unsafe { Ctx::new() };
+
+/// Tx execution context provides access to host env functions
+pub fn ctx() -> &'static mut Ctx {
+    unsafe { &mut CTX }
+}
 
 /// This module combines the native host function implementations from
 /// `native_tx_host_env` with the functions exposed to the tx wasm
 /// that will call to the native functions, instead of interfacing via a
 /// wasm runtime. It can be used for host environment integration tests.
 pub mod tx_host_env {
-    pub use namada_vm_env::tx_prelude::*;
+    pub use namada_tx_prelude::*;
 
+    pub use super::ctx;
     pub use super::native_tx_host_env::*;
 }
 

tests/src/vm_host_env/vp.rs
@@ -10,17 +10,27 @@ use namada::types::storage::{self, Key};
 use namada::vm::prefix_iter::PrefixIterators;
 use namada::vm::wasm::{self, VpCache};
 use namada::vm::{self, WasmCacheRwAccess};
+use namada_vp_prelude::Ctx;
 use tempfile::TempDir;
 
 use crate::tx::{tx_host_env, TestTxEnv};
+
+/// VP execution context provides access to host env functions
+pub static CTX: Ctx = unsafe { Ctx::new() };
+
+/// VP execution context provides access to host env functions
+pub fn ctx() -> &'static Ctx {
+    &CTX
+}
 
 /// This module combines the native host function implementations from
 /// `native_vp_host_env` with the functions exposed to the vp wasm
 /// that will call to the native functions, instead of interfacing via a
 /// wasm runtime. It can be used for host environment integration tests.
 pub mod vp_host_env {
-    pub use namada_vm_env::vp_prelude::*;
+    pub use namada_vp_prelude::*;
 
+    pub use super::ctx;
     pub use super::native_vp_host_env::*;
 }
 
@@ -160,7 +170,7 @@ mod native_vp_host_env {
     /// Initialize the VP host environment in [`ENV`] by running a transaction.
     /// The transaction is expected to modify the storage sub-space of the given
     /// address `addr` or to add it to the set of verifiers using
-    /// [`tx_host_env::insert_verifier`].
+    /// `ctx.insert_verifier`.
     pub fn init_from_tx(
         addr: Address,
         mut tx_env: TestTxEnv,