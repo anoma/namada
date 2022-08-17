anoma/anoma

VM: move vm_env sub-mod into tx/vp_prelude and add Tx/VpEnv and Ctx

tx_prelude/src/governance.rs
@@ -1,0 +1,78 @@
+//! Governance
+
+use anoma::ledger::governance::storage;
+use anoma::ledger::governance::vp::ADDRESS as governance_address;
+use anoma::types::address::xan as m1t;
+use anoma::types::token::Amount;
+use anoma::types::transaction::governance::{
+    InitProposalData, VoteProposalData,
+};
+
+use super::*;
+use crate::token::transfer;
+
+/// A proposal creation transaction.
+pub fn init_proposal(ctx: &mut Ctx, data: InitProposalData) -> TxResult {
+    let counter_key = storage::get_counter_key();
+    let proposal_id = if let Some(id) = data.id {
+        id
+    } else {
+        ctx.read(&counter_key)?.unwrap()
+    };
+
+    let content_key = storage::get_content_key(proposal_id);
+    ctx.write_bytes(&content_key, data.content)?;
+
+    let author_key = storage::get_author_key(proposal_id);
+    ctx.write(&author_key, data.author.clone())?;
+
+    let voting_start_epoch_key =
+        storage::get_voting_start_epoch_key(proposal_id);
+    ctx.write(&voting_start_epoch_key, data.voting_start_epoch)?;
+
+    let voting_end_epoch_key = storage::get_voting_end_epoch_key(proposal_id);
+    ctx.write(&voting_end_epoch_key, data.voting_end_epoch)?;
+
+    let grace_epoch_key = storage::get_grace_epoch_key(proposal_id);
+    ctx.write(&grace_epoch_key, data.grace_epoch)?;
+
+    if let Some(proposal_code) = data.proposal_code {
+        let proposal_code_key = storage::get_proposal_code_key(proposal_id);
+        ctx.write_bytes(&proposal_code_key, proposal_code)?;
+    }
+
+    ctx.write(&counter_key, proposal_id + 1)?;
+
+    let min_proposal_funds_key = storage::get_min_proposal_fund_key();
+    let min_proposal_funds: Amount =
+        ctx.read(&min_proposal_funds_key)?.unwrap();
+
+    let funds_key = storage::get_funds_key(proposal_id);
+    ctx.write(&funds_key, min_proposal_funds)?;
+
+    // this key must always be written for each proposal
+    let committing_proposals_key =
+        storage::get_committing_proposals_key(proposal_id, data.grace_epoch.0);
+    ctx.write(&committing_proposals_key, ())?;
+
+    transfer(
+        ctx,
+        &data.author,
+        &governance_address,
+        &m1t(),
+        min_proposal_funds,
+    )
+}
+
+/// A proposal vote transaction.
+pub fn vote_proposal(ctx: &mut Ctx, data: VoteProposalData) -> TxResult {
+    for delegation in data.delegations {
+        let vote_key = storage::get_vote_proposal_key(
+            data.id,
+            data.voter.clone(),
+            delegation,
+        );
+        ctx.write(&vote_key, data.vote.clone())?;
+    }
+    Ok(())
+}

tx_prelude/src/intent.rs
@@ -1,0 +1,18 @@
+use std::collections::HashSet;
+
+use anoma::proto::Signed;
+use anoma::types::intent;
+pub use anoma::types::intent::*;
+use anoma::types::key::*;
+
+use super::*;
+pub fn invalidate_exchange(
+    ctx: &mut Ctx,
+    intent: &Signed<Exchange>,
+) -> TxResult {
+    let key = intent::invalid_intent_key(&intent.data.addr);
+    let mut invalid_intent: HashSet<common::Signature> =
+        ctx.read(&key)?.unwrap_or_default();
+    invalid_intent.insert(intent.sig.clone());
+    ctx.write(&key, &invalid_intent)
+}

tx_prelude/src/token.rs
@@ -1,0 +1,53 @@
+use anoma::types::address::{Address, InternalAddress};
+use anoma::types::token;
+pub use anoma::types::token::*;
+
+use super::*;
+
+/// A token transfer that can be used in a transaction.
+pub fn transfer(
+    ctx: &mut Ctx,
+    src: &Address,
+    dest: &Address,
+    token: &Address,
+    amount: Amount,
+) -> TxResult {
+    let src_key = token::balance_key(token, src);
+    let dest_key = token::balance_key(token, dest);
+    let src_bal: Option<Amount> = ctx.read(&src_key)?;
+    let mut src_bal = src_bal.unwrap_or_else(|| match src {
+        Address::Internal(InternalAddress::IbcMint) => Amount::max(),
+        _ => {
+            log_string(format!("src {} has no balance", src));
+            unreachable!()
+        }
+    });
+    src_bal.spend(&amount);
+    let mut dest_bal: Amount = ctx.read(&dest_key)?.unwrap_or_default();
+    dest_bal.receive(&amount);
+    match src {
+        Address::Internal(InternalAddress::IbcMint) => {
+            ctx.write_temp(&src_key, src_bal)?;
+        }
+        Address::Internal(InternalAddress::IbcBurn) => {
+            log_string("invalid transfer from the burn address");
+            unreachable!()
+        }
+        _ => {
+            ctx.write(&src_key, src_bal)?;
+        }
+    }
+    match dest {
+        Address::Internal(InternalAddress::IbcMint) => {
+            log_string("invalid transfer to the mint address");
+            unreachable!()
+        }
+        Address::Internal(InternalAddress::IbcBurn) => {
+            ctx.write_temp(&dest_key, dest_bal)?;
+        }
+        _ => {
+            ctx.write(&dest_key, dest_bal)?;
+        }
+    }
+    Ok(())
+}

vp_prelude/src/token.rs
@@ -1,0 +1,61 @@
+//! A fungible token validity predicate.
+
+use std::collections::BTreeSet;
+
+use anoma::types::address::{Address, InternalAddress};
+use anoma::types::storage::Key;
+/// Vp imports and functions.
+use anoma::types::storage::KeySeg;
+use anoma::types::token;
+pub use anoma::types::token::*;
+
+use super::*;
+
+/// A token validity predicate.
+pub fn vp(
+    ctx: &Ctx,
+    token: &Address,
+    keys_changed: &BTreeSet<Key>,
+    verifiers: &BTreeSet<Address>,
+) -> VpResult {
+    let mut change: Change = 0;
+    for key in keys_changed.iter() {
+        match token::is_balance_key(token, key) {
+            None => {
+                // Unknown changes to this address space are disallowed, but
+                // unknown changes anywhere else are permitted
+                if key.segments.get(0) == Some(&token.to_db_key()) {
+                    return reject();
+                }
+            }
+            Some(owner) => {
+                // accumulate the change
+                let pre: Amount = match owner {
+                    Address::Internal(InternalAddress::IbcMint) => {
+                        Amount::max()
+                    }
+                    Address::Internal(InternalAddress::IbcBurn) => {
+                        Amount::default()
+                    }
+                    _ => ctx.read_pre(key)?.unwrap_or_default(),
+                };
+                let post: Amount = match owner {
+                    Address::Internal(InternalAddress::IbcMint) => {
+                        ctx.read_temp(key)?.unwrap_or_else(Amount::max)
+                    }
+                    Address::Internal(InternalAddress::IbcBurn) => {
+                        ctx.read_temp(key)?.unwrap_or_default()
+                    }
+                    _ => ctx.read_post(key)?.unwrap_or_default(),
+                };
+                let this_change = post.change() - pre.change();
+                change += this_change;
+                // make sure that the spender approved the transaction
+                if this_change < 0 && !verifiers.contains(owner) {
+                    return reject();
+                }
+            }
+        }
+    }
+    Ok(change == 0)
+}

anoma/namada

tx_prelude/src/governance.rs
@@ -1,0 +1,78 @@
+//! Governance
+
+use namada::ledger::governance::storage;
+use namada::ledger::governance::vp::ADDRESS as governance_address;
+use namada::types::address::xan as m1t;
+use namada::types::token::Amount;
+use namada::types::transaction::governance::{
+    InitProposalData, VoteProposalData,
+};
+
+use super::*;
+use crate::token::transfer;
+
+/// A proposal creation transaction.
+pub fn init_proposal(ctx: &mut Ctx, data: InitProposalData) -> TxResult {
+    let counter_key = storage::get_counter_key();
+    let proposal_id = if let Some(id) = data.id {
+        id
+    } else {
+        ctx.read(&counter_key)?.unwrap()
+    };
+
+    let content_key = storage::get_content_key(proposal_id);
+    ctx.write_bytes(&content_key, data.content)?;
+
+    let author_key = storage::get_author_key(proposal_id);
+    ctx.write(&author_key, data.author.clone())?;
+
+    let voting_start_epoch_key =
+        storage::get_voting_start_epoch_key(proposal_id);
+    ctx.write(&voting_start_epoch_key, data.voting_start_epoch)?;
+
+    let voting_end_epoch_key = storage::get_voting_end_epoch_key(proposal_id);
+    ctx.write(&voting_end_epoch_key, data.voting_end_epoch)?;
+
+    let grace_epoch_key = storage::get_grace_epoch_key(proposal_id);
+    ctx.write(&grace_epoch_key, data.grace_epoch)?;
+
+    if let Some(proposal_code) = data.proposal_code {
+        let proposal_code_key = storage::get_proposal_code_key(proposal_id);
+        ctx.write_bytes(&proposal_code_key, proposal_code)?;
+    }
+
+    ctx.write(&counter_key, proposal_id + 1)?;
+
+    let min_proposal_funds_key = storage::get_min_proposal_fund_key();
+    let min_proposal_funds: Amount =
+        ctx.read(&min_proposal_funds_key)?.unwrap();
+
+    let funds_key = storage::get_funds_key(proposal_id);
+    ctx.write(&funds_key, min_proposal_funds)?;
+
+    // this key must always be written for each proposal
+    let committing_proposals_key =
+        storage::get_committing_proposals_key(proposal_id, data.grace_epoch.0);
+    ctx.write(&committing_proposals_key, ())?;
+
+    transfer(
+        ctx,
+        &data.author,
+        &governance_address,
+        &m1t(),
+        min_proposal_funds,
+    )
+}
+
+/// A proposal vote transaction.
+pub fn vote_proposal(ctx: &mut Ctx, data: VoteProposalData) -> TxResult {
+    for delegation in data.delegations {
+        let vote_key = storage::get_vote_proposal_key(
+            data.id,
+            data.voter.clone(),
+            delegation,
+        );
+        ctx.write(&vote_key, data.vote.clone())?;
+    }
+    Ok(())
+}

tx_prelude/src/intent.rs
@@ -1,0 +1,18 @@
+use std::collections::HashSet;
+
+use namada::proto::Signed;
+use namada::types::intent;
+pub use namada::types::intent::*;
+use namada::types::key::*;
+
+use super::*;
+pub fn invalidate_exchange(
+    ctx: &mut Ctx,
+    intent: &Signed<Exchange>,
+) -> TxResult {
+    let key = intent::invalid_intent_key(&intent.data.addr);
+    let mut invalid_intent: HashSet<common::Signature> =
+        ctx.read(&key)?.unwrap_or_default();
+    invalid_intent.insert(intent.sig.clone());
+    ctx.write(&key, &invalid_intent)
+}

tx_prelude/src/token.rs
@@ -1,0 +1,53 @@
+use namada::types::address::{Address, InternalAddress};
+use namada::types::token;
+pub use namada::types::token::*;
+
+use super::*;
+
+/// A token transfer that can be used in a transaction.
+pub fn transfer(
+    ctx: &mut Ctx,
+    src: &Address,
+    dest: &Address,
+    token: &Address,
+    amount: Amount,
+) -> TxResult {
+    let src_key = token::balance_key(token, src);
+    let dest_key = token::balance_key(token, dest);
+    let src_bal: Option<Amount> = ctx.read(&src_key)?;
+    let mut src_bal = src_bal.unwrap_or_else(|| match src {
+        Address::Internal(InternalAddress::IbcMint) => Amount::max(),
+        _ => {
+            log_string(format!("src {} has no balance", src));
+            unreachable!()
+        }
+    });
+    src_bal.spend(&amount);
+    let mut dest_bal: Amount = ctx.read(&dest_key)?.unwrap_or_default();
+    dest_bal.receive(&amount);
+    match src {
+        Address::Internal(InternalAddress::IbcMint) => {
+            ctx.write_temp(&src_key, src_bal)?;
+        }
+        Address::Internal(InternalAddress::IbcBurn) => {
+            log_string("invalid transfer from the burn address");
+            unreachable!()
+        }
+        _ => {
+            ctx.write(&src_key, src_bal)?;
+        }
+    }
+    match dest {
+        Address::Internal(InternalAddress::IbcMint) => {
+            log_string("invalid transfer to the mint address");
+            unreachable!()
+        }
+        Address::Internal(InternalAddress::IbcBurn) => {
+            ctx.write_temp(&dest_key, dest_bal)?;
+        }
+        _ => {
+            ctx.write(&dest_key, dest_bal)?;
+        }
+    }
+    Ok(())
+}

vp_prelude/src/token.rs
@@ -1,0 +1,61 @@
+//! A fungible token validity predicate.
+
+use std::collections::BTreeSet;
+
+use namada::types::address::{Address, InternalAddress};
+use namada::types::storage::Key;
+/// Vp imports and functions.
+use namada::types::storage::KeySeg;
+use namada::types::token;
+pub use namada::types::token::*;
+
+use super::*;
+
+/// A token validity predicate.
+pub fn vp(
+    ctx: &Ctx,
+    token: &Address,
+    keys_changed: &BTreeSet<Key>,
+    verifiers: &BTreeSet<Address>,
+) -> VpResult {
+    let mut change: Change = 0;
+    for key in keys_changed.iter() {
+        match token::is_balance_key(token, key) {
+            None => {
+                // Unknown changes to this address space are disallowed, but
+                // unknown changes anywhere else are permitted
+                if key.segments.get(0) == Some(&token.to_db_key()) {
+                    return reject();
+                }
+            }
+            Some(owner) => {
+                // accumulate the change
+                let pre: Amount = match owner {
+                    Address::Internal(InternalAddress::IbcMint) => {
+                        Amount::max()
+                    }
+                    Address::Internal(InternalAddress::IbcBurn) => {
+                        Amount::default()
+                    }
+                    _ => ctx.read_pre(key)?.unwrap_or_default(),
+                };
+                let post: Amount = match owner {
+                    Address::Internal(InternalAddress::IbcMint) => {
+                        ctx.read_temp(key)?.unwrap_or_else(Amount::max)
+                    }
+                    Address::Internal(InternalAddress::IbcBurn) => {
+                        ctx.read_temp(key)?.unwrap_or_default()
+                    }
+                    _ => ctx.read_post(key)?.unwrap_or_default(),
+                };
+                let this_change = post.change() - pre.change();
+                change += this_change;
+                // make sure that the spender approved the transaction
+                if this_change < 0 && !verifiers.contains(owner) {
+                    return reject();
+                }
+            }
+        }
+    }
+    Ok(change == 0)
+}