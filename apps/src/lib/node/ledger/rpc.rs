IdaTucker/anoma

Wrote code to produce conversion Merkle trees and process queries for their nodes.

apps/src/lib/node/ledger/rpc.rs
@@ -5,6 +5,8 @@ use std::str::FromStr;
 
 use anoma::types::address::Address;
 use anoma::types::storage;
+use anoma::types::token::CONVERSION_KEY_PREFIX;
+use masp_primitives::asset_type::AssetType;
 #[cfg(not(feature = "ABCI"))]
 use tendermint::abci::Path as AbciPath;
 #[cfg(feature = "ABCI")]
@@ -24,6 +26,8 @@ pub enum Path {
     Prefix(storage::Key),
     /// Check if the given storage key exists
     HasKey(storage::Key),
+    /// Conversion associated with given asset type
+    Conversion(AssetType),
 }
 
 #[derive(Debug, Clone)]
@@ -54,6 +58,9 @@ impl Display for Path {
             Path::HasKey(storage_key) => {
                 write!(f, "{}/{}", HAS_KEY_PREFIX, storage_key)
             }
+            Path::Conversion(asset_type) => {
+                write!(f, "{}/{}", CONVERSION_KEY_PREFIX, asset_type)
+            }
         }
     }
 }
@@ -81,6 +88,11 @@ impl FromStr for Path {
                         .map_err(PathParseError::InvalidStorageKey)?;
                     Ok(Self::HasKey(key))
                 }
+                Some((CONVERSION_KEY_PREFIX, asset_type)) => {
+                    let key = AssetType::from_str(asset_type)
+                        .map_err(PathParseError::InvalidAssetType)?;
+                    Ok(Self::Conversion(key))
+                }
                 _ => Err(PathParseError::InvalidPath(s.to_string())),
             },
         }
@@ -103,4 +115,6 @@ pub enum PathParseError {
     InvalidPath(String),
     #[error("Invalid storage key: {0}")]
     InvalidStorageKey(storage::Error),
+    #[error("Unrecognized asset type: {0}")]
+    InvalidAssetType(std::io::Error),
 }

apps/src/lib/node/ledger/shell/queries.rs
@@ -3,6 +3,7 @@ use std::cmp::max;
 
 use anoma::ledger::parameters::EpochDuration;
 use anoma::ledger::pos::PosParams;
+use anoma::ledger::storage::types;
 use anoma::types::address::Address;
 use anoma::types::key;
 use anoma::types::key::dkg_session_keys::DkgPublicKey;
@@ -10,6 +11,7 @@ use anoma::types::storage::{Key, PrefixValue};
 use anoma::types::token::{self, Amount};
 use borsh::{BorshDeserialize, BorshSerialize};
 use ferveo_common::TendermintValidator;
+use masp_primitives::asset_type::AssetType;
 #[cfg(not(feature = "ABCI"))]
 use tendermint_proto::crypto::{ProofOp, ProofOps};
 #[cfg(not(feature = "ABCI"))]
@@ -62,6 +64,9 @@ where
                         ..Default::default()
                     }
                 }
+                Path::Conversion(asset_type) => {
+                    self.read_conversion(&asset_type)
+                }
                 Path::Value(storage_key) => {
                     self.read_storage_value(&storage_key, height, query.prove)
                 }
@@ -75,6 +80,38 @@ where
                 info: format!("RPC error: {}", err),
                 ..Default::default()
             },
+        }
+    }
+
+    /// Query to read a conversion from storage
+    pub fn read_conversion(&self, asset_type: &AssetType) -> response::Query {
+        // Conversion values are constructed on request
+        if let Some((addr, epoch, conv, pos)) =
+            self.storage.conversion_state.assets.get(asset_type)
+        {
+            let conv = (
+                addr,
+                epoch,
+                Into::<masp_primitives::transaction::components::Amount>::into(
+                    conv.clone(),
+                ),
+                self.storage.conversion_state.tree.path(*pos),
+            );
+            response::Query {
+                value: types::encode(&conv),
+                proof_ops: None,
+                ..Default::default()
+            }
+        } else {
+            response::Query {
+                code: 1,
+                info: format!(
+                    "No conversion found for asset type: {}",
+                    asset_type
+                ),
+                proof_ops: None,
+                ..Default::default()
+            }
         }
     }
 

anoma/namada

apps/src/lib/node/ledger/rpc.rs
@@ -3,8 +3,10 @@
 use std::fmt::Display;
 use std::str::FromStr;
 
+use masp_primitives::asset_type::AssetType;
 use namada::types::address::Address;
 use namada::types::storage;
+use namada::types::token::CONVERSION_KEY_PREFIX;
 use tendermint::abci::Path as AbciPath;
 use thiserror::Error;
 
@@ -21,6 +23,8 @@ pub enum Path {
     Prefix(storage::Key),
     /// Check if the given storage key exists
     HasKey(storage::Key),
+    /// Conversion associated with given asset type
+    Conversion(AssetType),
 }
 
 #[derive(Debug, Clone)]
@@ -51,6 +55,9 @@ impl Display for Path {
             Path::HasKey(storage_key) => {
                 write!(f, "{}/{}", HAS_KEY_PREFIX, storage_key)
             }
+            Path::Conversion(asset_type) => {
+                write!(f, "{}/{}", CONVERSION_KEY_PREFIX, asset_type)
+            }
         }
     }
 }
@@ -78,6 +85,11 @@ impl FromStr for Path {
                         .map_err(PathParseError::InvalidStorageKey)?;
                     Ok(Self::HasKey(key))
                 }
+                Some((CONVERSION_KEY_PREFIX, asset_type)) => {
+                    let key = AssetType::from_str(asset_type)
+                        .map_err(PathParseError::InvalidAssetType)?;
+                    Ok(Self::Conversion(key))
+                }
                 _ => Err(PathParseError::InvalidPath(s.to_string())),
             },
         }
@@ -100,4 +112,6 @@ pub enum PathParseError {
     InvalidPath(String),
     #[error("Invalid storage key: {0}")]
     InvalidStorageKey(storage::Error),
+    #[error("Unrecognized asset type: {0}")]
+    InvalidAssetType(std::io::Error),
 }

apps/src/lib/node/ledger/shell/queries.rs
@@ -3,8 +3,10 @@ use std::cmp::max;
 
 use borsh::{BorshDeserialize, BorshSerialize};
 use ferveo_common::TendermintValidator;
+use masp_primitives::asset_type::AssetType;
 use namada::ledger::parameters::EpochDuration;
 use namada::ledger::pos::PosParams;
+use namada::ledger::storage::types;
 use namada::types::address::Address;
 use namada::types::key;
 use namada::types::key::dkg_session_keys::DkgPublicKey;
@@ -53,6 +55,9 @@ where
                         ..Default::default()
                     }
                 }
+                Path::Conversion(asset_type) => {
+                    self.read_conversion(&asset_type)
+                }
                 Path::Value(storage_key) => {
                     self.read_storage_value(&storage_key, height, query.prove)
                 }
@@ -66,6 +71,38 @@ where
                 info: format!("RPC error: {}", err),
                 ..Default::default()
             },
+        }
+    }
+
+    /// Query to read a conversion from storage
+    pub fn read_conversion(&self, asset_type: &AssetType) -> response::Query {
+        // Conversion values are constructed on request
+        if let Some((addr, epoch, conv, pos)) =
+            self.storage.conversion_state.assets.get(asset_type)
+        {
+            let conv = (
+                addr,
+                epoch,
+                Into::<masp_primitives::transaction::components::Amount>::into(
+                    conv.clone(),
+                ),
+                self.storage.conversion_state.tree.path(*pos),
+            );
+            response::Query {
+                value: types::encode(&conv),
+                proof_ops: None,
+                ..Default::default()
+            }
+        } else {
+            response::Query {
+                code: 1,
+                info: format!(
+                    "No conversion found for asset type: {}",
+                    asset_type
+                ),
+                proof_ops: None,
+                ..Default::default()
+            }
         }
     }
 