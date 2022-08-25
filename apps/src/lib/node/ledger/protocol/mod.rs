IdaTucker/anoma

Now provide a get_tx_index function in the Tx and VP environments.

apps/src/lib/node/ledger/protocol/mod.rs
@@ -15,6 +15,7 @@ use anoma::ledger::treasury::TreasuryVp;
 use anoma::proto::{self, Tx};
 use anoma::types::address::{Address, InternalAddress};
 use anoma::types::storage;
+use anoma::types::storage::TxIndex;
 use anoma::types::transaction::{DecryptedTx, TxResult, TxType, VpsResult};
 use anoma::vm::wasm::{TxCache, VpCache};
 use anoma::vm::{self, wasm, WasmCacheAccess};
@@ -66,9 +67,11 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// If the given tx is a successfully decrypted payload apply the necessary
 /// vps. Otherwise, we include the tx on chain with the gas charge added
 /// but no further validations.
+#[allow(clippy::too_many_arguments)]
 pub fn apply_tx<D, H, CA>(
     tx: TxType,
     tx_length: usize,
+    tx_index: TxIndex,
     block_gas_meter: &mut BlockGasMeter,
     write_log: &mut WriteLog,
     storage: &Storage<D, H>,
@@ -89,6 +92,7 @@ where
         TxType::Decrypted(DecryptedTx::Decrypted(tx)) => {
             let verifiers = execute_tx(
                 &tx,
+                &tx_index,
                 storage,
                 block_gas_meter,
                 write_log,
@@ -98,6 +102,7 @@ where
 
             let vps_result = check_vps(
                 &tx,
+                &tx_index,
                 storage,
                 block_gas_meter,
                 write_log,
@@ -135,6 +140,7 @@ where
 /// Execute a transaction code. Returns verifiers requested by the transaction.
 fn execute_tx<D, H, CA>(
     tx: &Tx,
+    tx_index: &TxIndex,
     storage: &Storage<D, H>,
     gas_meter: &mut BlockGasMeter,
     write_log: &mut WriteLog,
@@ -155,6 +161,7 @@ where
         storage,
         write_log,
         gas_meter,
+        tx_index,
         &tx.code,
         tx_data,
         vp_wasm_cache,
@@ -166,6 +173,7 @@ where
 /// Check the acceptance of a transaction by validity predicates
 fn check_vps<D, H, CA>(
     tx: &Tx,
+    tx_index: &TxIndex,
     storage: &Storage<D, H>,
     gas_meter: &mut BlockGasMeter,
     write_log: &WriteLog,
@@ -186,6 +194,7 @@ where
         verifiers,
         keys_changed,
         tx,
+        tx_index,
         storage,
         write_log,
         initial_gas,
@@ -201,10 +210,12 @@ where
 }
 
 /// Execute verifiers' validity predicates
+#[allow(clippy::too_many_arguments)]
 fn execute_vps<D, H, CA>(
     verifiers: BTreeSet<Address>,
     keys_changed: BTreeSet<storage::Key>,
     tx: &Tx,
+    tx_index: &TxIndex,
     storage: &Storage<D, H>,
     write_log: &WriteLog,
     initial_gas: u64,
@@ -237,6 +248,7 @@ where
                     wasm::run::vp(
                         vp,
                         tx,
+                        tx_index,
                         addr,
                         storage,
                         write_log,
@@ -252,6 +264,7 @@ where
                         storage,
                         write_log,
                         tx,
+                        tx_index,
                         gas_meter,
                         vp_wasm_cache.clone(),
                     );

apps/src/lib/node/ledger/shell/mod.rs
@@ -33,7 +33,7 @@ use anoma::ledger::{ibc, parameters, pos};
 use anoma::proto::{self, Tx};
 use anoma::types::chain::ChainId;
 use anoma::types::key::*;
-use anoma::types::storage::{BlockHeight, Key};
+use anoma::types::storage::{BlockHeight, Key, TxIndex};
 use anoma::types::time::{DateTimeUtc, TimeZone, Utc};
 use anoma::types::transaction::{
     hash_tx, process_tx, verify_decrypted_correctly, AffineCurve, DecryptedTx,
@@ -595,6 +595,7 @@ where
                 match protocol::apply_tx(
                     tx,
                     tx_bytes.len(),
+                    TxIndex::default(),
                     &mut gas_meter,
                     &mut write_log,
                     &self.storage,

tests/src/vm_host_env/ibc.rs
@@ -65,7 +65,7 @@ use anoma::tendermint_proto::Protobuf;
 use anoma::types::address::{self, Address, InternalAddress};
 use anoma::types::ibc::data::FungibleTokenPacketData;
 use anoma::types::ibc::IbcEvent;
-use anoma::types::storage::{BlockHash, BlockHeight, Key};
+use anoma::types::storage::{BlockHash, BlockHeight, Key, TxIndex};
 use anoma::types::time::Rfc3339String;
 use anoma::types::token::{self, Amount};
 use anoma::vm::{wasm, WasmCacheRwAccess};
@@ -193,6 +193,7 @@ pub fn init_ibc_vp_from_tx<'a>(
         &tx_env.storage,
         &tx_env.write_log,
         tx,
+        &TxIndex(0),
         VpGasMeter::new(0),
         vp_wasm_cache,
     );
@@ -224,6 +225,7 @@ pub fn init_token_vp_from_tx<'a>(
         &tx_env.storage,
         &tx_env.write_log,
         tx,
+        &TxIndex(0),
         VpGasMeter::new(0),
         vp_wasm_cache,
     );

tests/src/vm_host_env/tx.rs
@@ -8,7 +8,7 @@ use anoma::ledger::storage::testing::TestStorage;
 use anoma::ledger::storage::write_log::WriteLog;
 use anoma::proto::Tx;
 use anoma::types::address::Address;
-use anoma::types::storage::Key;
+use anoma::types::storage::{Key, TxIndex};
 use anoma::types::time::DurationSecs;
 use anoma::types::{key, token};
 use anoma::vm::prefix_iter::PrefixIterators;
@@ -38,6 +38,7 @@ pub struct TestTxEnv {
     pub iterators: PrefixIterators<'static, MockDB>,
     pub verifiers: BTreeSet<Address>,
     pub gas_meter: BlockGasMeter,
+    pub tx_index: TxIndex,
     pub result_buffer: Option<Vec<u8>>,
     pub vp_wasm_cache: VpCache<WasmCacheRwAccess>,
     pub vp_cache_dir: TempDir,
@@ -57,6 +58,7 @@ impl Default for TestTxEnv {
             write_log: WriteLog::default(),
             iterators: PrefixIterators::default(),
             gas_meter: BlockGasMeter::default(),
+            tx_index: TxIndex::default(),
             verifiers: BTreeSet::default(),
             result_buffer: None,
             vp_wasm_cache,
@@ -241,7 +243,8 @@ mod native_tx_host_env {
                                 iterators,
                                 verifiers,
                                 gas_meter,
-                                result_buffer,
+                            result_buffer,
+                            tx_index,
                                 vp_wasm_cache,
                                 vp_cache_dir: _,
                                 tx_wasm_cache,
@@ -255,6 +258,7 @@ mod native_tx_host_env {
                                 iterators,
                                 verifiers,
                                 gas_meter,
+                                tx_index,
                                 result_buffer,
                                 vp_wasm_cache,
                                 tx_wasm_cache,
@@ -274,6 +278,7 @@ mod native_tx_host_env {
                     #[no_mangle]
                     extern "C" fn extern_fn_name( $($arg: $type),* ) -> $ret {
                         with(|TestTxEnv {
+                            tx_index,
                                 storage,
                                 write_log,
                                 iterators,
@@ -293,6 +298,7 @@ mod native_tx_host_env {
                                 iterators,
                                 verifiers,
                                 gas_meter,
+                                tx_index,
                                 result_buffer,
                                 vp_wasm_cache,
                                 tx_wasm_cache,
@@ -342,6 +348,7 @@ mod native_tx_host_env {
     native_host_fn!(tx_emit_ibc_event(event_ptr: u64, event_len: u64));
     native_host_fn!(tx_get_chain_id(result_ptr: u64));
     native_host_fn!(tx_get_block_height() -> u64);
+    native_host_fn!(tx_get_tx_index() -> u32);
     native_host_fn!(tx_get_block_time() -> i64);
     native_host_fn!(tx_get_block_hash(result_ptr: u64));
     native_host_fn!(tx_get_block_epoch() -> u64);

tests/src/vm_host_env/vp.rs
@@ -6,7 +6,7 @@ use anoma::ledger::storage::testing::TestStorage;
 use anoma::ledger::storage::write_log::WriteLog;
 use anoma::proto::Tx;
 use anoma::types::address::{self, Address};
-use anoma::types::storage::{self, Key};
+use anoma::types::storage::{self, Key, TxIndex};
 use anoma::vm::prefix_iter::PrefixIterators;
 use anoma::vm::wasm::{self, VpCache};
 use anoma::vm::{self, WasmCacheRwAccess};
@@ -32,6 +32,7 @@ pub struct TestVpEnv {
     pub iterators: PrefixIterators<'static, MockDB>,
     pub gas_meter: VpGasMeter,
     pub tx: Tx,
+    pub tx_index: TxIndex,
     pub keys_changed: BTreeSet<storage::Key>,
     pub verifiers: BTreeSet<Address>,
     pub eval_runner: native_vp_host_env::VpEval,
@@ -57,6 +58,7 @@ impl Default for TestVpEnv {
             iterators: PrefixIterators::default(),
             gas_meter: VpGasMeter::default(),
             tx: Tx::new(vec![], None),
+            tx_index: TxIndex::default(),
             keys_changed: BTreeSet::default(),
             verifiers: BTreeSet::default(),
             eval_runner,
@@ -234,7 +236,8 @@ mod native_vp_host_env {
                                 write_log,
                                 iterators,
                                 gas_meter,
-                                tx,
+                            tx,
+                            tx_index,
                                 keys_changed,
                                 verifiers,
                                 eval_runner,
@@ -250,6 +253,7 @@ mod native_vp_host_env {
                                 iterators,
                                 gas_meter,
                                 tx,
+                                tx_index,
                                 verifiers,
                                 result_buffer,
                                 keys_changed,
@@ -276,7 +280,8 @@ mod native_vp_host_env {
                                 write_log,
                                 iterators,
                                 gas_meter,
-                                tx,
+                            tx,
+                            tx_index,
                                 keys_changed,
                                 verifiers,
                                 eval_runner,
@@ -292,6 +297,7 @@ mod native_vp_host_env {
                                 iterators,
                                 gas_meter,
                                 tx,
+                                tx_index,
                                 verifiers,
                                 result_buffer,
                                 keys_changed,
@@ -320,6 +326,7 @@ mod native_vp_host_env {
     native_host_fn!(vp_iter_post_next(iter_id: u64) -> i64);
     native_host_fn!(vp_get_chain_id(result_ptr: u64));
     native_host_fn!(vp_get_block_height() -> u64);
+    native_host_fn!(vp_get_tx_index() -> u32);
     native_host_fn!(vp_get_block_hash(result_ptr: u64));
     native_host_fn!(vp_get_tx_code_hash(result_ptr: u64));
     native_host_fn!(vp_get_block_epoch() -> u64);

anoma/namada

apps/src/lib/node/ledger/protocol/mod.rs
@@ -15,6 +15,7 @@ use namada::ledger::treasury::TreasuryVp;
 use namada::proto::{self, Tx};
 use namada::types::address::{Address, InternalAddress};
 use namada::types::storage;
+use namada::types::storage::TxIndex;
 use namada::types::transaction::{DecryptedTx, TxResult, TxType, VpsResult};
 use namada::vm::wasm::{TxCache, VpCache};
 use namada::vm::{self, wasm, WasmCacheAccess};
@@ -66,9 +67,11 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// If the given tx is a successfully decrypted payload apply the necessary
 /// vps. Otherwise, we include the tx on chain with the gas charge added
 /// but no further validations.
+#[allow(clippy::too_many_arguments)]
 pub fn apply_tx<D, H, CA>(
     tx: TxType,
     tx_length: usize,
+    tx_index: TxIndex,
     block_gas_meter: &mut BlockGasMeter,
     write_log: &mut WriteLog,
     storage: &Storage<D, H>,
@@ -89,6 +92,7 @@ where
         TxType::Decrypted(DecryptedTx::Decrypted(tx)) => {
             let verifiers = execute_tx(
                 &tx,
+                &tx_index,
                 storage,
                 block_gas_meter,
                 write_log,
@@ -98,6 +102,7 @@ where
 
             let vps_result = check_vps(
                 &tx,
+                &tx_index,
                 storage,
                 block_gas_meter,
                 write_log,
@@ -135,6 +140,7 @@ where
 /// Execute a transaction code. Returns verifiers requested by the transaction.
 fn execute_tx<D, H, CA>(
     tx: &Tx,
+    tx_index: &TxIndex,
     storage: &Storage<D, H>,
     gas_meter: &mut BlockGasMeter,
     write_log: &mut WriteLog,
@@ -155,6 +161,7 @@ where
         storage,
         write_log,
         gas_meter,
+        tx_index,
         &tx.code,
         tx_data,
         vp_wasm_cache,
@@ -166,6 +173,7 @@ where
 /// Check the acceptance of a transaction by validity predicates
 fn check_vps<D, H, CA>(
     tx: &Tx,
+    tx_index: &TxIndex,
     storage: &Storage<D, H>,
     gas_meter: &mut BlockGasMeter,
     write_log: &WriteLog,
@@ -186,6 +194,7 @@ where
         verifiers,
         keys_changed,
         tx,
+        tx_index,
         storage,
         write_log,
         initial_gas,
@@ -201,10 +210,12 @@ where
 }
 
 /// Execute verifiers' validity predicates
+#[allow(clippy::too_many_arguments)]
 fn execute_vps<D, H, CA>(
     verifiers: BTreeSet<Address>,
     keys_changed: BTreeSet<storage::Key>,
     tx: &Tx,
+    tx_index: &TxIndex,
     storage: &Storage<D, H>,
     write_log: &WriteLog,
     initial_gas: u64,
@@ -237,6 +248,7 @@ where
                     wasm::run::vp(
                         vp,
                         tx,
+                        tx_index,
                         addr,
                         storage,
                         write_log,
@@ -252,6 +264,7 @@ where
                         storage,
                         write_log,
                         tx,
+                        tx_index,
                         gas_meter,
                         vp_wasm_cache.clone(),
                     );

apps/src/lib/node/ledger/shell/mod.rs
@@ -31,9 +31,10 @@ use namada::ledger::storage::{
 };
 use namada::ledger::{ibc, parameters, pos};
 use namada::proto::{self, Tx};
+use namada::types::address::{masp, masp_tx_key};
 use namada::types::chain::ChainId;
 use namada::types::key::*;
-use namada::types::storage::{BlockHeight, Key};
+use namada::types::storage::{BlockHeight, Key, TxIndex};
 use namada::types::time::{DateTimeUtc, TimeZone, Utc};
 use namada::types::transaction::{
     hash_tx, process_tx, verify_decrypted_correctly, AffineCurve, DecryptedTx,
@@ -567,6 +568,7 @@ where
                 match protocol::apply_tx(
                     tx,
                     tx_bytes.len(),
+                    TxIndex::default(),
                     &mut gas_meter,
                     &mut write_log,
                     &self.storage,

tests/src/vm_host_env/ibc.rs
@@ -65,7 +65,7 @@ use namada::tendermint_proto::Protobuf;
 use namada::types::address::{self, Address, InternalAddress};
 use namada::types::ibc::data::FungibleTokenPacketData;
 use namada::types::ibc::IbcEvent;
-use namada::types::storage::{BlockHash, BlockHeight, Key};
+use namada::types::storage::{BlockHash, BlockHeight, Key, TxIndex};
 use namada::types::time::Rfc3339String;
 use namada::types::token::{self, Amount};
 use namada::vm::{wasm, WasmCacheRwAccess};
@@ -193,6 +193,7 @@ pub fn init_ibc_vp_from_tx<'a>(
         &tx_env.storage,
         &tx_env.write_log,
         tx,
+        &TxIndex(0),
         VpGasMeter::new(0),
         vp_wasm_cache,
     );
@@ -224,6 +225,7 @@ pub fn init_token_vp_from_tx<'a>(
         &tx_env.storage,
         &tx_env.write_log,
         tx,
+        &TxIndex(0),
         VpGasMeter::new(0),
         vp_wasm_cache,
     );

tests/src/vm_host_env/tx.rs
@@ -9,7 +9,7 @@ use namada::ledger::storage::testing::TestStorage;
 use namada::ledger::storage::write_log::WriteLog;
 use namada::proto::Tx;
 use namada::types::address::Address;
-use namada::types::storage::Key;
+use namada::types::storage::{Key, TxIndex};
 use namada::types::time::DurationSecs;
 use namada::types::{key, token};
 use namada::vm::prefix_iter::PrefixIterators;
@@ -38,6 +38,7 @@ pub struct TestTxEnv {
     pub iterators: PrefixIterators<'static, MockDB>,
     pub verifiers: BTreeSet<Address>,
     pub gas_meter: BlockGasMeter,
+    pub tx_index: TxIndex,
     pub result_buffer: Option<Vec<u8>>,
     pub vp_wasm_cache: VpCache<WasmCacheRwAccess>,
     pub vp_cache_dir: TempDir,
@@ -57,6 +58,7 @@ impl Default for TestTxEnv {
             write_log: WriteLog::default(),
             iterators: PrefixIterators::default(),
             gas_meter: BlockGasMeter::default(),
+            tx_index: TxIndex::default(),
             verifiers: BTreeSet::default(),
             result_buffer: None,
             vp_wasm_cache,
@@ -241,7 +243,8 @@ mod native_tx_host_env {
                                 iterators,
                                 verifiers,
                                 gas_meter,
-                                result_buffer,
+                            result_buffer,
+                            tx_index,
                                 vp_wasm_cache,
                                 vp_cache_dir: _,
                                 tx_wasm_cache,
@@ -255,6 +258,7 @@ mod native_tx_host_env {
                                 iterators,
                                 verifiers,
                                 gas_meter,
+                                tx_index,
                                 result_buffer,
                                 vp_wasm_cache,
                                 tx_wasm_cache,
@@ -274,6 +278,7 @@ mod native_tx_host_env {
                     #[no_mangle]
                     extern "C" fn extern_fn_name( $($arg: $type),* ) -> $ret {
                         with(|TestTxEnv {
+                            tx_index,
                                 storage,
                                 write_log,
                                 iterators,
@@ -293,6 +298,7 @@ mod native_tx_host_env {
                                 iterators,
                                 verifiers,
                                 gas_meter,
+                                tx_index,
                                 result_buffer,
                                 vp_wasm_cache,
                                 tx_wasm_cache,
@@ -342,6 +348,7 @@ mod native_tx_host_env {
     native_host_fn!(tx_emit_ibc_event(event_ptr: u64, event_len: u64));
     native_host_fn!(tx_get_chain_id(result_ptr: u64));
     native_host_fn!(tx_get_block_height() -> u64);
+    native_host_fn!(tx_get_tx_index() -> u32);
     native_host_fn!(tx_get_block_time() -> i64);
     native_host_fn!(tx_get_block_hash(result_ptr: u64));
     native_host_fn!(tx_get_block_epoch() -> u64);

tests/src/vm_host_env/vp.rs
@@ -6,7 +6,7 @@ use namada::ledger::storage::testing::TestStorage;
 use namada::ledger::storage::write_log::WriteLog;
 use namada::proto::Tx;
 use namada::types::address::{self, Address};
-use namada::types::storage::{self, Key};
+use namada::types::storage::{self, Key, TxIndex};
 use namada::vm::prefix_iter::PrefixIterators;
 use namada::vm::wasm::{self, VpCache};
 use namada::vm::{self, WasmCacheRwAccess};
@@ -32,6 +32,7 @@ pub struct TestVpEnv {
     pub iterators: PrefixIterators<'static, MockDB>,
     pub gas_meter: VpGasMeter,
     pub tx: Tx,
+    pub tx_index: TxIndex,
     pub keys_changed: BTreeSet<storage::Key>,
     pub verifiers: BTreeSet<Address>,
     pub eval_runner: native_vp_host_env::VpEval,
@@ -57,6 +58,7 @@ impl Default for TestVpEnv {
             iterators: PrefixIterators::default(),
             gas_meter: VpGasMeter::default(),
             tx: Tx::new(vec![], None),
+            tx_index: TxIndex::default(),
             keys_changed: BTreeSet::default(),
             verifiers: BTreeSet::default(),
             eval_runner,
@@ -234,7 +236,8 @@ mod native_vp_host_env {
                                 write_log,
                                 iterators,
                                 gas_meter,
-                                tx,
+                            tx,
+                            tx_index,
                                 keys_changed,
                                 verifiers,
                                 eval_runner,
@@ -250,6 +253,7 @@ mod native_vp_host_env {
                                 iterators,
                                 gas_meter,
                                 tx,
+                                tx_index,
                                 verifiers,
                                 result_buffer,
                                 keys_changed,
@@ -276,7 +280,8 @@ mod native_vp_host_env {
                                 write_log,
                                 iterators,
                                 gas_meter,
-                                tx,
+                            tx,
+                            tx_index,
                                 keys_changed,
                                 verifiers,
                                 eval_runner,
@@ -292,6 +297,7 @@ mod native_vp_host_env {
                                 iterators,
                                 gas_meter,
                                 tx,
+                                tx_index,
                                 verifiers,
                                 result_buffer,
                                 keys_changed,
@@ -320,6 +326,7 @@ mod native_vp_host_env {
     native_host_fn!(vp_iter_post_next(iter_id: u64) -> i64);
     native_host_fn!(vp_get_chain_id(result_ptr: u64));
     native_host_fn!(vp_get_block_height() -> u64);
+    native_host_fn!(vp_get_tx_index() -> u32);
     native_host_fn!(vp_get_block_hash(result_ptr: u64));
     native_host_fn!(vp_get_tx_code_hash(result_ptr: u64));
     native_host_fn!(vp_get_block_epoch() -> u64);