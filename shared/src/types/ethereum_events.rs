f8b8ac1ca30b8eaaa57c0e8abe0c9e2a6e6b9cc3
James Hiew <james@chainfork.com>
2022-09-05 17:14:00 +0100

    Fix value of USDC_ERC20_ETH_ADDRESS

--- shared/src/types/ethereum_events.rs
+++ shared/src/types/ethereum_events.rs
@@ -292,10 +292,8 @@ pub mod testing {
     ]);
     pub const USDC_ERC20_ETH_ADDRESS_CHECKSUMMED: &str =
         "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-    // TODO: change this to match what [`USDC_ERC20_ETH_ADDRESS_CHECKSUMMED`]
-    // evaluates to
     pub const USDC_ERC20_ETH_ADDRESS: EthAddress = EthAddress([
-        0, 23, 84, 116, 232, 144, 148, 196, 77, 169, 139, 149, 78, 237, 234,
-        196, 149, 39, 29, 15,
+        160, 184, 105, 145, 198, 33, 139, 54, 193, 209, 157, 74, 46, 158, 176,
+        206, 54, 6, 235, 72,
     ]);
 }
