b3d27027e0bb4adba7774774d9caca9967455d4e
brentstone <brentstone01@gmail.com>
2022-09-10 02:25:01 +0200

    refactor and upgrade apply_inflation and rewards controller, including PoS parameter read and write

--- proof_of_stake/src/rewards.rs
+++ proof_of_stake/src/rewards.rs
@@ -3,8 +3,6 @@
 use rust_decimal::Decimal;
 use thiserror::Error;
 
-use crate::types::VotingPower;
-
 /// Errors during rewards calculation
 #[derive(Debug, Error)]
 pub enum RewardsError {
@@ -18,7 +16,7 @@ pub enum RewardsError {
     CoeffsNotSet,
 }
 
-/// Three different ways to get PoS rewards
+/// Holds coefficients for the three different ways to get PoS rewards
 #[derive(Debug, Copy, Clone)]
 pub struct PosRewards {
     proposer_coeff: Decimal,
@@ -26,23 +24,24 @@ pub struct PosRewards {
     active_val_coeff: Decimal,
 }
 
-/// bing
+/// Holds relevant PoS parameters and is used to calculate the coefficients for
+/// the rewards
 #[derive(Debug, Copy, Clone)]
 pub struct PosRewardsCalculator {
-    proposer_param: u64,
-    signer_param: u64,
-    signing_stake: VotingPower,
-    total_stake: VotingPower,
+    proposer_param: Decimal,
+    signer_param: Decimal,
+    signing_stake: u64,
+    total_stake: u64,
     pos_rewards: Option<PosRewards>,
 }
 
 impl PosRewardsCalculator {
-    /// new
+    /// Instantiate a new PosRewardsCalculator
     pub fn new(
-        proposer_param: u64,
-        signer_param: u64,
-        signing_stake: VotingPower,
-        total_stake: VotingPower,
+        proposer_param: Decimal,
+        signer_param: Decimal,
+        signing_stake: u64,
+        total_stake: u64,
     ) -> Self {
         Self {
             proposer_param,
@@ -53,7 +52,7 @@ impl PosRewardsCalculator {
         }
     }
 
-    /// descr
+    /// Calculate the reward coefficients
     pub fn set_reward_coeffs(&mut self) -> Result<(), RewardsError> {
         // TODO: think about possibility of u64 overflow
         let votes_needed = self.get_min_required_votes();
@@ -61,13 +60,13 @@ impl PosRewardsCalculator {
             return Err(RewardsError::InsufficentVotes);
         }
 
-        // Logic for determining the coefficients (WIP WIP WIP)
-        let proposer_coeff: Decimal = (self.proposer_param
-            * (u64::from(self.signing_stake) - votes_needed))
-            .into();
-        let signer_coeff: Decimal = self.signer_param.into();
-        let active_val_coeff =
-            Decimal::new(1, 0) - proposer_coeff - signer_coeff;
+        // Logic for determining the coefficients
+        // TODO: error handling to ensure proposer_coeff is > 0?
+        let proposer_coeff = self.proposer_param
+            * Decimal::from(self.signing_stake - votes_needed)
+            / Decimal::from(self.total_stake);
+        let signer_coeff = self.signer_param;
+        let active_val_coeff = dec!(1.0) - proposer_coeff - signer_coeff;
 
         self.pos_rewards = Some(PosRewards {
             proposer_coeff,
@@ -78,10 +77,9 @@ impl PosRewardsCalculator {
         Ok(())
     }
 
-    /// Implement as ceiling (2/3) * validator set size
+    /// Implement as ceiling (2/3) * validator set stake
     fn get_min_required_votes(&self) -> u64 {
-        let num = 2 * u64::from(self.total_stake);
-        (num + 3 - 1) / 3
+        ((2 * self.total_stake) + 3 - 1) / 3
     }
 
     /// get struct of the reward coefficients
