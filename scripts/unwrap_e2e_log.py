eb798c95c21c7fba1fe04e018a17b1c995058913
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-09-13 17:11:52 +0100

    Add a script to unwrap e2e log output

---
+++ scripts/unwrap_e2e_log.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import re
+import sys
+
+UNICODE = re.compile(r'\\u{([\da-fA-F]+)}')
+
+def main():
+    if len(sys.argv) > 1:
+        with open(sys.argv[1], 'r') as f:
+            process_file(f)
+    else:
+        process_file(sys.stdin)
+
+def process_file(f):
+    for line in f.readlines():
+        process_line(line)
+    sys.stdout.flush()
+
+def process_line(line):
+    prefix = 'read: '
+    for m in UNICODE.findall(line):
+        line = line.replace(f'\\u{{{m}}}', f'\\u{int(m, 16):04x}')
+    line = eval(line[len(prefix):])
+    sys.stdout.write(line)
+
+if __name__ == '__main__':
+    main()
