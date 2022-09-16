Gal-Shut/Anoma

multitoken transfer

apps/src/lib/client/tx.rs
@@ -15,7 +15,7 @@ use anoma::types::address::{xan as m1t, Address};
 use anoma::types::governance::{OfflineProposal, Proposal};
 use anoma::types::key::*;
 use anoma::types::nft::{self, Nft, NftToken};
-use anoma::types::storage::{Key, Epoch};
+use anoma::types::storage::{Epoch, Key};
 use anoma::types::token::Amount;
 use anoma::types::transaction::governance::InitProposalData;
 use anoma::types::transaction::nft::{CreateNft, MintNft};
@@ -417,13 +417,16 @@ pub async fn submit_transfer(ctx: Context, args: args::TxTransfer) {
         }
     }
     // Check source balance
-    let balance_key = match args.source_sub_prefix {
+    let (source_sub_prefix, balance_key) = match args.source_sub_prefix {
         Some(sub_prefix) => {
             let sub_prefix = Key::parse(sub_prefix).unwrap();
             let prefix = token::multitoken_balance_prefix(&token, &sub_prefix);
-            token::multitoken_balance_key(&prefix, &source)
+            (
+                Some(sub_prefix),
+                token::multitoken_balance_key(&prefix, &source),
+            )
         }
-        None => token::balance_key(&token, &source),
+        None => (None, token::balance_key(&token, &source)),
     };
     let client = HttpClient::new(args.tx.ledger_address.clone()).unwrap();
     match rpc::query_storage_value::<token::Amount>(&client, &balance_key).await
@@ -451,11 +454,15 @@ pub async fn submit_transfer(ctx: Context, args: args::TxTransfer) {
             }
         }
     }
+    let target_sub_prefix =
+        args.target_sub_prefix.map(|p| Key::parse(p).unwrap());
     let tx_code = ctx.read_wasm(TX_TRANSFER_WASM);
     let transfer = token::Transfer {
         source,
         target,
         token,
+        source_sub_prefix,
+        target_sub_prefix,
         amount: args.amount,
     };
     tracing::debug!("Transfer data {:?}", transfer);

anoma/namada

apps/src/lib/client/tx.rs
@@ -16,7 +16,7 @@ use namada::types::governance::{
 };
 use namada::types::key::*;
 use namada::types::nft::{self, Nft, NftToken};
-use namada::types::storage::Epoch;
+use namada::types::storage::{Epoch, Key};
 use namada::types::token::Amount;
 use namada::types::transaction::governance::{
     InitProposalData, VoteProposalData,
@@ -415,13 +415,16 @@ pub async fn submit_transfer(ctx: Context, args: args::TxTransfer) {
         }
     }
     // Check source balance
-    let balance_key = match args.source_sub_prefix {
+    let (source_sub_prefix, balance_key) = match args.source_sub_prefix {
         Some(sub_prefix) => {
             let sub_prefix = Key::parse(sub_prefix).unwrap();
             let prefix = token::multitoken_balance_prefix(&token, &sub_prefix);
-            token::multitoken_balance_key(&prefix, &source)
+            (
+                Some(sub_prefix),
+                token::multitoken_balance_key(&prefix, &source),
+            )
         }
-        None => token::balance_key(&token, &source),
+        None => (None, token::balance_key(&token, &source)),
     };
     let client = HttpClient::new(args.tx.ledger_address.clone()).unwrap();
     match rpc::query_storage_value::<token::Amount>(&client, &balance_key).await
@@ -449,11 +452,15 @@ pub async fn submit_transfer(ctx: Context, args: args::TxTransfer) {
             }
         }
     }
+    let target_sub_prefix =
+        args.target_sub_prefix.map(|p| Key::parse(p).unwrap());
     let tx_code = ctx.read_wasm(TX_TRANSFER_WASM);
     let transfer = token::Transfer {
         source,
         target,
         token,
+        source_sub_prefix,
+        target_sub_prefix,
         amount: args.amount,
     };
     tracing::debug!("Transfer data {:?}", transfer);