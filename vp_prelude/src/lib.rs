6d8ee5850ce3ad20d62d74d06afae17bd8622525
Marco Granelli <marco.granelli@ymail.com>
2022-09-21 10:50:41 +0200

    Skip tx whitelist for proposal code

--- vp_prelude/src/lib.rs
+++ vp_prelude/src/lib.rs
@@ -33,6 +33,27 @@ pub fn is_vp_whitelisted(vp_bytes: &[u8]) -> bool {
     whitelist.is_empty() || whitelist.contains(&vp_hash.to_string())
 }
 
+/// Checks if a proposal id is being executed
+pub fn is_proposal_accepted(proposal_id: u64) -> bool {
+    let proposal_execution_key =
+        gov_storage::get_proposal_execution_key(proposal_id);
+
+    has_key_pre(&proposal_execution_key.to_string())
+}
+
+/// Checks whether a transaction is valid, which happens in two cases:
+/// - tx is whitelisted, or
+/// - tx is executed by an approved governance proposal (no need to be whitelisted)
+pub fn is_valid_tx(tx_data: &[u8]) -> bool {
+    if is_tx_whitelisted() {
+        return true
+    } else {
+        let proposal_id = u64::try_from_slice(tx_data).ok();
+
+        proposal_id.map_or(false, |id| is_proposal_accepted(id))
+    }
+}
+
 /// Log a string in a debug build. The message will be printed at the
 /// `tracing::Level::Info`. Any `debug_log!` statements are only enabled in
 /// non optimized builds by default. An optimized build will not execute
