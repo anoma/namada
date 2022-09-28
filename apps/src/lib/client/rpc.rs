756fc57d0dddaa806492e813974056139c28db6f
brentstone <brentstone01@gmail.com>
2022-09-28 14:16:35 -0400

    commission rate: query + refactor validator change tx

--- apps/src/lib/client/rpc.rs
+++ apps/src/lib/client/rpc.rs
@@ -1024,6 +1024,47 @@ pub async fn query_voting_power(ctx: Context, args: args::QueryVotingPower) {
     println!("Total voting power: {}", total_voting_power);
 }
 
+/// Query PoS commssion rate
+pub async fn query_commission_rate(ctx: Context, args: args::QueryCommissionRate) {
+    let epoch = match args.epoch {
+        Some(epoch) => epoch,
+        None => query_epoch(args.query.clone()).await,
+    };
+    let client = HttpClient::new(args.query.ledger_address).unwrap();
+
+    match args.validator {
+        Some(validator) => {
+            let validator = ctx.get(&validator);
+            let validator_commission_key = pos::validator_commission_rate_key(&validator);
+            let commission_rates = query_storage_value::<pos::CommissionRates>(
+                &client,
+                &validator_commission_key,
+            )
+            .await;
+            let commission_rates = commission_rates
+                .expect("No commission rate found ");
+            match commission_rates.get(epoch) {
+                Some(rate) => {
+                    println!(
+                        "Validator {} commission rate: {}",
+                        validator.encode(),
+                        *rate
+                    )
+                }
+                None => {
+                    println!("No commission rate found for {} in epoch {}",
+                        validator.encode(),
+                        epoch
+                    )
+                }
+            }
+        }
+        None => {
+            println!("No validator found from the args")
+        }
+    }
+}
+
 /// Query PoS slashes
 pub async fn query_slashes(ctx: Context, args: args::QuerySlashes) {
     let client = HttpClient::new(args.query.ledger_address).unwrap();
