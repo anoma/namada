dba2e2bea1672b307f96f70dddb3abf3c1c262cd
James Hiew <james@chainfork.com>
2022-10-06 10:33:10 +0100

    Fix up events endpoint with bounded channel changes

--- apps/src/lib/node/ledger/ethereum_node/test_tools/events_endpoint.rs
+++ apps/src/lib/node/ledger/ethereum_node/test_tools/events_endpoint.rs
@@ -1,7 +1,8 @@
 use borsh::BorshDeserialize;
 use namada::types::ethereum_events::EthereumEvent;
 use tokio::macros::support::poll_fn;
-use tokio::sync::mpsc::UnboundedSender;
+use tokio::sync::mpsc::Sender as BoundedSender;
+use warp::reply::WithStatus;
 use warp::Filter;
 
 /// The default IP address and port on which the events endpoint will listen.
@@ -15,38 +16,14 @@ const EVENTS_POST_ENDPOINT: &str = "eth_events";
 /// and then forwards them to `sender`. It shuts down if `abort_sender` is
 /// closed.
 pub fn serve(
-    sender: UnboundedSender<EthereumEvent>,
+    sender: BoundedSender<EthereumEvent>,
     mut abort_sender: tokio::sync::oneshot::Sender<()>,
 ) -> tokio::task::JoinHandle<()> {
+    tracing::info!(?DEFAULT_LISTEN_ADDR, "Ethereum event endpoint is starting");
     let eth_events = warp::post()
         .and(warp::path(EVENTS_POST_ENDPOINT))
         .and(warp::body::bytes())
-        .map(move |bytes: bytes::Bytes| {
-            tracing::info!(len = bytes.len(), "Received request");
-            let event = match EthereumEvent::try_from_slice(&bytes) {
-                Ok(event) => event,
-                Err(error) => {
-                    tracing::warn!(?error, "Couldn't handle request");
-                    return warp::reply::with_status(
-                        "Bad request",
-                        warp::http::StatusCode::BAD_REQUEST,
-                    );
-                }
-            };
-            tracing::debug!("Serialized event - {:#?}", event);
-            match sender.send(event) {
-                Ok(()) => {
-                    warp::reply::with_status("OK", warp::http::StatusCode::OK)
-                }
-                Err(error) => {
-                    tracing::warn!(?error, "Couldn't send event");
-                    warp::reply::with_status(
-                        "Internal server error",
-                        warp::http::StatusCode::INTERNAL_SERVER_ERROR,
-                    )
-                }
-            }
-        });
+        .then(move |bytes: bytes::Bytes| send(bytes, sender.clone()));
 
     let (_, server) = warp::serve(eth_events).bind_with_graceful_shutdown(
         DEFAULT_LISTEN_ADDR,
@@ -64,4 +41,33 @@ pub fn serve(
     );
 
     tokio::task::spawn(server)
+}
+
+/// Callback to send out events from the oracle
+async fn send(
+    bytes: bytes::Bytes,
+    sender: BoundedSender<EthereumEvent>,
+) -> WithStatus<&'static str> {
+    tracing::info!(len = bytes.len(), "Received request");
+    let event = match EthereumEvent::try_from_slice(&bytes) {
+        Ok(event) => event,
+        Err(error) => {
+            tracing::warn!(?error, "Couldn't handle request");
+            return warp::reply::with_status(
+                "Bad request",
+                warp::http::StatusCode::BAD_REQUEST,
+            );
+        }
+    };
+    tracing::debug!("Serialized event - {:#?}", event);
+    match sender.send(event).await {
+        Ok(()) => warp::reply::with_status("OK", warp::http::StatusCode::OK),
+        Err(error) => {
+            tracing::warn!(?error, "Couldn't send event");
+            warp::reply::with_status(
+                "Internal server error",
+                warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+            )
+        }
+    }
 }
