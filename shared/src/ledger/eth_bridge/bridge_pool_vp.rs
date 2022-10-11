38a65e6ae196fe6ad42fd5d9a91f97d7fb147bf8
satan <jacob.turner870@gmail.com>
2022-10-11 15:06:05 +0200

    [feat]: Updated bridge pool vp with the the new merklized storage

--- shared/src/ledger/eth_bridge/bridge_pool_vp.rs
+++ shared/src/ledger/eth_bridge/bridge_pool_vp.rs
@@ -25,6 +25,7 @@ use crate::ledger::storage::{DBIter, DB};
 use crate::proto::SignedTxData;
 use crate::types::address::{xan, Address, InternalAddress};
 use crate::types::eth_bridge_pool::PendingTransfer;
+use crate::types::keccak::encode::Encode;
 use crate::types::storage::Key;
 use crate::types::token::{balance_key, Amount};
 use crate::vm::WasmCacheAccess;
