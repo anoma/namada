acff900de698794f282570e8344107f6c5a59297
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-10-12 11:31:36 +0100

    Improve the docstring of the event log module

--- apps/src/lib/node/ledger/events/log.rs
+++ apps/src/lib/node/ledger/events/log.rs
@@ -1,7 +1,7 @@
 //! A log to store events emitted by `FinalizeBlock` calls in the ledger.
 //!
-//! The log is flushed every other `N` block heights, where `N` is a
-//! configurable parameter.
+//! The log is flushed every other `N` block heights, or every other `E`
+//! `FinalizeBlock` events, where `N` and `E` are configurable parameters.
 
 pub mod dumb_queries;
 
