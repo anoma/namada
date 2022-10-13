edcbeaf23aef61ba264b0a66fd1caa8637915455
James Hiew <james@chainfork.com>
2022-10-13 17:09:48 +0100

    Use info logs for the moment

--- tests/src/e2e/setup.rs
+++ tests/src/e2e/setup.rs
@@ -664,9 +664,9 @@ where
     );
 
     run_cmd
-        .env("ANOMA_LOG", "debug")
+        .env("ANOMA_LOG", "info")
         .env("ANOMA_TM_STDOUT", "true")
-        .env("TM_LOG_LEVEL", "debug")
+        .env("TM_LOG_LEVEL", "info")
         .env("ANOMA_LEDGER__ETHEREUM__MODE", "Off")
         .env("ANOMA_LOG_COLOR", "false")
         .current_dir(working_dir)
