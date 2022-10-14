41ecd5b216a5c60a8e091f154e6f1a23f9f6479a
satan <jacob.turner870@gmail.com>
2022-10-14 17:08:43 +0200

    [feat]: Added recovery ids to secp256k1 signatures

--- apps/src/lib/node/ledger/shell/queries.rs
+++ apps/src/lib/node/ledger/shell/queries.rs
@@ -16,8 +16,8 @@ use namada::types::address::Address;
 use namada::types::eth_bridge_pool::{
     MultiSignedMerkleRoot, PendingTransfer, RelayProof,
 };
-use namada::types::ethereum_events::EthAddress;
 use namada::types::keccak::encode::Encode;
+use namada::types::ethereum_events::EthAddress;
 use namada::types::key;
 use namada::types::key::dkg_session_keys::DkgPublicKey;
 use namada::types::storage::MembershipProof::BridgePool;
