29175fe677f32d2f19856a45720cb68738effd1f
James Hiew <james@chainfork.com>
2022-10-20 17:44:31 +0100

    Add more types and docstrings

--- apps/src/lib/config/ethereum_bridge/params.rs
+++ apps/src/lib/config/ethereum_bridge/params.rs
@@ -1,6 +1,14 @@
 //! Blockchain-level parameters for the configuration of the Ethereum bridge.
 use serde::{Deserialize, Serialize};
 
+/// Represents a configuration value for an Ethereum address e.g.
+/// 0x6B175474E89094C44Da98b954EedeAC495271d0F
+#[derive(Clone, Eq, PartialEq, Debug, Deserialize, Serialize)]
+#[repr(transparent)]
+pub struct Address(String);
+
+/// Represents a configuration value for the minimum number of
+/// confirmations an Ethereum event must reach before it can be acted on.
 #[derive(Clone, Copy, Eq, PartialEq, Debug, Deserialize, Serialize)]
 #[repr(transparent)]
 pub struct MinimumConfirmations(u64);
@@ -11,29 +19,32 @@ impl Default for MinimumConfirmations {
     }
 }
 
+/// Represents chain parameters for the Ethereum bridge.
 #[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize)]
 pub struct Config {
     /// Minimum number of confirmations needed to trust an Ethereum branch.
     /// This must be at least one.
     pub min_confirmations: MinimumConfirmations,
     /// The addresses of the Ethereum contracts that need to be directly known
-    /// by validators
-    pub contract_addresses: Addresses,
+    /// by validators.
+    pub contracts: Contracts,
 }
 
+/// Represents all the Ethereum contracts that need to be directly know about by
+/// validators.
 #[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize)]
-pub struct Addresses {
+pub struct Contracts {
     /// The Ethereum address of the ERC20 contract that represents this chain's
-    /// native token e.g. 0x6B175474E89094C44Da98b954EedeAC495271d0F
-    pub native_erc20: String,
-    /// The Ethereum address of the bridge contract e.g.
-    /// 0x6B175474E89094C44Da98b954EedeAC495271d0F
-    pub bridge: EthereumContract,
-    /// The Ethereum address of the governance contract e.g.
-    /// 0x6B175474E89094C44Da98b954EedeAC495271d0F
-    pub governance: EthereumContract,
+    /// native token.
+    pub native_erc20: Address,
+    /// The Ethereum address of the bridge contract.
+    pub bridge: UpgradeableContract,
+    /// The Ethereum address of the governance contract.
+    pub governance: UpgradeableContract,
 }
 
+/// Represents a configuration value for the version of a contract that can be
+/// upgraded. Starts from 1.
 #[derive(Clone, Copy, Eq, PartialEq, Debug, Deserialize, Serialize)]
 #[repr(transparent)]
 pub struct ContractVersion(u64);
@@ -44,12 +55,12 @@ impl Default for ContractVersion {
     }
 }
 
+/// Represents an Ethereum contract that may be upgraded.
 #[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize)]
-pub struct EthereumContract {
-    /// The Ethereum address of the contract e.g.
-    /// 0x6B175474E89094C44Da98b954EedeAC495271d0F
-    pub address: String,
-    /// The version of the contract e.g. 1
+pub struct UpgradeableContract {
+    /// The Ethereum address of the contract.
+    pub address: Address,
+    /// The version of the contract. Starts from 1.
     pub version: ContractVersion,
 }
 
@@ -66,17 +77,22 @@ mod tests {
     fn test_round_trip_toml_serde() -> Result<()> {
         let config = Config {
             min_confirmations: MinimumConfirmations::default(),
-            contract_addresses: Addresses {
-                native_erc20: "0x1721b337BBdd2b11f9Eef736d9192a8E9Cba5872"
-                    .to_string(),
-                bridge: EthereumContract {
-                    address: "0x237d915037A1ba79365E84e2b8574301B6D25Ea0"
-                        .to_string(),
+            contracts: Contracts {
+                native_erc20: Address(
+                    "0x1721b337BBdd2b11f9Eef736d9192a8E9Cba5872".to_string(),
+                ),
+                bridge: UpgradeableContract {
+                    address: Address(
+                        "0x237d915037A1ba79365E84e2b8574301B6D25Ea0"
+                            .to_string(),
+                    ),
                     version: ContractVersion::default(),
                 },
-                governance: EthereumContract {
-                    address: "0x308728EEa73538d0edEfd95EF148Eb678F71c71D"
-                        .to_string(),
+                governance: UpgradeableContract {
+                    address: Address(
+                        "0x308728EEa73538d0edEfd95EF148Eb678F71c71D"
+                            .to_string(),
+                    ),
                     version: ContractVersion::default(),
                 },
             },
