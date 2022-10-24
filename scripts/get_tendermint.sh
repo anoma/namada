d014df533c7dc14db67a6a4a484552ef3b159088
Tomas Zemanovic <tomas@heliax.dev>
2022-10-24 12:38:56 +0200

    switch to tendermint fork release v0.1.3-abciplus

--- scripts/get_tendermint.sh
+++ scripts/get_tendermint.sh
@@ -6,7 +6,7 @@ set -Eo pipefail
 # https://github.com/tendermint/tendermint/releases/download/v0.34.13/tendermint_0.34.13_linux_amd64.tar.gz
 # https://github.com/heliaxdev/tendermint/releases/download/v0.1.1-abcipp/tendermint_0.1.0-abcipp_darwin_amd64.tar.gz
 TM_MAJORMINOR="0.1"
-TM_PATCH="2"
+TM_PATCH="3"
 TM_SUFFIX="-abciplus"
 TM_REPO="https://github.com/heliaxdev/tendermint"
 
