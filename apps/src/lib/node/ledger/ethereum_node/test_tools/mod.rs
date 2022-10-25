cd55e32e535fd8e8a4ca6cfc383f0f77264cb06f
satan <jacob.turner870@gmail.com>
2022-10-25 10:29:43 +0200

    [fix]: Formatting

--- apps/src/lib/node/ledger/ethereum_node/test_tools/mod.rs
+++ apps/src/lib/node/ledger/ethereum_node/test_tools/mod.rs
@@ -215,7 +215,8 @@ pub mod mock_web3_client {
                         client.events.push((event_ty, data, height, seen));
                     }
                 }
-                if client.last_block_processed.as_ref() < Some(&block_to_check) {
+                if client.last_block_processed.as_ref() < Some(&block_to_check)
+                {
                     client
                         .blocks_processed
                         .send(block_to_check.clone())
