8d9340a15ee93aff066e9475b067703dd400866d
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-10-26 15:54:12 +0100

    Fetch storage key for a given valset upd vext

--- apps/src/lib/node/ledger/protocol/transactions/validator_set_update/mod.rs
+++ apps/src/lib/node/ledger/protocol/transactions/validator_set_update/mod.rs
@@ -5,6 +5,7 @@
 use std::collections::{HashMap, HashSet};
 
 use eyre::Result;
+use namada::ledger::eth_bridge::storage::vote_tallies;
 use namada::ledger::storage::traits::StorageHasher;
 use namada::ledger::storage::{DBIter, Storage, DB};
 use namada::types::address::Address;
@@ -15,6 +16,7 @@ use namada::types::voting_power::FractionalVotingPower;
 
 use super::ChangedKeys;
 use crate::node::ledger::protocol::transactions::utils;
+use crate::node::ledger::shell::queries::QueriesExt;
 
 impl utils::GetVoters for validator_set_update::VextDigest {
     #[inline]
@@ -31,13 +33,18 @@ where
     D: 'static + DB + for<'iter> DBIter<'iter> + Sync,
     H: 'static + StorageHasher + Sync,
 {
+    if ext.signatures.is_empty() {
+        tracing::debug!("Ignoring empty validator set update");
+        return Ok(Default::default());
+    }
+
     tracing::info!(
         num_votes = ext.signatures.len(),
         "Aggregating new votes for validator set update"
     );
 
     let voting_powers = utils::get_voting_powers(storage, &ext)?;
-    let changed_keys = apply_updates(storage, ext, voting_powers)?;
+    let changed_keys = apply_update(storage, ext, voting_powers)?;
 
     Ok(TxResult {
         changed_keys,
@@ -45,15 +52,39 @@ where
     })
 }
 
-fn apply_updates<D, H>(
-    _storage: &mut Storage<D, H>,
-    _ext: validator_set_update::VextDigest,
+fn apply_update<D, H>(
+    storage: &mut Storage<D, H>,
+    ext: validator_set_update::VextDigest,
     _voting_powers: HashMap<(Address, BlockHeight), FractionalVotingPower>,
 ) -> Result<ChangedKeys>
 where
     D: 'static + DB + for<'iter> DBIter<'iter> + Sync,
     H: 'static + StorageHasher + Sync,
 {
-    tracing::warn!("Called apply_updates() with no side effects");
+    let epoch = {
+        // all votes we gathered are for the same epoch, so
+        // we can just fetch the block height from the first
+        // signature we iterate over, and calculate its cor-
+        // responding epoch
+        let height = ext
+            .signatures
+            .keys()
+            .map(|(_, height)| *height)
+            .by_ref()
+            .next()
+            .expect(
+                "We have at least one signature present in this validator set \
+                 update vote extension digest",
+            );
+
+        storage
+            .get_epoch(height)
+            .expect("The epoch of the given block height should be known")
+    };
+
+    let valset_upd_keys = vote_tallies::Keys::from(&epoch);
+    let _ = valset_upd_keys;
+
+    tracing::warn!("Called apply_update() with no side effects");
     Ok(ChangedKeys::new())
 }
