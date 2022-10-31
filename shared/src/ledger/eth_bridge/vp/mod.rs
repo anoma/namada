47a9848f127d5743cfc1b3bfb2b69ab9e52d12eb
satan <jacob.turner870@gmail.com>
2022-10-31 14:28:31 +0100

    [feat]: Initialized VP substorage

--- shared/src/ledger/eth_bridge/vp/mod.rs
+++ shared/src/ledger/eth_bridge/vp/mod.rs
@@ -17,6 +17,30 @@ use crate::types::storage::Key;
 use crate::types::token::{balance_key, Amount};
 use crate::vm::WasmCacheAccess;
 
+
+/// Initialize the storage owned by the Ethereum Bridge VP.
+///
+/// This means that the amount of escrowed Nam is
+/// initialized to 0.
+pub fn init_storage<D, H>(storage: &mut ledger_storage::Storage<D, H>)
+    where
+        D: ledger_storage::DB + for<'iter> ledger_storage::DBIter<'iter>,
+        H: StorageHasher,
+{
+    let escrow_key = balance_key(&xan(), &super::ADDRESS);
+    storage
+        .write(
+            &escrow_key,
+            Amount::default()
+                .try_to_vec()
+                .expect("Serializing an amount shouldn't fail."),
+        )
+        .expect(
+            "Initializing the escrow balance of the Ethereum Bridge VP shouldn't \
+             fail.",
+        );
+}
+
 /// Validity predicate for the Ethereum bridge
 pub struct EthBridge<'ctx, DB, H, CA>
 where
