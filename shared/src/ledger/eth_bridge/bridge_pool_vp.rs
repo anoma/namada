cc7faae4d762458d626af1c47110eebeb0c0cb2d
satan <jacob.turner870@gmail.com>
2022-10-31 14:28:31 +0100

    [feat]: Initialized VP substorage

--- shared/src/ledger/eth_bridge/bridge_pool_vp.rs
+++ shared/src/ledger/eth_bridge/bridge_pool_vp.rs
@@ -11,7 +11,7 @@
 //! added to the pool and gas fees are submitted appropriately.
 use std::collections::BTreeSet;
 
-use borsh::BorshDeserialize;
+use borsh::{BorshDeserialize, BorshSerialize};
 use eyre::eyre;
 
 use crate::ledger::eth_bridge::storage::bridge_pool::{
@@ -19,7 +19,7 @@ use crate::ledger::eth_bridge::storage::bridge_pool::{
 };
 use crate::ledger::native_vp::{Ctx, NativeVp, StorageReader};
 use crate::ledger::storage::traits::StorageHasher;
-use crate::ledger::storage::{DBIter, DB};
+use crate::ledger::storage::{DBIter, Storage, DB};
 use crate::proto::SignedTxData;
 use crate::types::address::{xan, Address, InternalAddress};
 use crate::types::eth_bridge_pool::PendingTransfer;
@@ -36,6 +36,29 @@ pub struct Error(#[from] eyre::Error);
 enum SignedAmount {
     Positive(Amount),
     Negative(Amount),
+}
+
+/// Initialize the storage owned by the Bridge Pool VP.
+///
+/// This means that the amount of escrowed gas fees is
+/// initialized to 0.
+pub fn init_storage<D, H>(storage: &mut Storage<D, H>)
+where
+    D: DB + for<'iter> DBIter<'iter>,
+    H: StorageHasher,
+{
+    let escrow_key = balance_key(&xan(), &BRIDGE_POOL_ADDRESS);
+    storage
+        .write(
+            &escrow_key,
+            Amount::default()
+                .try_to_vec()
+                .expect("Serializing an amount shouldn't fail."),
+        )
+        .expect(
+            "Initializing the escrow balance of the Bridge pool VP shouldn't \
+             fail.",
+        );
 }
 
 /// Validity predicate for the Ethereum bridge
