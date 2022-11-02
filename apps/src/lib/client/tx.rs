eb5e9197f0fce63c322b8688dd313ab51bd57056
Christopher Goes <cwgoes@pluranimity.org>
2022-11-02 17:21:14 +0100

    fixup: use wallet address in process_tx call

--- apps/src/lib/client/tx.rs
+++ apps/src/lib/client/tx.rs
@@ -1665,7 +1665,8 @@ pub async fn submit_transfer(mut ctx: Context, args: args::TxTransfer) {
         .expect("Encoding tx data shouldn't fail");
 
     let tx = Tx::new(tx_code, Some(data));
-    process_tx(ctx, &args.tx, tx, Some(&args.source)).await;
+    let signing_address = TxSigningKey::WalletAddress(args.source.to_address());
+    process_tx(ctx, &args.tx, tx, signing_address).await;
 }
 
 pub async fn submit_init_proposal(mut ctx: Context, args: args::InitProposal) {
