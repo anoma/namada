df84dee1b6fc6c541b1398d7e6e05e045c458bb4
James Hiew <james@chainfork.com>
2022-11-07 15:38:08 +0000

    fix: require_latest_height check should skip for requests with height 0

--- shared/src/ledger/queries/mod.rs
+++ shared/src/ledger/queries/mod.rs
@@ -10,6 +10,7 @@ pub use types::{
 
 use super::storage::{DBIter, StorageHasher, DB};
 use super::storage_api;
+use crate::types::storage::BlockHeight;
 
 #[macro_use]
 mod router;
@@ -47,7 +48,9 @@ where
     D: 'static + DB + for<'iter> DBIter<'iter> + Sync,
     H: 'static + StorageHasher + Sync,
 {
-    if request.height != ctx.storage.last_height {
+    if request.height != BlockHeight(0)
+        && request.height != ctx.storage.last_height
+    {
         return Err(storage_api::Error::new_const(
             "This query doesn't support arbitrary block heights, only the \
              latest committed block height ('0' can be used as a special \
