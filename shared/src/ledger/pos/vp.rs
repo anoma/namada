105410e2664e740cb3dc3d5e6d81110045b6a1ea
Tomáš Zemanovič <tomas@heliax.dev>
2022-11-09 10:18:36 +0100

    pos/vp: do not return early on unknown permitted key

--- shared/src/ledger/pos/vp.rs
+++ shared/src/ledger/pos/vp.rs
@@ -282,7 +282,6 @@ where
                 return Ok(false);
             } else {
                 // Unknown changes anywhere else are permitted
-                return Ok(true);
             }
         }
 
