aae550a58a01a64c9c8363e011724f7d7603b112
James Hiew <james@chainfork.com>
2022-11-14 14:28:07 +0000

    Add a test for votes::storage::write

--- shared/src/ledger/protocol/transactions/votes/storage.rs
+++ shared/src/ledger/protocol/transactions/votes/storage.rs
@@ -22,3 +22,47 @@ where
     storage.write(&keys.voting_power(), &tally.voting_power.try_to_vec()?)?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::BTreeMap;
+
+    use super::*;
+    use crate::ledger::storage::testing::TestStorage;
+    use crate::types::address;
+    use crate::types::ethereum_events::EthereumEvent;
+    use crate::types::voting_power::FractionalVotingPower;
+
+    #[test]
+    fn test_write_tally() {
+        let mut storage = TestStorage::default();
+        let event = EthereumEvent::TransfersToNamada {
+            nonce: 0.into(),
+            transfers: vec![],
+        };
+        let keys = vote_tallies::Keys::from(&event);
+        let tally = Tally {
+            voting_power: FractionalVotingPower::new(1, 3).unwrap(),
+            seen_by: BTreeMap::from([(
+                address::testing::established_address_1(),
+                10.into(),
+            )]),
+            seen: false,
+        };
+
+        let result = write(&mut storage, &keys, &event, &tally);
+
+        assert!(result.is_ok());
+        let (body, _) = storage.read(&keys.body()).unwrap();
+        assert_eq!(body, Some(event.try_to_vec().unwrap()));
+        let (seen, _) = storage.read(&keys.seen()).unwrap();
+        assert_eq!(seen, Some(tally.seen.try_to_vec().unwrap()));
+        let (seen_by, _) = storage.read(&keys.seen_by()).unwrap();
+        assert_eq!(seen_by, Some(tally.seen_by.try_to_vec().unwrap()));
+        let (voting_power, _) = storage.read(&keys.voting_power()).unwrap();
+        assert_eq!(
+            voting_power,
+            Some(tally.voting_power.try_to_vec().unwrap())
+        );
+    }
+}
