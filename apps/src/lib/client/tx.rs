e3660e1cae4c9649dbe8ac861a00caeaf72692d9
Tomáš Zemanovič <tomas@heliax.dev>
2022-12-01 12:41:39 +0100

    tx/transfer: use the same default signer for wrapper tx

--- apps/src/lib/client/tx.rs
+++ apps/src/lib/client/tx.rs
@@ -1648,8 +1648,7 @@ pub async fn submit_transfer(mut ctx: Context, args: args::TxTransfer) {
         .expect("Encoding tx data shouldn't fail");
 
     let tx = Tx::new(tx_code, Some(data));
-    let signing_address = TxSigningKey::WalletAddress(args.source.to_address());
-    process_tx(ctx, &args.tx, tx, signing_address).await;
+    process_tx(ctx, &args.tx, tx, default_signer).await;
 }
 
 pub async fn submit_ibc_transfer(ctx: Context, args: args::TxIbcTransfer) {
