84d10b352783f48d108a4da2fef3665ed6688efa
brentstone <brentstone01@gmail.com>
2022-12-09 08:45:08 -0500

    new lazy functions in tx_prelude

--- tx_prelude/src/proof_of_stake.rs
+++ tx_prelude/src/proof_of_stake.rs
@@ -13,9 +13,13 @@ use namada_proof_of_stake::storage::{
     Unbonds, ValidatorConsensusKeys, ValidatorDeltas, ValidatorSets,
 };
 use namada_proof_of_stake::types::{CommissionRates, ValidatorStates};
+use namada_proof_of_stake::{
+    become_validator_new, bond_tokens_new,
+    change_validator_commission_rate_new, read_pos_params, unbond_tokens_new,
+    withdraw_tokens_new,
+};
 pub use namada_proof_of_stake::{
-    bond_tokens_new, epoched, parameters, types, PosActions as PosWrite,
-    PosReadOnly as PosRead,
+    epoched, parameters, types, PosActions as PosWrite, PosReadOnly as PosRead,
 };
 use rust_decimal::Decimal;
 
@@ -79,6 +83,25 @@ impl Ctx {
         )
     }
 
+    /// NEW: Unbond self-bonded tokens from a validator when `source` is `None`
+    /// or equal to the `validator` address, or unbond delegated tokens from
+    /// the `source` to the `validator`.
+    pub fn unbond_tokens_new(
+        &mut self,
+        source: Option<&Address>,
+        validator: &Address,
+        amount: token::Amount,
+    ) -> TxResult {
+        let current_epoch = self.get_block_epoch()?;
+        unbond_tokens_new(
+            self,
+            source,
+            validator,
+            token::Change::from(amount),
+            current_epoch,
+        )
+    }
+
     /// Withdraw unbonded tokens from a self-bond to a validator when `source`
     /// is `None` or equal to the `validator` address, or withdraw unbonded
     /// tokens delegated to the `validator` to the `source`.
@@ -96,6 +119,18 @@ impl Ctx {
         )
     }
 
+    /// NEW: Withdraw unbonded tokens from a self-bond to a validator when
+    /// `source` is `None` or equal to the `validator` address, or withdraw
+    /// unbonded tokens delegated to the `validator` to the `source`.
+    pub fn withdraw_tokens_new(
+        &mut self,
+        source: Option<&Address>,
+        validator: &Address,
+    ) -> EnvResult<token::Amount> {
+        let current_epoch = self.get_block_epoch()?;
+        withdraw_tokens_new(self, source, validator, current_epoch)
+    }
+
     /// Change validator commission rate.
     pub fn change_validator_commission_rate(
         &mut self,
@@ -104,6 +139,21 @@ impl Ctx {
     ) -> TxResult {
         let current_epoch = self.get_block_epoch()?;
         namada_proof_of_stake::PosActions::change_validator_commission_rate(
+            self,
+            validator,
+            *rate,
+            current_epoch,
+        )
+    }
+
+    /// NEW: Change validator commission rate.
+    pub fn change_validator_commission_rate_new(
+        &mut self,
+        validator: &Address,
+        rate: &Decimal,
+    ) -> TxResult {
+        let current_epoch = self.get_block_epoch()?;
+        change_validator_commission_rate_new(
             self,
             validator,
             *rate,
@@ -136,6 +186,44 @@ impl Ctx {
         self.write(&dkg_pk_key, &dkg_key)?;
 
         self.become_validator(
+            &validator_address,
+            &consensus_key,
+            current_epoch,
+            commission_rate,
+            max_commission_rate_change,
+        )?;
+
+        Ok(validator_address)
+    }
+
+    /// NEW: Attempt to initialize a validator account. On success, returns the
+    /// initialized validator account's address.
+    pub fn init_validator_new(
+        &mut self,
+        InitValidator {
+            account_key,
+            consensus_key,
+            protocol_key,
+            dkg_key,
+            commission_rate,
+            max_commission_rate_change,
+            validator_vp_code,
+        }: InitValidator,
+    ) -> EnvResult<Address> {
+        let current_epoch = self.get_block_epoch()?;
+        // Init validator account
+        let validator_address = self.init_account(&validator_vp_code)?;
+        let pk_key = key::pk_key(&validator_address);
+        self.write(&pk_key, &account_key)?;
+        let protocol_pk_key = key::protocol_pk_key(&validator_address);
+        self.write(&protocol_pk_key, &protocol_key)?;
+        let dkg_pk_key = key::dkg_session_keys::dkg_pk_key(&validator_address);
+        self.write(&dkg_pk_key, &dkg_key)?;
+
+        let params = read_pos_params(self)?;
+        become_validator_new(
+            self,
+            &params,
             &validator_address,
             &consensus_key,
             current_epoch,
