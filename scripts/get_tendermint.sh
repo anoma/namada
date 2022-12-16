a621bf960c95f5ddcc3abecbe2da265589de7766
Gianmarco Fraccaroli <>
2022-12-16 17:03:59 +0100

    fix: tendermint version update

--- scripts/get_tendermint.sh
+++ scripts/get_tendermint.sh
@@ -4,9 +4,9 @@ set -Eo pipefail
 
 # an examplary download-url
 # https://github.com/tendermint/tendermint/releases/download/v0.34.13/tendermint_0.34.13_linux_amd64.tar.gz
-# https://github.com/heliaxdev/tendermint/releases/download/v0.1.1-abcipp/tendermint_0.1.0-abcipp_darwin_amd64.tar.gz
+# https://github.com/heliaxdev/tendermint/releases/download/v0.1.4-abcipp/tendermint_0.1.0-abcipp_darwin_amd64.tar.gz
 TM_MAJORMINOR="0.1"
-TM_PATCH="1"
+TM_PATCH="4"
 TM_SUFFIX="-abcipp"
 TM_REPO="https://github.com/heliaxdev/tendermint"
 
