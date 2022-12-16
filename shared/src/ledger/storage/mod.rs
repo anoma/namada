ec891ae1909aa1b8098894779f3e5e4b1a82e6da
Tomáš Zemanovič <tomas@heliax.dev>
2022-12-16 19:29:53 +0100

    shared: Add WlStorage, combining WriteLog with Storage

--- shared/src/ledger/storage/mod.rs
+++ shared/src/ledger/storage/mod.rs
@@ -1,7 +1,9 @@
 //! Ledger's state storage with key-value backed store and a merkle tree
 
+mod wl_storage;
 pub mod write_log;
 
 #[cfg(any(test, feature = "testing"))]
 pub use namada_core::ledger::storage::mockdb;
 pub use namada_core::ledger::storage::{traits, *};
+pub use wl_storage::{PrefixIter, RoWlStorage, RwWlStorage, WlStorage};
