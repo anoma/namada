dd16a346fa7941cab9cf5ef96075a36e6564f897
Raymond E. Pasco <raymond@heliax.dev>
2022-12-21 02:46:42 -0500

    make-package.sh: include license information in tarball
    Generate LICENSE.thirdparty using cargo-about, and include it and the
    Namada LICENSE file in release tarballs.

--- scripts/make-package.sh
+++ scripts/make-package.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Make a release archive from built Namada binaries and dylib(s)
+# depends on cargo-about 0.5.2
 
 set -e
 
@@ -14,5 +15,7 @@ cd target/release && \
 MM_TOKEN_EXCH=$(find . -maxdepth 1 -name 'libmm_token_exch.so' -or -name 'libmm_token_exch.dll' -or -name 'libmm_token_exch.dylib') && \
 ln ${BIN} ${MM_TOKEN_EXCH} ../../${PACKAGE_NAME} && \
 cd ../.. && \
+ln LICENSE ${PACKAGE_NAME} && \
+cargo about generate about.hbs > ${PACKAGE_NAME}/LICENSE.thirdparty && \
 tar -c -z -f ${PACKAGE_NAME}.tar.gz ${PACKAGE_NAME} && \
 rm -rf ${PACKAGE_NAME}
