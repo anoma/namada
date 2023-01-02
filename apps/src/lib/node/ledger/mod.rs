16c4bca5e34046ac617793fa784d4805932dcaee
Marco Granelli <marco.granelli@ymail.com>
2023-01-03 00:49:37 +0100

    Moves ledger sleep

--- apps/src/lib/node/ledger/mod.rs
+++ apps/src/lib/node/ledger/mod.rs
@@ -15,7 +15,6 @@ use byte_unit::Byte;
 use futures::future::TryFutureExt;
 use namada::ledger::governance::storage as gov_storage;
 use namada::types::storage::Key;
-use namada::types::time::Utc;
 use once_cell::unsync::Lazy;
 use sysinfo::{RefreshKind, System, SystemExt};
 use tokio::task;
@@ -23,7 +22,6 @@ use tower::ServiceBuilder;
 
 use self::abortable::AbortableSpawner;
 use self::shims::abcipp_shim::AbciService;
-use crate::cli::args;
 use crate::config::utils::num_of_threads;
 use crate::config::TendermintMode;
 use crate::facade::tendermint_proto::abci::CheckTxType;
@@ -158,7 +156,7 @@ impl Shell {
 }
 
 /// Run the ledger with an async runtime
-pub fn run(config: config::Ledger, args: args::LedgerRun, wasm_dir: PathBuf) {
+pub fn run(config: config::Ledger, wasm_dir: PathBuf) {
     let logical_cores = num_cpus::get();
     tracing::info!("Available logical cores: {}", logical_cores);
 
@@ -175,22 +173,6 @@ pub fn run(config: config::Ledger, args: args::LedgerRun, wasm_dir: PathBuf) {
         logical_cores / 2,
     );
     tracing::info!("Using {} threads for Tokio.", tokio_threads);
-
-    if let Some(time) = args.0 {
-        // Sleep until start time if not yet reached
-        if let Ok(sleep_time) =
-            time.0.signed_duration_since(Utc::now()).to_std()
-        {
-            if !sleep_time.is_zero() {
-                tracing::info!(
-                    "Waiting ledger start time: {:?}, time left: {:?}",
-                    time,
-                    sleep_time
-                );
-                std::thread::sleep(sleep_time)
-            }
-        }
-    }
 
     // Configure number of threads for rayon (used in `par_iter` when running
     // VPs)
