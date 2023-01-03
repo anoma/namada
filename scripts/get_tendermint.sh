56bbf8c356549cc696b3e501b040f17a1ac1eaf1
James Hiew <james@chainfork.com>
2023-01-03 17:35:31 +0000

    Update get_tendermint.sh to download v0.37.0-rc1

--- scripts/get_tendermint.sh
+++ scripts/get_tendermint.sh
@@ -5,10 +5,10 @@ set -Eo pipefail
 # an examplary download-url
 # https://github.com/tendermint/tendermint/releases/download/v0.34.13/tendermint_0.34.13_linux_amd64.tar.gz
 # https://github.com/heliaxdev/tendermint/releases/download/v0.1.1-abcipp/tendermint_0.1.0-abcipp_darwin_amd64.tar.gz
-TM_MAJORMINOR="0.1"
-TM_PATCH="4"
-TM_SUFFIX="-abciplus"
-TM_REPO="https://github.com/heliaxdev/tendermint"
+TM_MAJORMINOR="0.37"
+TM_PATCH="0"
+TM_SUFFIX="-rc1"
+TM_REPO="https://github.com/tendermint/tendermint"
 
 TM_VERSION="${TM_MAJORMINOR}.${TM_PATCH}${TM_SUFFIX}"
 
