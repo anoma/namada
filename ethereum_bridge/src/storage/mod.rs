4f286fd2c7d96c23855d89bdd9eed7b8e23b677f
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-01-09 15:29:44 +0000

    Ethereum proofs

--- ethereum_bridge/src/storage/mod.rs
+++ ethereum_bridge/src/storage/mod.rs
@@ -1,5 +1,6 @@
 //! Functionality for accessing the storage subspace
 pub mod proof;
 pub mod vote_tallies;
-pub use namada_core::ledger::eth_bridge::storage::bridge_pool;
-pub use namada_core::ledger::eth_bridge::storage::{wrapped_erc20s, *};
+pub use namada_core::ledger::eth_bridge::storage::{
+    bridge_pool, wrapped_erc20s, *,
+};
--- ethereum_bridge/src/storage/proof.rs
+++ ethereum_bridge/src/storage/proof.rs
@@ -1,43 +1,22 @@
-//! Proofs over some arbitrarily signed data.
+//! Proofs over some arbitrary data.
 
 use std::collections::BTreeMap;
 
-use crate::core::ledger::storage::{self, Storage};
-use crate::proto::{SerializeWithBorsh, SignedSerialize};
-use crate::types::address::Address;
-use crate::types::key::common;
-use crate::types::storage::BlockHeight;
+use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
+use namada_core::types::address::Address;
+use namada_core::types::key::secp256k1;
+use namada_core::types::storage::BlockHeight;
 
-/// Some [`Proof`] was constructed with signatures derived from
-/// Ethereum secp keys.
-pub enum ForEthereum {}
-
-/// Some [`Proof`] was constructed with signatures derived from
-/// Namada validator keys.
-pub enum ForNamada {}
-
-/// Proofs contain the signatures of validators reflecting
-/// more than 2/3 of the staked NAM over some data to be signed.
-pub struct Proof<F, T> {
+/// Ethereum proofs contain the [`secp256k1`] signatures of validators
+/// over some data to be signed.
+///
+/// At any given time, an [`EthereumProof`] will be considered
+/// "complete" once a number of signatures pertaining to validators
+/// reflecting more than 2/3 of the bonded stake on Namada is available.
+#[derive(Debug, BorshSerialize, BorshDeserialize, BorshSchema)]
+pub struct EthereumProof<T> {
     /// The signatures contained in the proof.
-    pub signatures: HashMap<(Address, BlockHeight), Signature>,
-    /// The data to be signed.
+    pub signatures: BTreeMap<(Address, BlockHeight), secp256k1::Signature>,
+    /// The signed data.
     pub data: T,
-    /// The type of the underlying nodes whose keys were
-    /// used to sign `data`.
-    ///
-    /// A [`Proof`] will either be constructed with
-    /// signatures derived from Ethereum or Namada
-    /// hot keys.
-    _for_whom: PhantomData<*const F>,
-}
-
-impl<T, S: SignedSerialize> Proof<T, ForEthereum, S> {
-    /// Verify a [`Proof`] constructed with Ethereum hot keys.
-    pub fn eth_verify(&self, storage: &Storage<D, H>) -> bool
-    where
-        D: storage::DB + for<'iter> storage::DBIter<'iter>,
-        H: storage::StorageHasher,
-    {
-    }
 }
