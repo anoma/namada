8ac033dc9215db4317cd2c9a71c8330ac356c435
mariari <mariari@protonmail.com>
2023-01-16 18:44:23 +0800

    Add Parameter data structure for holding an initalizing token params

--- core/src/types/token/parameters.rs
+++ core/src/types/token/parameters.rs
@@ -1,8 +1,41 @@
 //! Custom parameters for each token type. These are used for
 //! determining the shielded pool incentives.
 
+use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
+use serde::{Deserialize, Serialize};
+
+use crate::ledger::storage::{self as ledger_storage};
+use crate::ledger::storage_api::StorageWrite;
 use crate::types::address::Address;
 use crate::types::storage::{Key, KeySeg};
+
+/// Token parameters for each kind of asset held on chain
+#[derive(
+    Clone,
+    Debug,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Hash,
+    BorshSerialize,
+    BorshDeserialize,
+    BorshSchema,
+    Deserialize,
+    Serialize,
+)]
+pub struct Parameters {
+    /// Maximum reward rate
+    max_reward_rate: Decimal,
+    /// Shielded Pool nominal derivative gain
+    kd_gain_nom: Decimal,
+    /// Shielded Pool nominal proportional gain for the given token
+    kp_gain_nom: Decimal,
+    /// Locked ratio for the given token
+    locked_ratio_target_key: Decimal,
+}
 
 /// The key for the nominal proportional gain of a shielded pool for a given
 /// asset
@@ -51,4 +84,46 @@ pub fn key_of_token(
     Key::from(token_addr.to_db_key())
         .push(&specific_key.to_owned())
         .expect(expect_message)
+}
+
+impl Parameters {
+    /// Initialize parameters for the token in storage during the genesis block.
+    pub fn init_storage<DB, H>(
+        &self,
+        address: &Address,
+        wl_storage: &mut ledger_storage::WlStorage<DB, H>,
+    ) where
+        DB: ledger_storage::DB + for<'iter> ledger_storage::DBIter<'iter>,
+        H: ledger_storage::StorageHasher,
+    {
+        let Self {
+            max_reward_rate: max_rate,
+            kd_gain_nom,
+            kp_gain_nom,
+            locked_ratio_target_key: locked_target,
+        } = self;
+        wl_storage
+            .write(&max_reward_rate(address), max_rate)
+            .expect("max reward rate for the given asset must be initialized");
+        wl_storage
+            .write(&locked_token_ratio(address), locked_target)
+            .expect("locked ratio must be initialized");
+        wl_storage
+            .write(&kp_sp_gain(address), kp_gain_nom)
+            .expect("The nominal proportional gain must be initialized");
+        wl_storage
+            .write(&kd_sp_gain(address), kd_gain_nom)
+            .expect("The nominal derivative gain must be initialized");
+    }
+}
+
+impl Default for Parameters {
+    fn default() -> Self {
+        Self {
+            max_reward_rate: dec!(0.1),
+            kp_gain_nom: dec!(0.1),
+            kd_gain_nom: dec!(0.1),
+            locked_ratio_target_key: dec!(0.6667),
+        }
+    }
 }
