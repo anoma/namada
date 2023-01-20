1d733c00e7018ebeed0254918293ef38841a3759
Raymond E. Pasco <raymond@heliax.dev>
2023-01-20 11:34:25 -0500

    wasm tests: don't explicitly lowercase inputs
    This will serve as a canary for if lowercasing in init_parameters
    somehow fails.

--- wasm/wasm_source/src/vp_user.rs
+++ wasm/wasm_source/src/vp_user.rs
@@ -639,11 +639,7 @@ mod tests {
             std::fs::read(VP_ALWAYS_TRUE_WASM).expect("cannot load wasm");
 
         let vp_hash = sha256(&vp_code);
-        tx_env.init_parameters(
-            None,
-            Some(vec![vp_hash.to_string().to_lowercase()]),
-            None,
-        );
+        tx_env.init_parameters(None, Some(vec![vp_hash.to_string()]), None);
 
         // Spawn the accounts to be able to modify their storage
         tx_env.spawn_accounts([&vp_owner]);
@@ -732,7 +728,7 @@ mod tests {
             std::fs::read(VP_ALWAYS_TRUE_WASM).expect("cannot load wasm");
 
         // hardcoded hash of VP_ALWAYS_TRUE_WASM
-        tx_env.init_parameters(None, None, Some(vec!["E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855".to_string().to_lowercase()]));
+        tx_env.init_parameters(None, None, Some(vec!["E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855".to_string()]));
 
         // Spawn the accounts to be able to modify their storage
         tx_env.spawn_accounts([&vp_owner]);
