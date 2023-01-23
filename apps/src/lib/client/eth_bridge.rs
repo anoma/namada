da122d7dd3cc070b2051b8e3702036b11e1cd712
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-01-23 15:04:14 +0000

    Commence impl of validator set queries

--- apps/src/lib/client/eth_bridge.rs
+++ apps/src/lib/client/eth_bridge.rs
@@ -1,1 +1,2 @@
 pub mod bridge_pool;
+pub mod validator_set;
---
+++ apps/src/lib/client/eth_bridge/validator_set.rs
@@ -0,0 +1,10 @@
+/// Query an ABI encoding of the validator set to be installed
+/// at the given epoch, and its associated proof.
+pub async fn query_validator_set_update_proof() {
+    todo!()
+}
+
+/// Query an ABI encoding of the validator set at a given epoch.
+pub async fn query_validator_set_args() {
+    todo!()
+}
