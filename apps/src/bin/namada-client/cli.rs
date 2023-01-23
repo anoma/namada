3ef3bdc463794f5984d954b7ae004051f879bafb
Bengt <bengtlofgren8@gmail.com>
2023-01-23 13:16:52 +0000

    deleted instead of comments

--- apps/src/bin/namada-client/cli.rs
+++ apps/src/bin/namada-client/cli.rs
@@ -52,9 +52,7 @@ pub async fn main() -> Result<()> {
                 Sub::QueryEpoch(QueryEpoch(args)) => {
                     rpc::query_epoch(args).await;
                 }
-                // Sub::QueryTransfers(QueryTransfers(args)) => {
-                //     rpc::query_transfers(ctx, args).await;
-                // }
+            
                 Sub::QueryConversions(QueryConversions(args)) => {
                     rpc::query_conversions(ctx, args).await;
                 }
