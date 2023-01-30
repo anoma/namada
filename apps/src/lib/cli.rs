af1a27952b2b38d22748bf6f30943ce66d536826
Marco Granelli <marco.granelli@ymail.com>
2023-01-30 19:04:33 +0100

    Rephrase time cli help

--- apps/src/lib/cli.rs
+++ apps/src/lib/cli.rs
@@ -1705,7 +1705,7 @@ pub mod args {
             app.arg(NAMADA_START_TIME.def().about(
                 "The start time of the ledger. Accepts a relaxed form of \
                  RFC3339. A space or a 'T' are accepted as the separator \
-                 between the date and time parts. Additional spaces are \
+                 between the date and time components. Additional spaces are \
                  allowed between each component.\nAll of these examples are \
                  equivalent:\n2023-01-20:12:12Z\n2023-01-20 12:12:12Z\n2023-  \
                  01-20T12:  12:12Z",
