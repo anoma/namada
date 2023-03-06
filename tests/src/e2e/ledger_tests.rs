cf6f945dddf5addcfc53fb2ead5a3f318b2bb8c5
Gianmarco Fraccaroli <>
2023-03-06 13:02:33 +0100

    fix: e2e test

--- tests/src/e2e/ledger_tests.rs
+++ tests/src/e2e/ledger_tests.rs
@@ -389,7 +389,7 @@ fn ledger_txs_and_queries() -> Result<()> {
             &vp_user,
             "--alias",
             "Test-Account",
-            "--signers",
+            "--source",
             BERTHA,
             "--gas-amount",
             "0",
