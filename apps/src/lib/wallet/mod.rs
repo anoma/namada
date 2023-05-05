a1049f04ad1be8f1281c8ccd23712030c7ad9a52
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-05-05 14:25:12 +0100

    Assert pre-existing keys are secp256k1 keys

--- apps/src/lib/wallet/mod.rs
+++ apps/src/lib/wallet/mod.rs
@@ -41,6 +41,8 @@ pub enum FindKeyError {
     KeyNotFound,
     #[error("{0}")]
     KeyDecryptionError(keys::DecryptionError),
+    #[error("Expected a Secp256k1 key, but found another key kind")]
+    NotSecp256k1Error,
 }
 
 impl Wallet {
@@ -168,6 +170,12 @@ impl Wallet {
         protocol_pk: Option<common::PublicKey>,
         protocol_key_scheme: SchemeType,
     ) -> Result<ValidatorKeys, FindKeyError> {
+        match &eth_bridge_pk {
+            Some(common::PublicKey::Secp256k1(_)) | None => {}
+            _ => {
+                return Err(FindKeyError::NotSecp256k1Error);
+            }
+        }
         let protocol_keypair = self
             .find_secret_key(protocol_pk, |data| data.keys.protocol_keypair)?;
         let eth_bridge_keypair = self
