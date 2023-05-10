ecb135c7d3302a5a3cbb3bfb2e11003ff8611c46
mariari <mariari@protonmail.com>
2023-05-10 18:47:39 +0800

    Add a new destination for ibc rpc endpoint

--- shared/src/ledger/queries/vp/token.rs
+++ shared/src/ledger/queries/vp/token.rs
@@ -9,6 +9,7 @@ use crate::ledger::queries::RequestCtx;
 
 router! {TOKEN,
     ( "denomination" / [addr: Address] / [sub_prefix: opt Key] ) -> Option<token::Denomination> = denomination,
+    ( "denomination" / [addr: Address] / "ibc" / [_ibc_junk: String] ) -> Option<token::Denomination> = denomination_ibc,
 }
 
 /// Get the number of decimal places (in base 10) for a
@@ -23,4 +24,20 @@ where
     H: 'static + StorageHasher + Sync,
 {
     read_denom(ctx.wl_storage, &addr, sub_prefix.as_ref())
+}
+
+// TODO Please fix this
+
+/// Get the number of decimal places (in base 10) for a
+/// token specified by `addr`.
+fn denomination_ibc<D, H>(
+    ctx: RequestCtx<'_, D, H>,
+    addr: Address,
+    _ibc_junk: String,
+) -> storage_api::Result<Option<token::Denomination>>
+where
+    D: 'static + DB + for<'iter> DBIter<'iter> + Sync,
+    H: 'static + StorageHasher + Sync,
+{
+    read_denom(ctx.wl_storage, &addr, None)
 }
