cfab643cd7e043a122736c1e4d17241cc915371c
Tomáš Zemanovič <tomas@heliax.dev>
2023-05-25 17:00:14 +0200

    add cli to sdk impl for tx unjail

--- apps/src/lib/cli.rs
+++ apps/src/lib/cli.rs
@@ -3028,6 +3028,22 @@ pub mod args {
         }
     }
 
+    impl CliToSdk<TxUnjailValidator<SdkTypes>> for TxUnjailValidator<CliTypes> {
+        fn to_sdk(self, ctx: &mut Context) -> TxUnjailValidator<SdkTypes> {
+            TxUnjailValidator {
+                tx: self.tx.to_sdk(ctx),
+                validator: ctx.get(&self.validator),
+                tx_code_path: self
+                    .tx_code_path
+                    .as_path()
+                    .to_str()
+                    .unwrap()
+                    .to_string()
+                    .into_bytes(),
+            }
+        }
+    }
+
     impl Args for TxUnjailValidator<CliTypes> {
         fn parse(matches: &ArgMatches) -> Self {
             let tx = Tx::parse(matches);
