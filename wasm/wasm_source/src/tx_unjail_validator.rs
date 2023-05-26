65c5a33d0a413c4ffac6d0aa74735695dee94526
brentstone <brent@heliax.dev>
2023-05-26 03:22:02 +0200

    evil: fix `tx_unjail_validator` from cubic slashing

--- wasm/wasm_source/src/tx_unjail_validator.rs
+++ wasm/wasm_source/src/tx_unjail_validator.rs
@@ -4,10 +4,9 @@
 use namada_tx_prelude::*;
 
 #[transaction]
-fn apply_tx(ctx: &mut Ctx, tx_data: Vec<u8>) -> TxResult {
-    let signed = SignedTxData::try_from_slice(&tx_data[..])
-        .wrap_err("failed to decode SignedTxData")?;
-    let data = signed.data.ok_or_err_msg("Missing data")?;
+fn apply_tx(ctx: &mut Ctx, tx_data: Tx) -> TxResult {
+    let signed = tx_data;
+    let data = signed.data().ok_or_err_msg("Missing data")?;
     let validator = Address::try_from_slice(&data[..])
         .wrap_err("failed to decode an Address")?;
     ctx.unjail_validator(&validator)
