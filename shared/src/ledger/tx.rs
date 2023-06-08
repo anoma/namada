cb864f992685cce2c568d6cde95319e977cc9575
Murisi Tarusenga <murisi@localhost.localdomain>
2023-06-08 10:37:30 +0200

    Fixed the header of transactions in dry_run.

--- shared/src/ledger/tx.rs
+++ shared/src/ledger/tx.rs
@@ -46,7 +46,6 @@ use crate::types::key::*;
 use crate::types::masp::TransferTarget;
 use crate::types::storage::{Epoch, RESERVED_ADDRESS_PREFIX};
 use crate::types::time::DateTimeUtc;
-use crate::types::transaction::decrypted::DecryptedTx;
 use crate::types::transaction::{pos, InitAccount, TxType, UpdateVp};
 use crate::types::{storage, token};
 use crate::vm;
@@ -353,12 +352,7 @@ pub async fn submit_reveal_pk_aux<
     .await
     .unwrap();
 
-    let mut tx = Tx::new(TxType::Decrypted(DecryptedTx::Decrypted {
-        #[cfg(not(feature = "mainnet"))]
-        // To be able to dry-run testnet faucet withdrawal, pretend 
-        // that we got a valid PoW
-        has_valid_pow: true,
-    }));
+    let mut tx = Tx::new(TxType::Raw);
     tx.header.chain_id = args.chain_id.clone().expect("value should be there");
     tx.header.expiration = args.expiration;
     tx.set_data(Data::new(tx_data));
