9759185361ebdc50319648b58a03e43e1a119b1e
yito88 <yuji@phact-columba.com>
2023-06-19 22:26:14 +0900

    remove InternalAddress::Minted

--- core/src/types/address.rs
+++ core/src/types/address.rs
@@ -53,8 +53,6 @@ pub const FIXED_LEN_STRING_BYTES: usize = 45;
 
 /// Internal IBC address
 pub const IBC: Address = Address::Internal(InternalAddress::Ibc);
-/// Internal multitoken mint address
-pub const MINTED: Address = Address::Internal(InternalAddress::Minted);
 /// Internal ledger parameters address
 pub const PARAMETERS: Address = Address::Internal(InternalAddress::Parameters);
 /// Internal PoS address
@@ -89,8 +87,6 @@ mod internal {
         "ano::Replay Protection                       ";
     pub const MULTITOKEN: &str =
         "ano::Multitoken                              ";
-    pub const MINTED: &str =
-        "ano::Multitoken Mint Address                 ";
 }
 
 /// Fixed-length address strings prefix for established addresses.
@@ -240,7 +236,6 @@ impl Address {
                     InternalAddress::Multitoken => {
                         internal::MULTITOKEN.to_string()
                     }
-                    InternalAddress::Minted => internal::MINTED.to_string(),
                 };
                 debug_assert_eq!(string.len(), FIXED_LEN_STRING_BYTES);
                 string
@@ -316,9 +311,6 @@ impl Address {
                 }
                 internal::MULTITOKEN => {
                     Ok(Address::Internal(InternalAddress::Multitoken))
-                }
-                internal::MINTED => {
-                    Ok(Address::Internal(InternalAddress::Minted))
                 }
                 _ => Err(Error::new(
                     ErrorKind::InvalidData,
@@ -535,8 +527,6 @@ pub enum InternalAddress {
     ReplayProtection,
     /// Multitoken
     Multitoken,
-    /// Minted multitoken address
-    Minted,
 }
 
 impl Display for InternalAddress {
@@ -556,7 +546,6 @@ impl Display for InternalAddress {
                 Self::EthBridgePool => "EthBridgePool".to_string(),
                 Self::ReplayProtection => "ReplayProtection".to_string(),
                 Self::Multitoken => "Multitoken".to_string(),
-                Self::Minted => "Minted".to_string(),
             }
         )
     }
@@ -850,9 +839,8 @@ pub mod testing {
             InternalAddress::EthBridge => {}
             InternalAddress::EthBridgePool => {}
             InternalAddress::ReplayProtection => {}
-            InternalAddress::Multitoken => {}
-            InternalAddress::Minted => {} /* Add new addresses in the
-                                           * `prop_oneof` below. */
+            InternalAddress::Multitoken => {} /* Add new addresses in the
+                                               * `prop_oneof` below. */
         };
         prop_oneof![
             Just(InternalAddress::PoS),
@@ -866,7 +854,6 @@ pub mod testing {
             Just(InternalAddress::EthBridgePool),
             Just(InternalAddress::ReplayProtection),
             Just(InternalAddress::Multitoken),
-            Just(InternalAddress::Minted)
         ]
     }
 
--- tests/src/e2e/helpers.rs
+++ tests/src/e2e/helpers.rs
@@ -25,8 +25,7 @@ use namada_apps::config::{Config, TendermintMode};
 use namada_core::types::token::NATIVE_MAX_DECIMAL_PLACES;
 
 use super::setup::{
-    self, sleep, NamadaBgCmd, NamadaCmd, Test, ENV_VAR_DEBUG,
-    ENV_VAR_USE_PREBUILT_BINARIES,
+    sleep, NamadaCmd, Test, ENV_VAR_DEBUG, ENV_VAR_USE_PREBUILT_BINARIES,
 };
 use crate::e2e::setup::{Bin, Who, APPS_PACKAGE};
 use crate::{run, run_as};
