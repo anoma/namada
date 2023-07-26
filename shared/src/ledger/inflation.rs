1cf11df959c253182236eee16fed6f5244152603
Tomáš Zemanovič <tomas@heliax.dev>
2023-07-26 12:44:10 +0100

    test/inflation: add tests for inflation calc

--- shared/src/ledger/inflation.rs
+++ shared/src/ledger/inflation.rs
@@ -103,3 +103,152 @@ impl RewardsController {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::str::FromStr;
+
+    use namada_core::types::token::NATIVE_MAX_DECIMAL_PLACES;
+
+    use super::*;
+
+    #[test]
+    fn test_inflation_calc_up() {
+        let mut controller = RewardsController {
+            locked_tokens: token::Amount::from_uint(
+                2_000,
+                NATIVE_MAX_DECIMAL_PLACES,
+            )
+            .unwrap(),
+            total_tokens: token::Amount::from_uint(
+                4_000,
+                NATIVE_MAX_DECIMAL_PLACES,
+            )
+            .unwrap(),
+            locked_ratio_target: Dec::from_str("0.66666666").unwrap(),
+            locked_ratio_last: Dec::from_str("0.5").unwrap(),
+            max_reward_rate: Dec::from_str("0.1").unwrap(),
+            last_inflation_amount: token::Amount::zero(),
+            p_gain_nom: Dec::from_str("0.1").unwrap(),
+            d_gain_nom: Dec::from_str("0.1").unwrap(),
+            epochs_per_year: 365,
+        };
+        dbg!(&controller);
+
+        let ValsToUpdate {
+            locked_ratio: locked_ratio_0,
+            inflation: inflation_0,
+        } = controller.clone().run();
+        println!(
+            "Round 0: Locked ratio: {locked_ratio_0}, inflation: {}",
+            inflation_0.to_string_native()
+        );
+        assert_eq!(locked_ratio_0, Dec::from_str("0.5").unwrap());
+        assert_eq!(inflation_0, token::Amount::from_uint(18_264, 0).unwrap());
+
+        controller.locked_ratio_last = locked_ratio_0;
+        controller.last_inflation_amount = inflation_0;
+        controller.total_tokens += inflation_0;
+        controller.locked_tokens += inflation_0;
+
+        let ValsToUpdate {
+            locked_ratio: locked_ratio_1,
+            inflation: inflation_1,
+        } = controller.clone().run();
+        println!(
+            "Round 1: Locked ratio: {locked_ratio_1}, inflation: {}",
+            inflation_1.to_string_native()
+        );
+        assert!(locked_ratio_1 > locked_ratio_0);
+        assert!(locked_ratio_1 > Dec::from_str("0.5").unwrap());
+        assert!(locked_ratio_1 < Dec::from_str("0.51").unwrap());
+        assert_eq!(inflation_1, token::Amount::from_uint(36_528, 0).unwrap());
+
+        controller.locked_ratio_last = locked_ratio_1;
+        controller.last_inflation_amount = inflation_1;
+        controller.total_tokens += inflation_1;
+        controller.locked_tokens += inflation_1;
+
+        let ValsToUpdate {
+            locked_ratio: locked_ratio_2,
+            inflation: inflation_2,
+        } = controller.run();
+        println!(
+            "Round 2: Locked ratio: {locked_ratio_2}, inflation: {}",
+            inflation_2.to_string_native()
+        );
+        assert!(locked_ratio_2 > locked_ratio_1);
+        assert!(locked_ratio_2 > Dec::from_str("0.5").unwrap());
+        assert!(locked_ratio_2 < Dec::from_str("0.51").unwrap());
+        assert_eq!(inflation_2, token::Amount::from_uint(54_792, 0).unwrap());
+    }
+
+    #[test]
+    fn test_inflation_calc_down() {
+        let mut controller = RewardsController {
+            locked_tokens: token::Amount::from_uint(
+                900,
+                NATIVE_MAX_DECIMAL_PLACES,
+            )
+            .unwrap(),
+            total_tokens: token::Amount::from_uint(
+                1_000,
+                NATIVE_MAX_DECIMAL_PLACES,
+            )
+            .unwrap(),
+            locked_ratio_target: Dec::from_str("0.66666666").unwrap(),
+            locked_ratio_last: Dec::from_str("0.9").unwrap(),
+            max_reward_rate: Dec::from_str("0.1").unwrap(),
+            last_inflation_amount: token::Amount::from_uint(10_000, 0).unwrap(),
+            p_gain_nom: Dec::from_str("0.1").unwrap(),
+            d_gain_nom: Dec::from_str("0.1").unwrap(),
+            epochs_per_year: 365,
+        };
+        dbg!(&controller);
+
+        let ValsToUpdate {
+            locked_ratio: locked_ratio_0,
+            inflation: inflation_0,
+        } = controller.clone().run();
+        println!(
+            "Round 0: Locked ratio: {locked_ratio_0}, inflation: {}",
+            inflation_0.to_string_native()
+        );
+        assert_eq!(locked_ratio_0, Dec::from_str("0.9").unwrap());
+        assert_eq!(inflation_0, token::Amount::from_uint(3_607, 0).unwrap());
+
+        controller.locked_ratio_last = locked_ratio_0;
+        controller.last_inflation_amount = inflation_0;
+        controller.total_tokens += inflation_0;
+        controller.locked_tokens += inflation_0;
+
+        let ValsToUpdate {
+            locked_ratio: locked_ratio_1,
+            inflation: inflation_1,
+        } = controller.clone().run();
+        println!(
+            "Round 1: Locked ratio: {locked_ratio_1}, inflation: {}",
+            inflation_1.to_string_native()
+        );
+        assert!(locked_ratio_1 > locked_ratio_0);
+        assert!(locked_ratio_1 > Dec::from_str("0.9").unwrap());
+        assert!(locked_ratio_1 < Dec::from_str("0.91").unwrap());
+        assert_eq!(inflation_1, token::Amount::zero());
+
+        controller.locked_ratio_last = locked_ratio_1;
+        controller.last_inflation_amount = inflation_1;
+        controller.total_tokens += inflation_1;
+        controller.locked_tokens += inflation_1;
+
+        let ValsToUpdate {
+            locked_ratio: locked_ratio_2,
+            inflation: inflation_2,
+        } = controller.run();
+        println!(
+            "Round 2: Locked ratio: {locked_ratio_2}, inflation: {}",
+            inflation_2.to_string_native()
+        );
+        assert_eq!(locked_ratio_2, locked_ratio_1);
+        assert_eq!(inflation_2, token::Amount::zero());
+    }
+}
