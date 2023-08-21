2835ca544ed837e46c4f566c7837d693eaebdba3
Gianmarco Fraccaroli <gianmarco@heliax.dev>
2023-08-21 09:57:12 +0200

    multisig: fix hashes ordering

--- core/src/proto/types.rs
+++ core/src/proto/types.rs
@@ -1329,13 +1329,15 @@ impl Tx {
 
     /// Get the inner section hashes
     pub fn inner_section_targets(&self) -> Vec<crate::types::hash::Hash> {
-        self.sections
+        let mut sections_hashes = self.sections
             .iter()
             .filter_map(|section| match section {
                 Section::Data(_) | Section::Code(_) => Some(section.get_hash()),
                 _ => None,
             })
-            .collect()
+            .collect::<Vec<crate::types::hash::Hash>>();
+        sections_hashes.sort();
+        sections_hashes
     }
 
     /// Verify that the section with the given hash has been signed by the given
@@ -1457,7 +1459,7 @@ impl Tx {
         secret_keys: &[common::SecretKey],
         public_keys_index_map: &AccountPublicKeysMap,
     ) -> BTreeSet<SignatureIndex> {
-        let targets = [*self.data_sechash(), *self.code_sechash()].to_vec();
+        let targets = self.inner_section_targets();
         MultiSignature::new(targets, secret_keys, public_keys_index_map)
             .signatures
     }
@@ -1695,14 +1697,7 @@ impl Tx {
         account_public_keys_map: AccountPublicKeysMap,
     ) -> &mut Self {
         self.protocol_filter();
-        let hashes = self
-            .sections
-            .iter()
-            .filter_map(|section| match section {
-                Section::Data(_) | Section::Code(_) => Some(section.get_hash()),
-                _ => None,
-            })
-            .collect();
+        let hashes = self.inner_section_targets();
         self.add_section(Section::SectionSignature(MultiSignature::new(
             hashes,
             &keypairs,
