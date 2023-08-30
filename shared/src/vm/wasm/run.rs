60807304d51e83a477b74b7b4c037c67f03217b8
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-08-30 13:19:48 +0100

    Document reason for disabling wasm stack limiter tests
    Co-authored-by: Tomáš Zemanovič <tomas@heliax.dev>

--- shared/src/vm/wasm/run.rs
+++ shared/src/vm/wasm/run.rs
@@ -488,6 +488,9 @@ mod tests {
     /// Test that when a transaction wasm goes over the stack-height limit, the
     /// execution is aborted.
     #[test]
+    // NB: Disabled on aarch64 macOS since a fix for
+    // https://github.com/wasmerio/wasmer/issues/4072
+    // reduced the available stack space on mac
     #[cfg_attr(all(target_arch = "aarch64", target_os = "macos"), ignore)]
     fn test_tx_stack_limiter() {
         // Because each call into `$loop` inside the wasm consumes 5 stack
@@ -510,6 +513,9 @@ mod tests {
     /// Test that when a VP wasm goes over the stack-height limit, the execution
     /// is aborted.
     #[test]
+    // NB: Disabled on aarch64 macOS since a fix for
+    // https://github.com/wasmerio/wasmer/issues/4072
+    // reduced the available stack space on mac
     #[cfg_attr(all(target_arch = "aarch64", target_os = "macos"), ignore)]
     fn test_vp_stack_limiter() {
         // Because each call into `$loop` inside the wasm consumes 5 stack
