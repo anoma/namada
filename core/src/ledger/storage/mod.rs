da2358b0a95042d843d975ca77b93e1346aed8e6
Tomáš Zemanovič <tomas@heliax.dev>
2023-10-17 07:34:50 +0200

    core/storage: set the initial height from genesis

--- core/src/ledger/storage/mod.rs
+++ core/src/ledger/storage/mod.rs
@@ -963,6 +963,11 @@ where
         } = parameters.epoch_duration;
         self.next_epoch_min_start_height = initial_height + min_num_of_blocks;
         self.next_epoch_min_start_time = genesis_time + min_duration;
+        // The default start height in `Epochs` is set to 1, we override it
+        // here in case it differs at genesis
+        self.block.pred_epochs = Epochs {
+            first_block_heights: vec![initial_height],
+        };
         self.update_epoch_in_merkle_tree()
     }
 
