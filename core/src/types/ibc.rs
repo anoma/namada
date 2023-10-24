87ea94a0e341d538ea71211baeb610644407f26f
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-10-24 20:19:47 +0100

    IBC token hash type

--- core/src/types/ibc.rs
+++ core/src/types/ibc.rs
@@ -2,13 +2,53 @@
 
 use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::str::FromStr;
 
 use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
+use data_encoding::{DecodePartial, HEXLOWER, HEXLOWER_PERMISSIVE};
+use serde::{Deserialize, Serialize};
+
+use super::address::HASH_LEN;
 
 /// The event type defined in ibc-rs for receiving a token
 pub const EVENT_TYPE_PACKET: &str = "fungible_token_packet";
 /// The event type defined in ibc-rs for IBC denom
 pub const EVENT_TYPE_DENOM_TRACE: &str = "denomination_trace";
+
+/// IBC token hash derived from a denomination.
+#[derive(
+    Debug,
+    Clone,
+    Serialize,
+    Deserialize,
+    BorshSerialize,
+    BorshDeserialize,
+    BorshSchema,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Hash,
+)]
+#[repr(transparent)]
+pub struct IbcTokenHash(pub [u8; HASH_LEN]);
+
+impl std::fmt::Display for IbcTokenHash {
+    #[inline(always)]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", HEXLOWER.encode(&self.0))
+    }
+}
+
+impl FromStr for IbcTokenHash {
+    type Err = DecodePartial;
+
+    fn from_str(h: &str) -> std::result::Result<Self, Self::Err> {
+        let mut output = [0u8; HASH_LEN];
+        HEXLOWER_PERMISSIVE.decode_mut(h.as_ref(), &mut output)?;
+        Ok(IbcTokenHash(output))
+    }
+}
 
 /// Wrapped IbcEvent
 #[derive(
