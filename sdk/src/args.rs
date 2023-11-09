21e3382673067cdb91813c1b9622a77489384f49
bengtlofgren <bengtlofgren8@gmail.com>
2023-11-09 16:41:37 +0100

    implemented tx_builder

--- sdk/src/args.rs
+++ sdk/src/args.rs
@@ -1010,6 +1010,18 @@ impl Redelegate {
     }
 }
 
+impl<C: NamadaTypes> TxBuilder<C> for Redelegate<C> {
+    fn tx<F>(self, func: F) -> Self
+    where
+        F: FnOnce(Tx<C>) -> Tx<C>,
+    {
+        Redelegate {
+            tx: func(self.tx),
+            ..self
+        }
+    }
+}
+
 /// Reveal public key
 #[derive(Clone, Debug)]
 pub struct RevealPk<C: NamadaTypes = SdkTypes> {
