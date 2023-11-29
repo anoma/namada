e1547c0bc8ac7ecf7fefc5c6cf10972fe660d1e4
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-11-29 10:12:11 +0000

    Fix dev genesis

--- apps/src/lib/config/genesis.rs
+++ apps/src/lib/config/genesis.rs
@@ -458,7 +458,7 @@ pub fn make_dev_genesis(
 
             let validator_account_tx = transactions::ValidatorAccountTx {
                 address: StringEncoded::new(address.clone()),
-                vp: "vp_validator".to_string(),
+                vp: "vp_user".to_string(),
                 commission_rate: Dec::new(5, 2).expect("This can't fail"),
                 max_commission_rate_change: Dec::new(1, 2)
                     .expect("This can't fail"),
