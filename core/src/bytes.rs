a2f138dac57307b99b8c6180393d5ed80a8477c6
DoTheBestToGetTheBest <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2023-12-23 11:12:53 -0800

    enhance bytes hex

--- core/src/bytes.rs
+++ core/src/bytes.rs
@@ -1,24 +1,52 @@
-//! A helper module for dealing with bytes
+use std::fmt::{self, Display, Formatter, UpperHex, LowerHex, Debug};
 
-use std::fmt::Display;
-
-/// A helper to show bytes in hex
 pub struct ByteBuf<'a>(pub &'a [u8]);
 
-impl<'a> std::fmt::LowerHex for ByteBuf<'a> {
-    fn fmt(
-        &self,
-        f: &mut std::fmt::Formatter,
-    ) -> std::result::Result<(), std::fmt::Error> {
+impl<'a> Display for ByteBuf<'a> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{:x}", self)
+    }
+}
+
+impl<'a> LowerHex for ByteBuf<'a> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         for byte in self.0 {
-            f.write_fmt(format_args!("{:02x}", byte))?;
+            write!(f, "{:02x}", byte)?;
         }
         Ok(())
     }
 }
 
-impl<'a> Display for ByteBuf<'a> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:x}", self)
+impl<'a> UpperHex for ByteBuf<'a> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        for byte in self.0 {
+            write!(f, "{:02X}", byte)?;
+        }
+        Ok(())
+    }
+}
+
+impl<'a> Debug for ByteBuf<'a> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        f.debug_tuple("ByteBuf")
+         .field(&self.to_hex_string())
+         .finish()
+    }
+}
+
+impl<'a> ByteBuf<'a> {
+    // Converts the byte buffer into a hexadecimal string
+    pub fn to_hex_string(&self) -> String {
+        self.0.iter().map(|byte| format!("{:02x}", byte)).collect()
+    }
+
+    // Returns the length of the byte buffer
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    // Returns a slice of the byte buffer
+    pub fn slice(&self, start: usize, end: usize) -> &[u8] {
+        &self.0[start..end]
     }
 }
