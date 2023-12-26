3590303a1ccf13ebfecbe60ac7eb26ec2b0769b4
brentstone <brentstone01@gmail.com>
2023-12-26 16:23:00 -0700

    evil: clippy and Cargo.lock

--- apps/src/lib/client/tx.rs
+++ apps/src/lib/client/tx.rs
@@ -769,10 +769,12 @@ pub async fn submit_become_validator(
                     &wallet.find_key_by_pk(&consensus_key, None).expect(
                         "unable to find consensus key pair in the wallet",
                     ),
-                ).unwrap();
+                )
+                .unwrap();
                 // To avoid wallet deadlocks in following operations
                 drop(wallet);
-                tendermint_node::write_validator_state(tendermint_home).unwrap();
+                tendermint_node::write_validator_state(tendermint_home)
+                    .unwrap();
 
                 // Write Namada config stuff or figure out how to do the above
                 // tendermint_node things two epochs in the future!!!
