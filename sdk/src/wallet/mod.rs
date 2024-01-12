c20df21a8a2e1f037ee84fcd75f747416276ad2a
Aleksandr Karbyshev <karbyshev@users.noreply.github.com>
2024-01-12 22:33:29 +0100

    Fix: undo erroneous changes in wallet introduced in 0.28.0 rc1

--- sdk/src/wallet/mod.rs
+++ sdk/src/wallet/mod.rs
@@ -614,7 +614,6 @@ impl<U: WalletIo> Wallet<U> {
     pub fn gen_hd_seed(
         passphrase: Option<Zeroizing<String>>,
         rng: &mut U::Rng,
-        unsafe_dont_encrypt: bool,
     ) -> Result<(Mnemonic, Seed), GenRestoreKeyError> {
         const MNEMONIC_TYPE: MnemonicType = MnemonicType::Words24;
         let mnemonic = U::generate_mnemonic_code(MNEMONIC_TYPE, rng)?;
@@ -624,11 +623,8 @@ impl<U: WalletIo> Wallet<U> {
         );
         println!("{}", mnemonic.clone().into_phrase());
 
-        let passphrase = if unsafe_dont_encrypt {
-            Zeroizing::new(String::new())
-        } else {
-            passphrase.unwrap_or_else(|| U::read_mnemonic_passphrase(true))
-        };
+        let passphrase =
+            passphrase.unwrap_or_else(|| U::read_mnemonic_passphrase(true));
         let seed = Seed::new(&mnemonic, &passphrase);
         Ok((mnemonic, seed))
     }
