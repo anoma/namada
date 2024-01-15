c5214b59be93f9790b7ba97a3765a6ef035abd1e
nodersteam <dudava45@gmail.com>
2024-01-15 21:50:34 +0700

    Revert "Address issue here -> https://github.com/anoma/namada/issues/2151"
    This reverts commit 98e76d14d272a598cb4ed786a50f91249e7f6db2.

--- apps/src/lib/wallet/mod.rs
+++ apps/src/lib/wallet/mod.rs
@@ -11,7 +11,9 @@ use namada::types::key::*;
 pub use namada_sdk::wallet::alias::Alias;
 use namada_sdk::wallet::fs::FsWalletStorage;
 use namada_sdk::wallet::store::Store;
-use namada_sdk::wallet::{ConfirmationResponse, FindKeyError, GenRestoreKeyError, LoadStoreError, Wallet, WalletIo};
+use namada_sdk::wallet::{
+    ConfirmationResponse, FindKeyError, GenRestoreKeyError, Wallet, WalletIo,
+};
 pub use namada_sdk::wallet::{ValidatorData, ValidatorKeys};
 use rand_core::OsRng;
 pub use store::wallet_file;
@@ -240,10 +242,12 @@ pub fn save(wallet: &Wallet<CliWalletUtils>) -> std::io::Result<()> {
 }
 
 /// Load a wallet from the store file.
-pub fn load(store_dir: &Path) -> Result<Wallet<CliWalletUtils>, LoadStoreError> {
+pub fn load(store_dir: &Path) -> Option<Wallet<CliWalletUtils>> {
     let mut wallet = CliWalletUtils::new(store_dir.to_path_buf());
-    wallet.load()?;
-    Ok(wallet)
+    if wallet.load().is_err() {
+        return None;
+    }
+    Some(wallet)
 }
 
 /// Load a wallet from the store file or create a new wallet without any
