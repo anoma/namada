f0acd6dafd7f52d718c6261ea7744b8c2c880050
satan <jacob.turner870@gmail.com>
2024-01-23 10:56:00 +0100

    formatting

--- crates/tests/src/e2e/ledger_tests.rs
+++ crates/tests/src/e2e/ledger_tests.rs
@@ -770,11 +770,7 @@ fn wrapper_disposable_signer() -> Result<()> {
     client.exp_string(TX_ACCEPTED)?;
     client.exp_string(TX_APPLIED_SUCCESS)?;
     let _ep1 = epoch_sleep(&test, &validator_one_rpc, 720)?;
-    let tx_args = vec![
-        "shielded-sync",
-        "--node",
-        &validator_one_rpc,
-    ];
+    let tx_args = vec!["shielded-sync", "--node", &validator_one_rpc];
     let mut client = run!(test, Bin::Client, tx_args, Some(120))?;
     client.assert_success();
     let tx_args = vec![
