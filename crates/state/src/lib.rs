cf90067363d42d37c78fac892b6a0ca75116d49c
Marco Granelli <marco@heliax.dev>
2024-02-28 12:07:06 +0100

    Refactors `read` and `has_key` implementations in `impl_storage_read`

--- crates/state/src/lib.rs
+++ crates/state/src/lib.rs
@@ -192,13 +192,12 @@ macro_rules! impl_storage_read {
         {
             type PrefixIter<'iter> = PrefixIter<'iter, D> where Self: 'iter;
 
-            //FIXME: need a temp_read in this trait?
             fn read_bytes(
                 &self,
                 key: &storage::Key,
             ) -> namada_storage::Result<Option<Vec<u8>>> {
                 // try to read from the write log first
-                let (log_val, gas) = self.write_log().read(key);
+                let (log_val, gas) = self.write_log().read_persistent(key);
                 self.charge_gas(gas).into_storage_result()?;
                 match log_val {
                     Some(write_log::StorageModification::Write { ref value }) => {
@@ -208,31 +207,8 @@ macro_rules! impl_storage_read {
                     Some(write_log::StorageModification::InitAccount {
                         ref vp_code_hash,
                     }) => Ok(Some(vp_code_hash.to_vec())),
-                    Some(write_log::StorageModification::Temp { .. }) => {
-                        // Ignore temp writes, try to read from block write log first
-
-                let (log_val, gas) = self.write_log().read_pre(key);
-                self.charge_gas(gas).into_storage_result()?;
-                        match log_val {
-
- Some(write_log::StorageModification::Write { ref value }) => {
-                        Ok(Some(value.clone()))
-                    }
-                    Some(write_log::StorageModification::Delete) => Ok(None),
-                    Some(write_log::StorageModification::InitAccount {
-                        ref vp_code_hash,
-                    }) => Ok(Some(vp_code_hash.to_vec())),
                     Some(write_log::StorageModification::Temp { .. }) | None => {
-
-// when not found in write log, try to read from the storage
-                        let (value, gas) = self.db_read(key).into_storage_result()?;
-                        self.charge_gas(gas).into_storage_result()?;
-                        Ok(value)
-                            },
-                        }
-                    }
-                    None => {
-                        // when not found in write log, try to read from the storage
+                        // when not found in write log or only found a temporary value, try to read from the storage
                         let (value, gas) = self.db_read(key).into_storage_result()?;
                         self.charge_gas(gas).into_storage_result()?;
                         Ok(value)
@@ -247,33 +223,12 @@ macro_rules! impl_storage_read {
                 match log_val {
                     Some(&write_log::StorageModification::Write { .. })
                     | Some(&write_log::StorageModification::InitAccount { .. }) => Ok(true),
-                     Some(&write_log::StorageModification::Temp { .. }) => {
-                        // Ignore temporary writes
-
-                let (log_val, gas) = self.write_log().read_pre(key);
-                self.charge_gas(gas).into_storage_result()?;
-                        match log_val {
-
-                    Some(&write_log::StorageModification::Write { .. })
-                    | Some(&write_log::StorageModification::InitAccount { .. }) => Ok(true),
                     Some(&write_log::StorageModification::Delete) => {
                         // the given key has been deleted
                         Ok(false)
                     }
-                     Some(&write_log::StorageModification::Temp { .. }) | None => {
-                        // when not found in write log, try to check the storage
-                        let (present, gas) = self.db_has_key(key).into_storage_result()?;
-                        self.charge_gas(gas).into_storage_result()?;
-                        Ok(present)
-                                }
-                        }
-                    }
-                    Some(&write_log::StorageModification::Delete) => {
-                        // the given key has been deleted
-                        Ok(false)
-                    }
-                    None => {
-                        // when not found in write log, try to check the storage
+                    Some(&write_log::StorageModification::Temp { .. }) | None => {
+                        // when not found in write log or only found a temporary value, try to check the storage
                         let (present, gas) = self.db_has_key(key).into_storage_result()?;
                         self.charge_gas(gas).into_storage_result()?;
                         Ok(present)
