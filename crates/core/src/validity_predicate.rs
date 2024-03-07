137db33b2bb4c8fb82ecf5d650f4c0ea8e54988a
Tiago Carvalho <sugoiuguu@tfwno.gf>
2024-03-07 16:18:18 +0100

    Add VP wasm error shared with the host

--- crates/core/src/validity_predicate.rs
+++ crates/core/src/validity_predicate.rs
@@ -1,5 +1,42 @@
 //! Types that are used in validity predicates.
 
+use thiserror::Error;
+
+use crate::borsh::{BorshDeserialize, BorshSerialize};
+
+/// Helper trait for converting between result types.
+pub trait VpErrorExtResult<T> {
+    /// Convert to a [`Result`] with [`VpError`] errors.
+    fn into_vp_error(self) -> Result<T, VpError>;
+}
+
+impl<T, E> VpErrorExtResult<T> for Result<T, E>
+where
+    E: core::fmt::Display,
+{
+    #[inline]
+    fn into_vp_error(self) -> Result<T, VpError> {
+        match self {
+            Ok(ok) => Ok(ok),
+            Err(err) => Err(err.to_string()),
+        }
+    }
+}
+
+/// Error result returned by validity predicates.
+#[allow(missing_docs)]
+#[derive(Debug, Error, BorshSerialize, BorshDeserialize)]
+pub enum VpError {
+    #[error("Transaction rejected")]
+    Unspecified,
+    #[error("Gas limit exceeded")]
+    OutOfGas,
+    #[error("Found invalid transaction signature")]
+    InvalidSignature,
+    #[error("{0}")]
+    Erased(String), // type erased error
+}
+
 /// Sentinel used in validity predicates to signal events that require special
 /// replay protection handling back to the protocol.
 #[derive(Debug, Default)]
