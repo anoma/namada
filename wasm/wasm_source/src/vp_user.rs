39a62fc59f2a17e3dd34a5a48be75f32d1574a7d
brentstone <brentstone01@gmail.com>
2024-03-08 17:20:28 +0100

    fix bug for deleting metadata

--- wasm/wasm_source/src/vp_user.rs
+++ wasm/wasm_source/src/vp_user.rs
@@ -190,11 +190,12 @@ fn validate_pos_changes(
     // metadata is manipulated
     let is_valid_metadata_change = || {
         let validator = is_validator_metadata_key(key);
-        let metadata = ctx
-            .post()
-            .read::<String>(key)?
-            .expect("Metadata should exist in post state");
-        let valid_len = (metadata.len() as u64) <= MAX_VALIDATOR_METADATA_LEN; // FIXME: hard-code for now
+        let metadata = ctx.post().read::<String>(key)?;
+        let valid_len = if let Some(metadata) = metadata {
+            (metadata.len() as u64) <= MAX_VALIDATOR_METADATA_LEN
+        } else {
+            true
+        };
         let is_valid = match validator {
             Some(address) => valid_len && address == owner && **valid_sig,
             None => false,
