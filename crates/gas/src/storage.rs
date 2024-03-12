3a4a733d268ae64dd479323463fe1209929347d6
Gianmarco Fraccaroli <fraccaman@Gianmarcos-MBP-004.homenet.telecomitalia.it>
2024-03-12 20:59:03 +0100

    added tx gas field to inmem db, added writes to rocksdb state

---
+++ crates/gas/src/storage.rs
@@ -0,0 +1,16 @@
+//! Gas storage keys
+
+use namada_core::hash::Hash;
+use namada_core::storage::Key;
+
+const ERROR_MSG: &str = "Cannot obtain a valid db key";
+
+/// Get the transaction hash prefix under the `all` subkey
+pub fn pred_prefix() -> Key {
+    Key::parse("pred/gas").expect(ERROR_MSG)
+}
+
+/// Get the full transaction hash prefix under the `last` subkey
+pub fn gas_prefix() -> Key {
+    Key::parse("gas").expect(ERROR_MSG)
+}
