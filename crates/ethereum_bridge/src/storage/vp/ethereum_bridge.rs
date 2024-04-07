010634e170ee34f8b8fccbe84f7ca06763043c79
Discord2 <discord2@Phoenixs-MacBook-Air.local>
2024-04-07 17:29:58 +0530

    refactor: Add functions to update balances and token supplies

--- crates/ethereum_bridge/src/storage/vp/ethereum_bridge.rs
+++ crates/ethereum_bridge/src/storage/vp/ethereum_bridge.rs
@@ -1,6 +1,4 @@
 use namada_storage::{StorageRead, StorageWrite};
-use namada_trans_token::storage_key::balance_key;
-use namada_trans_token::Amount;
 
 use crate::ADDRESS;
 
@@ -12,10 +10,5 @@ pub fn init_storage<S>(storage: &mut S)
 where
     S: StorageRead + StorageWrite,
 {
-    let escrow_key =
-        balance_key(&storage.get_native_token().unwrap(), &ADDRESS);
-    storage.write(&escrow_key, Amount::default()).expect(
-        "Initializing the escrow balance of the Ethereum Bridge VP shouldn't \
-         fail.",
-    );
+    namada_trans_token::init_bridge_storage(storage, &ADDRESS);
 }
--- crates/trans_token/src/storage.rs
+++ crates/trans_token/src/storage.rs
@@ -4,6 +4,7 @@ use namada_core::token::{self, Amount, DenominatedAmount};
 use namada_storage as storage;
 use namada_storage::{StorageRead, StorageWrite};
 
+// use namada_trans_token::Amount;
 use crate::storage_key::*;
 
 /// Initialize parameters for the token in storage during the genesis block.
@@ -225,6 +226,21 @@ pub fn denom_to_amount(
         )
     })?;
     denom_amount.scale(denom).map_err(storage::Error::new)
+}
+
+/// Initialize the storage owned by the Ethereum Bridge VP.
+///
+/// This means that the amount of escrowed Nam is
+/// initialized to 0.
+pub fn init_bridge_storage<S>(storage: &mut S, source: &Address)
+where
+    S: StorageRead + StorageWrite,
+{
+    let escrow_key = balance_key(&storage.get_native_token().unwrap(), source);
+    storage.write(&escrow_key, Amount::default()).expect(
+        "Initializing the escrow balance of the Ethereum Bridge VP shouldn't \
+         fail.",
+    );
 }
 
 #[cfg(test)]
