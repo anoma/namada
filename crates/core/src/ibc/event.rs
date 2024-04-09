e8164c2e3a6ce298bc15988a40985dc0d763a1ff
Tiago Carvalho <sugoiuguu@tfwno.gf>
2024-04-09 09:59:20 +0100

    Add new IBC receiver helper methods

--- crates/core/src/ibc/event.rs
+++ crates/core/src/ibc/event.rs
@@ -9,7 +9,7 @@ use namada_migrations::*;
 use serde::{Deserialize, Serialize};
 
 use super::IbcShieldedTransfer;
-use crate::address::Address;
+use crate::address::{Address, MASP};
 use crate::borsh::*;
 use crate::collections::HashMap;
 use crate::event::extend::{
@@ -400,6 +400,26 @@ impl FromStr for OwnedIbcReceiver {
                      {s:?}"
                 )
             })
+    }
+}
+
+impl OwnedIbcReceiver {
+    /// Return a reference to the owned values.
+    pub fn as_ref(&self) -> BorrowedIbcReceiver<'_> {
+        match self {
+            Self::Transparent(addr) => IbcReceiver::Transparent(addr),
+            Self::Shielded(addr) => IbcReceiver::Shielded(addr),
+        }
+    }
+}
+
+impl BorrowedIbcReceiver<'_> {
+    /// Return a transparent address from this [`IbcReceiver`].
+    pub fn transparent_address(&self) -> &Address {
+        match self {
+            Self::Transparent(addr) => addr,
+            Self::Shielded(_) => &MASP,
+        }
     }
 }
 
