85999c00dbf28ef6a242715149d1359a6321bc51
TerzoMillenio <beghemooth@gmail.com>
2024-04-11 17:16:21 +0300

    Revert "Adds an output for submit_reveal_aux when the public key is already revealed to avoid confusion"
    This reverts commit 48976f9dc6c668dddec4e520a01477708b4f2cf1.

--- crates/apps/src/lib/client/tx.rs
+++ crates/apps/src/lib/client/tx.rs
@@ -227,11 +227,7 @@ pub async fn submit_reveal_aux(
             sign(context, &mut tx, &args, signing_data).await?;
 
             context.submit(tx, &args).await?;
-        } else {
-	    println!(
-		"Public key for address {address} already revealed."
-	    );
-	}
+        }
     }
 
     Ok(())
