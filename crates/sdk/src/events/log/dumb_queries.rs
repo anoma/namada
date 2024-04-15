49de95208507737a8e01279681e9144c3c039d28
Tiago Carvalho <sugoiuguu@tfwno.gf>
2024-04-15 13:08:38 +0100

    Match the prefix of an event type

--- crates/sdk/src/events/log/dumb_queries.rs
+++ crates/sdk/src/events/log/dumb_queries.rs
@@ -1,6 +1,10 @@
 //! Silly simple event matcher.
 
 use namada_core::collections::HashMap;
+use namada_core::ethereum_structs::event_types::{
+    BRIDGE_POOL_EXPIRED, BRIDGE_POOL_RELAYED,
+};
+use namada_core::ethereum_structs::BridgePoolTxHash;
 use namada_core::hash::Hash;
 use namada_core::keccak::KeccakHash;
 use namada_core::storage::BlockHeight;
@@ -8,7 +12,7 @@ use namada_core::storage::BlockHeight;
 use crate::events::extend::{
     ExtendAttributesMap, ExtendEventAttributes, TxHash as TxHashAttr,
 };
-use crate::events::{Event, EventType, EventTypeBuilder};
+use crate::events::{Event, EventToEmit, EventType, EventTypeBuilder};
 use crate::ibc::core::client::types::Height as IbcHeight;
 use crate::ibc::core::host::types::identifiers::{
     ChannelId, ClientId, PortId, Sequence,
@@ -25,8 +29,19 @@ use crate::tx::event::types::APPLIED as APPLIED_TX;
 /// given Tendermint query.
 #[derive(Debug, Clone)]
 pub struct QueryMatcher {
+    event_type_match: MatchType,
     event_type: EventType,
     attributes: HashMap<String, String>,
+}
+
+/// Determine which kind of match will be performed over a series of event
+/// types.
+#[derive(Debug, Clone)]
+pub enum MatchType {
+    /// Exact match.
+    Exact,
+    /// Prefix match.
+    Prefix,
 }
 
 impl ExtendAttributesMap for QueryMatcher {
@@ -46,10 +61,33 @@ impl QueryMatcher {
         &self.event_type
     }
 
+    /// Return the match type performed over the
+    /// [`EventType`].
+    pub fn match_type(&self) -> &MatchType {
+        &self.event_type_match
+    }
+
+    /// Create a new [`QueryMatcher`] matching event types
+    /// with the given `prefix`.
+    pub fn of_event_type<E: EventToEmit>() -> Self {
+        Self::with_prefix(EventType::new(E::DOMAIN))
+    }
+
+    /// Create a new [`QueryMatcher`] matching event types
+    /// with the given `prefix`.
+    pub fn with_prefix(prefix: EventType) -> Self {
+        Self {
+            event_type: prefix,
+            event_type_match: MatchType::Prefix,
+            attributes: Default::default(),
+        }
+    }
+
     /// Create a new [`QueryMatcher`] with the given event type.
     pub fn with_event_type(event_type: EventType) -> Self {
         Self {
             event_type,
+            event_type_match: MatchType::Exact,
             attributes: Default::default(),
         }
     }
@@ -67,7 +105,12 @@ impl QueryMatcher {
     /// Checks if this [`QueryMatcher`] validates the
     /// given [`Event`].
     pub fn matches(&self, event: &Event) -> bool {
-        if *event.kind() != self.event_type {
+        let matches_event_type = match self.match_type() {
+            MatchType::Exact => *event.kind() == self.event_type,
+            MatchType::Prefix => event.kind().starts_with(&*self.event_type),
+        };
+
+        if !matches_event_type {
             return false;
         }
         event.has_subset_of_attrs(&self.attributes)
@@ -75,18 +118,14 @@ impl QueryMatcher {
 
     /// Returns a query matching the given relayed Bridge pool transaction hash.
     pub fn bridge_pool_relayed(tx_hash: &KeccakHash) -> Self {
-        Self::with_event_type(
-            namada_core::ethereum_structs::event_types::BRIDGE_POOL_RELAYED,
-        )
-        .and_attribute(namada_core::ethereum_structs::BridgePoolTxHash(tx_hash))
+        Self::with_event_type(BRIDGE_POOL_RELAYED)
+            .and_attribute(BridgePoolTxHash(tx_hash))
     }
 
     /// Returns a query matching the given expired Bridge pool transaction hash.
     pub fn bridge_pool_expired(tx_hash: &KeccakHash) -> Self {
-        Self::with_event_type(
-            namada_core::ethereum_structs::event_types::BRIDGE_POOL_EXPIRED,
-        )
-        .and_attribute(namada_core::ethereum_structs::BridgePoolTxHash(tx_hash))
+        Self::with_event_type(BRIDGE_POOL_EXPIRED)
+            .and_attribute(BridgePoolTxHash(tx_hash))
     }
 
     /// Returns a query matching the given applied transaction hash.
@@ -130,29 +169,65 @@ impl QueryMatcher {
 
 #[cfg(test)]
 mod tests {
+    use namada_core::ethereum_structs::EthBridgeEvent;
+
     use super::*;
     use crate::events::extend::ComposeEvent;
     use crate::events::EventLevel;
 
+    const HASH: &str =
+        "DEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF";
+
+    /// Test if matching the prefix of an event type works as expected.
+    #[test]
+    fn test_query_matching_prefix() {
+        let matcher = QueryMatcher::of_event_type::<EthBridgeEvent>();
+
+        let tests = {
+            let bp_hash: KeccakHash = HASH.parse().unwrap();
+            let tx_hash: Hash = HASH.parse().unwrap();
+
+            let event_1: Event =
+                Event::new(BRIDGE_POOL_RELAYED, EventLevel::Tx)
+                    .with(BridgePoolTxHash(&bp_hash))
+                    .into();
+            let matches_1 = true;
+
+            let event_2: Event =
+                Event::new(BRIDGE_POOL_EXPIRED, EventLevel::Tx)
+                    .with(BridgePoolTxHash(&bp_hash))
+                    .into();
+            let matches_2 = true;
+
+            let event_3: Event = Event::new(UPDATE_CLIENT, EventLevel::Tx)
+                .with(TxHashAttr(tx_hash))
+                .into();
+            let matches_3 = false;
+
+            [
+                (event_1, matches_1),
+                (event_2, matches_2),
+                (event_3, matches_3),
+            ]
+        };
+
+        for (ref ev, status) in tests {
+            if matcher.matches(ev) != status {
+                panic!("Test failed");
+            }
+        }
+    }
+
     /// Test if query matching is working as expected.
     #[test]
     fn test_tm_query_matching() {
-        const HASH: &str =
-            "DEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF";
-
         let tx_hash: Hash = HASH.parse().unwrap();
 
-        let matcher = QueryMatcher {
-            event_type: APPLIED_TX,
-            attributes: {
-                let mut attrs = namada_core::collections::HashMap::new();
-                attrs.with_attribute(TxHashAttr(tx_hash));
-                attrs
-            },
-        };
+        let matcher = QueryMatcher::with_event_type(APPLIED_TX)
+            .and_attribute(TxHashAttr(tx_hash));
 
         let tests = {
-            let event_1: Event = Event::new(UPDATE_CLIENT, EventLevel::Block)
+            let event_1: Event = Event::new(UPDATE_CLIENT, EventLevel::Tx)
                 .with(TxHashAttr(tx_hash))
                 .into();
             let applied_1 = false;
