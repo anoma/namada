5a96caee4e9dc4809cdd660598d4c838d6316f51
Tiago Carvalho <sugoiuguu@tfwno.gf>
2024-05-08 13:11:49 +0100

    scripts/gen_localnet.py: Format code

--- scripts/gen_localnet.py
+++ scripts/gen_localnet.py
@@ -6,11 +6,13 @@ import subprocess
 import shutil
 import toml
 
+
 def system(cmd):
     if os.system(cmd) != 0:
         exit(1)
 
-def move_genesis_wallet(genesis_wallet_toml : str, wallet_toml : str):
+
+def move_genesis_wallet(genesis_wallet_toml: str, wallet_toml: str):
     genesis_wallet = toml.load(genesis_wallet_toml)
     wallet = toml.load(wallet_toml)
 
@@ -18,7 +20,8 @@ def move_genesis_wallet(genesis_wallet_toml : str, wallet_toml : str):
         value_dict = genesis_wallet[key]
         if key in wallet.keys():
             wallet[key].update(value_dict)
-    toml.dump(wallet, open(wallet_toml, 'w'))
+    toml.dump(wallet, open(wallet_toml, "w"))
+
 
 def edit_parameters(params_toml, **kwargs):
     # Make sure the kwargs are valid
@@ -34,27 +37,49 @@ def edit_parameters(params_toml, **kwargs):
                     del kwargs[k][key]
                 else:
                     params[k][key] = kwargs[k][key]
-    
-    toml.dump(params, open(params_toml, 'w'))
 
+    toml.dump(params, open(params_toml, "w"))
 
 
 # Get the absolute path to the directory of the script
 script_dir = sys.path[0]
 # Get absolute path of parent directory
-namada_dir = script_dir[:script_dir.rfind('/')]
+namada_dir = script_dir[: script_dir.rfind("/")]
 
 # Create the parser
-parser = argparse.ArgumentParser(description='Builds a localnet for testing purposes')
+parser = argparse.ArgumentParser(description="Builds a localnet for testing purposes")
 
 # Add the arguments
-parser.add_argument('--base-dir', type=str, help='The path to the base directory of the chain.')
-parser.add_argument('--localnet-dir', type=str, help='The localnet directory containing the genesis templates.')
-parser.add_argument('-m', '--mode', type=str, help='The mode to run the localnet in. Can be release or debug, defaults to debug.')
-parser.add_argument('--epoch-length', type=int, help='The epoch length in seconds, defaults to parameters.toml value.')
-parser.add_argument('--max-validator-slots', type=int, help='The maximum number of validators, defaults to parameters.toml value.')
+parser.add_argument(
+    "--base-dir", type=str, help="The path to the base directory of the chain."
+)
+parser.add_argument(
+    "--localnet-dir",
+    type=str,
+    help="The localnet directory containing the genesis templates.",
+)
+parser.add_argument(
+    "-m",
+    "--mode",
+    type=str,
+    help="The mode to run the localnet in. Can be release or debug, defaults to debug.",
+)
+parser.add_argument(
+    "--epoch-length",
+    type=int,
+    help="The epoch length in seconds, defaults to parameters.toml value.",
+)
+parser.add_argument(
+    "--max-validator-slots",
+    type=int,
+    help="The maximum number of validators, defaults to parameters.toml value.",
+)
 # Change any parameters in the parameters.toml file
-parser.add_argument('--params', type=str, help='A string representation of a dictionary of parameters to update in the parameters.toml. Must be of the same format.')
+parser.add_argument(
+    "--params",
+    type=str,
+    help="A string representation of a dictionary of parameters to update in the parameters.toml. Must be of the same format.",
+)
 
 
 # Parse the arguments
@@ -62,50 +87,50 @@ args = parser.parse_args()
 
 # Access the arguments
 if args.localnet_dir:
-    if args.localnet_dir[-1] == '/':
+    if args.localnet_dir[-1] == "/":
         args.localnet_dir = args.localnet_dir[:-1]
     print(os.path.basename(args.localnet_dir))
-    localnet_dir = namada_dir + '/' + os.path.basename(args.localnet_dir)
+    localnet_dir = namada_dir + "/" + os.path.basename(args.localnet_dir)
     shutil.copytree(args.localnet_dir, localnet_dir)
 
     if os.path.isdir(localnet_dir) and os.listdir(localnet_dir):
-        print('Using localnet directory: ' + localnet_dir)
+        print("Using localnet directory: " + localnet_dir)
     else:
-        print('Cannot find localnet directory that is not empty')
+        print("Cannot find localnet directory that is not empty")
         sys.exit(1)
 else:
-    localnet_dir = namada_dir + '/genesis/localnet'
+    localnet_dir = namada_dir + "/genesis/localnet"
 
 if args.mode:
     mode = args.mode
 else:
-    mode = 'debug'
+    mode = "debug"
 
-if mode.lower() != 'release':
-    mode = 'debug'
+if mode.lower() != "release":
+    mode = "debug"
 
 params = {}
 if args.params:
     params = eval(args.params)
 if args.max_validator_slots:
-    params['pos_params'] = {'max_validator_slots': args.max_validator_slots}
+    params["pos_params"] = {"max_validator_slots": args.max_validator_slots}
 if args.epoch_length:
     epochs_per_year = round(365 * 24 * 60 * 60 / args.epoch_length)
-    params['parameters'] = {'epochs_per_year': epochs_per_year }
-if len(params.keys())>0:
-    edit_parameters(localnet_dir + '/parameters.toml', **params)
-        
-namada_bin_dir = namada_dir + '/target/' + mode + '/'
+    params["parameters"] = {"epochs_per_year": epochs_per_year}
+if len(params.keys()) > 0:
+    edit_parameters(localnet_dir + "/parameters.toml", **params)
+
+namada_bin_dir = namada_dir + "/target/" + mode + "/"
 
 # Check that namada_bin_dir exists and is not empty
 if not os.path.isdir(namada_bin_dir) or not os.listdir(namada_bin_dir):
-    print('Cannot find namada binary directory that is not empty')
+    print("Cannot find namada binary directory that is not empty")
     sys.exit(1)
 
-namada_bin = namada_bin_dir + 'namada'
-namadac_bin = namada_bin_dir + 'namadac'
-namadan_bin = namada_bin_dir + 'namadan'
-namadaw_bin = namada_bin_dir + 'namadaw'
+namada_bin = namada_bin_dir + "namada"
+namadac_bin = namada_bin_dir + "namadac"
+namadan_bin = namada_bin_dir + "namadan"
+namadaw_bin = namada_bin_dir + "namadaw"
 
 bins = [namada_bin, namadac_bin, namadan_bin, namadaw_bin]
 # Check that each binary exists and is executable
@@ -115,20 +140,23 @@ for bin in bins:
         sys.exit(1)
 
 
-
 print(f"Using {bins[0].split('/')[-1]} version: {os.popen(bin + ' --version').read()}")
 
 # Run namadac utils init_network with the correct arguments
-print('Running namadac utils init_network')
-CHAIN_PREFIX='local'
+print("Running namadac utils init_network")
+CHAIN_PREFIX = "local"
 GENESIS_TIME = datetime.datetime.now(datetime.timezone.utc).isoformat()
-TEMPLATES_PATH=localnet_dir
-WASM_CHECKSUMS_PATH=namada_dir + '/wasm/checksums.json'
-WASM_PATH=namada_dir + '/wasm/'
+TEMPLATES_PATH = localnet_dir
+WASM_CHECKSUMS_PATH = namada_dir + "/wasm/checksums.json"
+WASM_PATH = namada_dir + "/wasm/"
 BASE_DIR = args.base_dir
 
 if not BASE_DIR:
-    BASE_DIR = subprocess.check_output([namadac_bin, "utils", "default-base-dir"]).decode().strip()
+    BASE_DIR = (
+        subprocess.check_output([namadac_bin, "utils", "default-base-dir"])
+        .decode()
+        .strip()
+    )
 
 # Delete the base dir
 if os.path.isdir(BASE_DIR):
@@ -145,39 +173,42 @@ if not os.path.isdir(WASM_PATH) or not os.listdir(WASM_PATH):
     print(f"Cannot find wasm directory that is not empty at {WASM_PATH}")
     sys.exit(1)
 
-system(f"{namadac_bin} --base-dir='{BASE_DIR}' utils init-network --chain-prefix {CHAIN_PREFIX} --genesis-time {GENESIS_TIME} --templates-path {TEMPLATES_PATH} --wasm-checksums-path {WASM_CHECKSUMS_PATH}")
+system(
+    f"{namadac_bin} --base-dir='{BASE_DIR}' utils init-network --chain-prefix {CHAIN_PREFIX} --genesis-time {GENESIS_TIME} --templates-path {TEMPLATES_PATH} --wasm-checksums-path {WASM_CHECKSUMS_PATH}"
+)
 
 base_dir_files = os.listdir(BASE_DIR)
-CHAIN_ID=""
+CHAIN_ID = ""
 for file in base_dir_files:
     if file.startswith(CHAIN_PREFIX):
         CHAIN_ID = file
         break
 
-# create a new directory within the base_dir 
-temp_dir = BASE_DIR + '/tmp/'
+# create a new directory within the base_dir
+temp_dir = BASE_DIR + "/tmp/"
 os.mkdir(temp_dir)
-shutil.move(BASE_DIR + '/' + CHAIN_ID, BASE_DIR + '/tmp/' + CHAIN_ID)
-shutil.move(namada_dir + '/' + CHAIN_ID + '.tar.gz', temp_dir + CHAIN_ID + '.tar.gz')
+shutil.move(BASE_DIR + "/" + CHAIN_ID, BASE_DIR + "/tmp/" + CHAIN_ID)
+shutil.move(namada_dir + "/" + CHAIN_ID + ".tar.gz", temp_dir + CHAIN_ID + ".tar.gz")
 
-GENESIS_VALIDATOR='validator-0'
-PRE_GENESIS_PATH=localnet_dir + '/src/pre-genesis/' + GENESIS_VALIDATOR
+GENESIS_VALIDATOR = "validator-0"
+PRE_GENESIS_PATH = localnet_dir + "/src/pre-genesis/" + GENESIS_VALIDATOR
 if not os.path.isdir(PRE_GENESIS_PATH) or not os.listdir(PRE_GENESIS_PATH):
     print(f"Cannot find pre-genesis directory that is not empty at {PRE_GENESIS_PATH}")
     sys.exit(1)
 
-system(f"NAMADA_NETWORK_CONFIGS_DIR='{temp_dir}' {namadac_bin} --base-dir='{BASE_DIR}' utils join-network --chain-id {CHAIN_ID} --genesis-validator {GENESIS_VALIDATOR} --pre-genesis-path {PRE_GENESIS_PATH} --dont-prefetch-wasm")
+system(
+    f"NAMADA_NETWORK_CONFIGS_DIR='{temp_dir}' {namadac_bin} --base-dir='{BASE_DIR}' utils join-network --chain-id {CHAIN_ID} --genesis-validator {GENESIS_VALIDATOR} --pre-genesis-path {PRE_GENESIS_PATH} --dont-prefetch-wasm"
+)
 
-shutil.rmtree(BASE_DIR + '/' + CHAIN_ID + '/wasm/')
-shutil.move(temp_dir + CHAIN_ID + '/wasm/', BASE_DIR + '/' + CHAIN_ID + '/wasm/')
+shutil.rmtree(BASE_DIR + "/" + CHAIN_ID + "/wasm/")
+shutil.move(temp_dir + CHAIN_ID + "/wasm/", BASE_DIR + "/" + CHAIN_ID + "/wasm/")
 
 # Move the genesis wallet to the base dir
-genesis_wallet_toml = localnet_dir + '/src/pre-genesis' + '/wallet.toml'
-wallet = BASE_DIR + '/' + CHAIN_ID + '/wallet.toml'
+genesis_wallet_toml = localnet_dir + "/src/pre-genesis" + "/wallet.toml"
+wallet = BASE_DIR + "/" + CHAIN_ID + "/wallet.toml"
 move_genesis_wallet(genesis_wallet_toml, wallet)
 # Delete the temp dir
 shutil.rmtree(temp_dir)
 
 print("Run the ledger using the following command:")
 print(f"{namada_bin} --base-dir='{BASE_DIR}' --chain-id '{CHAIN_ID}' ledger run")
-
