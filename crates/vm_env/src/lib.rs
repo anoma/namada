293b5cab2718c62c8223a161f9a0ec3eede1e376
Tomáš Zemanovič <tomas@heliax.dev>
2024-05-10 18:21:56 +0200

    vm_env: add lints

--- crates/vm_env/src/lib.rs
+++ crates/vm_env/src/lib.rs
@@ -4,6 +4,15 @@
 #![doc(html_logo_url = "https://dev.namada.net/master/rustdoc-logo.png")]
 #![deny(rustdoc::broken_intra_doc_links)]
 #![deny(rustdoc::private_intra_doc_links)]
+#![warn(
+    missing_docs,
+    rust_2018_idioms,
+    clippy::cast_sign_loss,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_lossless,
+    clippy::arithmetic_side_effects
+)]
 
 use borsh::BorshDeserialize;
 use namada_core::internal::{HostEnvResult, KeyVal};
