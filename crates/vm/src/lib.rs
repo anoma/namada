9bc4ab84e57d7fc15f70f8e7847612a4d8c95b93
Tomáš Zemanovič <tomas@heliax.dev>
2024-06-17 19:00:23 +0100

    vm + vp: add common crate attrs

--- crates/vm/src/lib.rs
+++ crates/vm/src/lib.rs
@@ -1,5 +1,22 @@
 //! Virtual machine modules for running transactions and validity predicates.
 
+#![doc(html_favicon_url = "https://dev.namada.net/master/favicon.png")]
+#![doc(html_logo_url = "https://dev.namada.net/master/rustdoc-logo.png")]
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(rustdoc::private_intra_doc_links)]
+#![warn(
+    missing_docs,
+    rust_2018_idioms,
+    clippy::cast_sign_loss,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_lossless,
+    clippy::arithmetic_side_effects,
+    clippy::dbg_macro,
+    clippy::print_stdout,
+    clippy::print_stderr
+)]
+
 use std::marker::PhantomData;
 use std::ptr::NonNull;
 
--- crates/vp/src/lib.rs
+++ crates/vp/src/lib.rs
@@ -1,6 +1,23 @@
 //! This crate contains the trait for native validity predicates with its
 //! various context types and host functions implementation.
 
+#![doc(html_favicon_url = "https://dev.namada.net/master/favicon.png")]
+#![doc(html_logo_url = "https://dev.namada.net/master/rustdoc-logo.png")]
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(rustdoc::private_intra_doc_links)]
+#![warn(
+    missing_docs,
+    rust_2018_idioms,
+    clippy::cast_sign_loss,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_lossless,
+    clippy::arithmetic_side_effects,
+    clippy::dbg_macro,
+    clippy::print_stdout,
+    clippy::print_stderr
+)]
+
 pub mod native_vp;
 pub mod vp_host_fns;
 
