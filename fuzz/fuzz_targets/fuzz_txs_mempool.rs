fb6d7fda78e7bcdff15ec8f3d4982979a49d1304
Tomáš Zemanovič <tomas@heliax.dev>
2024-07-01 16:09:58 +0100

    fuzz_txs_mempool: re-use TestShell between runs

--- fuzz/fuzz_targets/fuzz_txs_mempool.rs
+++ fuzz/fuzz_targets/fuzz_txs_mempool.rs
@@ -1,11 +1,19 @@
 #![no_main]
 
+use lazy_static::lazy_static;
 use libfuzzer_sys::fuzz_target;
 use namada_node::shell;
+use namada_node::shell::test_utils::TestShell;
 use namada_node::shell::MempoolTxType;
 use namada_tx::Tx;
 
+lazy_static! {
+    static ref SHELL: TestShell = {
+        let (shell, _recv, _, _) = shell::test_utils::setup();
+        shell
+    };
+}
+
 fuzz_target!(|tx: Tx| {
-    let (shell, _recv, _, _) = shell::test_utils::setup();
-    shell.mempool_validate(&tx.to_bytes(), MempoolTxType::NewTransaction);
+    SHELL.mempool_validate(&tx.to_bytes(), MempoolTxType::NewTransaction);
 });
