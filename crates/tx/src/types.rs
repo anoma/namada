c94a2f524d71b9f37994b2411ed1ac431ef0af9a
Tomáš Zemanovič <tomas@heliax.dev>
2024-07-02 09:18:12 +0100

    tx: fix manual impl Arbitrary for Section

--- crates/tx/src/types.rs
+++ crates/tx/src/types.rs
@@ -820,7 +820,7 @@ impl<'arbitrary> arbitrary::Arbitrary<'arbitrary> for Section {
         let result = (|| {
             Ok(
                 match (u64::from(<u32 as arbitrary::Arbitrary>::arbitrary(u)?)
-                    * 7u64)
+                    * 5u64)
                     >> 32
                 {
                     0u64 => Section::Data(arbitrary::Arbitrary::arbitrary(u)?),
@@ -867,7 +867,7 @@ impl<'arbitrary> arbitrary::Arbitrary<'arbitrary> for Section {
             Ok(
                 match (u64::from(<u32 as arbitrary::Arbitrary>::arbitrary(
                     &mut u,
-                )?) * 7u64)
+                )?) * 5u64)
                     >> 32
                 {
                     0u64 => Section::Data(
