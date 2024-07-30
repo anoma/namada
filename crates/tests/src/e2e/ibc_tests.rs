ce47855e78da4e3bc69303b5e35c13f3923cb1d4
yito88 <yuji@phact-columba.com>
2024-07-30 11:24:02 +0200

    fix after rebasing

--- crates/tests/src/e2e/ibc_tests.rs
+++ crates/tests/src/e2e/ibc_tests.rs
@@ -2340,7 +2340,6 @@ fn propose_upgrade_client(test_a: &Test) -> Result<Epoch> {
     let start_epoch = (epoch.0 + 6) / 3 * 3;
     let proposal_json_path = prepare_proposal_data(
         test_a.test_dir.path(),
-        0,
         albert,
         TestWasms::TxProposalIbcClientUpgrade.read_bytes(),
         start_epoch,
@@ -2350,6 +2349,8 @@ fn propose_upgrade_client(test_a: &Test) -> Result<Epoch> {
         "init-proposal",
         "--data-path",
         proposal_json_path.to_str().unwrap(),
+        "--gas-limit",
+        "2000000",
         "--node",
         &rpc_a,
     ];
