2c848472a8db76b0a564771b96df4ecdb47b4329
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-07-31 18:24:43 +0200

    avoid load shedding tower info service

--- crates/sdk/src/tx.rs
+++ crates/sdk/src/tx.rs
@@ -3548,10 +3548,15 @@ pub async fn build_update_account(
         let threshold = *threshold;
 
         let invalid_threshold = threshold.is_zero();
-        let invalid_threshold_updated = !public_keys.is_empty() && public_keys.len() < threshold as usize;
-        let invalid_threshold_current = public_keys.is_empty() && account.get_all_public_keys().len() < threshold as usize;
+        let invalid_threshold_updated =
+            !public_keys.is_empty() && public_keys.len() < threshold as usize;
+        let invalid_threshold_current = public_keys.is_empty()
+            && account.get_all_public_keys().len() < threshold as usize;
 
-        if invalid_threshold || invalid_threshold_updated || invalid_threshold_current {
+        if invalid_threshold
+            || invalid_threshold_updated
+            || invalid_threshold_current
+        {
             edisplay_line!(
                 context.io(),
                 "Invalid account threshold: either the provided threshold is \
@@ -3566,7 +3571,8 @@ pub async fn build_update_account(
 
         Some(threshold)
     } else {
-        let invalid_too_few_pks = !public_keys.is_empty() && public_keys.len() < account.threshold as usize;
+        let invalid_too_few_pks = !public_keys.is_empty()
+            && public_keys.len() < account.threshold as usize;
 
         if invalid_too_few_pks {
             return Err(Error::from(TxSubmitError::InvalidAccountThreshold));
