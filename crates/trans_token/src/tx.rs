7d8e4bb72e25e7fd95275551ccecd566144d439a
Tomáš Zemanovič <tomas@heliax.dev>
2024-09-13 14:08:02 +0200

    trans_token: test a valid transfer tx

--- crates/trans_token/src/tx.rs
+++ crates/trans_token/src/tx.rs
@@ -5,7 +5,7 @@ use namada_events::{EmitEvents, EventLevel};
 use namada_tx_env::{Result, TxEnv};
 
 use crate::event::{TokenEvent, TokenOperation};
-use crate::{Amount, UserAccount};
+use crate::{read_balance, Amount, UserAccount};
 
 /// Transfer transparent token, insert the verifier expected by the VP and an
 /// emit an event.
@@ -39,10 +39,128 @@ where
             UserAccount::Internal(dest.clone()),
             token.clone(),
             amount.into(),
-            crate::read_balance(env, token, src)?.into(),
-            Some(crate::read_balance(env, token, dest)?.into()),
+            read_balance(env, token, src)?.into(),
+            Some(read_balance(env, token, dest)?.into()),
         ),
     });
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::BTreeMap;
+
+    use namada_core::address::testing::{
+        arb_address, arb_non_internal_address,
+    };
+    use namada_core::token;
+    use namada_core::token::testing::arb_amount;
+    use namada_core::uint::Uint;
+    use namada_events::extend::EventAttributeEntry;
+    use namada_tests::tx::{ctx, tx_host_env};
+    use proptest::prelude::*;
+
+    use super::*;
+    use crate::event::{PostBalances, SourceAccounts, TargetAccounts};
+
+    proptest! {
+        #[test]
+        fn test_valid_transfer_tx(
+            (src, dest, token) in (
+                arb_non_internal_address(),
+                arb_non_internal_address(),
+                arb_address()
+            ).prop_filter("unique addresses", |(src, dest, token)|
+                src != dest && dest != token && src != token),
+
+            amount in arb_amount(),
+        ) {
+            test_valid_transfer_tx_aux(src, dest, token, amount)
+        }
+    }
+
+    fn test_valid_transfer_tx_aux(
+        src: Address,
+        dest: Address,
+        token: Address,
+        amount: Amount,
+    ) {
+        tx_host_env::init();
+
+        tx_host_env::with(|tx_env| {
+            tx_env.spawn_accounts([&src, &dest, &token]);
+            tx_env.credit_tokens(&src, &token, amount);
+        });
+        assert_eq!(read_balance(ctx(), &token, &src).unwrap(), amount);
+
+        transfer(ctx(), &src, &dest, &token, amount).unwrap();
+
+        // Dest received the amount
+        assert_eq!(read_balance(ctx(), &token, &dest).unwrap(), amount);
+
+        // Src spent the amount
+        assert_eq!(
+            read_balance(ctx(), &token, &src).unwrap(),
+            token::Amount::zero()
+        );
+
+        tx_host_env::with(|tx_env| {
+            // Internal token address have to be part of the verifier set
+            assert!(!token.is_internal() || tx_env.verifiers.contains(&token));
+            // Src and dest should always verify
+            assert!(tx_env.verifiers.contains(&src));
+            assert!(tx_env.verifiers.contains(&dest));
+        });
+
+        // The transfer must emit an event
+        tx_host_env::with(|tx_env| {
+            let events: Vec<_> = tx_env
+                .state
+                .write_log()
+                .get_events_of::<TokenEvent>()
+                .collect();
+            assert_eq!(events.len(), 1);
+            let event = events[0].clone();
+            assert_eq!(event.level(), &EventLevel::Tx);
+            assert_eq!(event.kind(), &crate::event::types::TRANSFER);
+            let attrs = event.into_attributes();
+            let amount_uint: Uint = amount.into();
+
+            let exp_balances = if src < dest {
+                format!(
+                    "[[[\"internal-address/{src}\",\"{token}\"],\"0\"],[[\"\
+                     internal-address/{dest}\",\"{token}\"],\"{amount_uint}\"\
+                     ]]",
+                )
+            } else {
+                format!(
+                    "[[[\"internal-address/{dest}\",\"{token}\"],\"\
+                     {amount_uint}\"],[[\"internal-address/{src}\",\"{token}\"\
+                     ],\"0\"]]",
+                )
+            };
+            let exp_sources = format!(
+                "[[[\"internal-address/{src}\",\"{token}\"],\"{amount_uint}\"\
+                 ]]",
+            );
+            let exp_targets = format!(
+                "[[[\"internal-address/{dest}\",\"{token}\"],\"{amount_uint}\"\
+                 ]]",
+            );
+
+            itertools::assert_equal(
+                attrs,
+                BTreeMap::from_iter([
+                    (PostBalances::KEY.to_string(), exp_balances),
+                    (SourceAccounts::KEY.to_string(), exp_sources),
+                    (TargetAccounts::KEY.to_string(), exp_targets),
+                    (
+                        "token-event-descriptor".to_string(),
+                        "transfer-from-wasm".to_string(),
+                    ),
+                ]),
+            );
+        })
+    }
 }
