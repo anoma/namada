85566ebd5d1fc6ab1df8758d94e7b1f6013ebd23
Marco Granelli <marco@heliax.dev>
2024-10-16 11:29:10 +0200

    Proptest inner txs malleability attacks
    (cherry picked from commit 44d9e47d5d6208a911f7ed90115d9dbcb3eae47d)

---
+++ wasm_for_tests/vp_verify_signature/src/lib.rs
@@ -0,0 +1,13 @@
+use namada_vp_prelude::*;
+
+#[validity_predicate]
+fn validate_tx(
+    ctx: &Ctx,
+    tx_data: BatchedTx,
+    addr: Address,
+    _keys_changed: BTreeSet<storage::Key>,
+    _verifiers: BTreeSet<Address>,
+) -> VpResult {
+    let mut gadget = VerifySigGadget::new();
+    gadget.verify_signatures(ctx, &tx_data.tx, &addr)
+}
