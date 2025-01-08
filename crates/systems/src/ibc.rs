0d473cddf8728cc9a3201b1d2c89c7fda56ac073
yito88 <yuji@phact-columba.com>
2025-01-09 00:08:43 +0100

    WIP: fix masp vp and add test

--- crates/systems/src/ibc.rs
+++ crates/systems/src/ibc.rs
@@ -6,7 +6,7 @@ use masp_primitives::transaction::components::ValueSum;
 use masp_primitives::transaction::TransparentAddress;
 use namada_core::address::Address;
 use namada_core::borsh::BorshDeserialize;
-use namada_core::masp::TAddrData;
+use namada_core::masp::{MaspEpoch, TAddrData};
 use namada_core::{masp_primitives, storage, token};
 pub use namada_storage::Result;
 
@@ -15,6 +15,13 @@ pub trait Read<S> {
     /// Extract MASP transaction from IBC envelope
     fn try_extract_masp_tx_from_envelope<Transfer: BorshDeserialize>(
         tx_data: &[u8],
+    ) -> Result<Option<masp_primitives::transaction::Transaction>>;
+
+    /// Try to read a MASP transaction for the refund
+    fn try_get_refund_masp_tx<Transfer: BorshDeserialize>(
+        storage: &S,
+        tx_data: &[u8],
+        masp_epoch: MaspEpoch,
     ) -> Result<Option<masp_primitives::transaction::Transaction>>;
 
     /// Apply relevant IBC packets to the changed balances structure
