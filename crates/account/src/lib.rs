2c1ca7838dcc8d37039313c0e9015cb89e03cdac
crStiv <cryptostiv7@gmail.com>
2025-01-09 02:28:05 +0100

    Update lib.rs

--- crates/account/src/lib.rs
+++ crates/account/src/lib.rs
@@ -47,18 +47,33 @@ pub use types::*;
     Serialize,
     Deserialize,
 )]
-/// Account data
+/// Account data structure that holds public keys and signature threshold
+/// for multi-signature authorization.
+///
+/// # Example
+/// ```
+/// use namada_account::{Account, AccountPublicKeysMap};
+/// use namada_core::address::Address;
+///
+/// let public_keys_map = AccountPublicKeysMap::default();
+/// let account = Account {
+///     public_keys_map,
+///     threshold: 1,
+///     address: Address::decode("...").unwrap(),
+/// };
+/// ```
 pub struct Account {
     /// The map between indexes and public keys for an account
     pub public_keys_map: AccountPublicKeysMap,
-    /// The account signature threshold
+    /// The minimum number of signatures required to authorize an action
     pub threshold: u8,
-    /// The address corresponding to the account owner
+    /// The unique address corresponding to the account owner
     pub address: Address,
 }
 
 impl Account {
-    /// Retrieve a public key from the index
+    /// Retrieve a public key by its index in the account's public keys map.
+    /// Returns None if the index is not found.
     pub fn get_public_key_from_index(
         &self,
         index: u8,
@@ -66,7 +81,8 @@ impl Account {
         self.public_keys_map.get_public_key_from_index(index)
     }
 
-    /// Retrieve the index of a public key
+    /// Find the index assigned to a given public key in this account.
+    /// Returns None if the public key is not associated with this account.
     pub fn get_index_from_public_key(
         &self,
         public_key: &common::PublicKey,
@@ -74,7 +90,8 @@ impl Account {
         self.public_keys_map.get_index_from_public_key(public_key)
     }
 
-    /// Get all public keys of the account
+    /// Get a vector containing all public keys associated with this account.
+    /// The keys are returned in no particular order.
     pub fn get_all_public_keys(&self) -> Vec<common::PublicKey> {
         self.public_keys_map.pk_to_idx.keys().cloned().collect()
     }
