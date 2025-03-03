4395abebafa0a233834646aca85390e44fd90dc9
Murisi Tarusenga <murisit@gmail.com>
2025-03-03 20:42:11 +0200

    Make client try all Transaction permutations.

--- crates/shielded_token/src/masp/shielded_wallet.rs
+++ crates/shielded_token/src/masp/shielded_wallet.rs
@@ -143,12 +143,11 @@ impl<U: ShieldedUtils + MaybeSend + MaybeSync> ShieldedWallet<U> {
 
     /// Update the merkle tree of witnesses the first time we
     /// scan new MASP transactions.
-    pub(crate) async fn update_witness_map<M: MaspClient + Send + Sync + Unpin + 'static,>(
+    pub(crate) async fn update_witness_map(
         &mut self,
-        client: &M,
         indexed_tx: IndexedTx,
         shielded: &Transaction,
-    ) -> Result<bool, eyre::Error> {
+    ) -> Result<(), eyre::Error> {
         let mut note_pos = self.tree.size();
         self.note_index.insert(indexed_tx, note_pos);
 
@@ -174,14 +173,7 @@ impl<U: ShieldedUtils + MaybeSend + MaybeSync> ShieldedWallet<U> {
             self.witness_map.insert(note_pos, witness);
             note_pos = checked!(note_pos + 1).unwrap();
         }
-        let root = self.tree.root();
-        let anchor_exists = client.commitment_anchor_exists(&root).await?;
-        #[allow(clippy::print_stdout)] {
-            println!("Transaction Index: {:?}", indexed_tx);
-            println!("Commitment Anchor: {:?}", root);
-            println!("Commitment Anchor Exists: {}", anchor_exists);
-        }
-        Ok(anchor_exists)
+        Ok(())
     }
 
     /// Sync the current state of the multi-asset shielded pool in a
