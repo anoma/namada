005845de179aa07316a47c96e26882ca950024e5
Murisi Tarusenga <murisit@gmail.com>
2025-03-14 09:08:00 +0200

    Implemented a migration to set token precisions.

--- examples/make-db-migration.rs
+++ examples/make-db-migration.rs
@@ -3,14 +3,16 @@ use std::collections::BTreeMap;
 use borsh::{BorshDeserialize, BorshSerialize};
 use namada_macros::BorshDeserializer;
 use namada_sdk::address::Address;
+use namada_sdk::ibc::trace::ibc_token;
 use namada_sdk::masp_primitives::asset_type::AssetType;
 use namada_sdk::masp_primitives::merkle_tree::FrozenCommitmentTree;
 use namada_sdk::masp_primitives::sapling;
 use namada_sdk::migrations;
 use namada_sdk::storage::DbColFam;
+use namada_shielded_token::storage_key::masp_reward_precision_key;
 use namada_shielded_token::{ConversionLeaf, ConversionState};
 use namada_trans_token::storage_key::{balance_key, minted_balance_key};
-use namada_trans_token::Amount;
+use namada_trans_token::{Amount, Store};
 
 pub const OLD_CONVERSION_STATE_TYPE_HASH: &str =
     "05E2FD0BEBD54A05AAE349BBDE61F90893F09A72850EFD4F69060821EC5DE65F";
@@ -38,8 +40,8 @@ impl From<ConversionState> for NewConversionState {
     }
 }
 
-#[allow(dead_code)]
-fn example() {
+// Demonstrate how to set the minted balance using a migration
+fn minted_balance_migration() {
     let person =
         Address::decode("tnam1q9rhgyv3ydq0zu3whnftvllqnvhvhm270qxay5tn")
             .unwrap();
@@ -68,10 +70,59 @@ fn example() {
     let changes = migrations::DbChanges {
         changes: updates.into_iter().collect(),
     };
-    std::fs::write("migrations.json", serde_json::to_string(&changes).unwrap())
-        .unwrap();
+    std::fs::write(
+        "minted_balance_migration.json",
+        serde_json::to_string(&changes).unwrap(),
+    )
+    .unwrap();
 }
 
+// Demonstrate how to set the shielded reward precision of IBC tokens using a
+// migration
+fn shielded_reward_precision_migration() {
+    pub type ChannelId = &'static str;
+    pub type BaseToken = &'static str;
+    pub type Precision = u128;
+
+    const IBC_TOKENS: [(ChannelId, BaseToken, Precision); 6] = [
+        ("channel-1", "uosmo", 1000u128),
+        ("channel-2", "uatom", 1000u128),
+        ("channel-3", "utia", 1000u128),
+        ("channel-0", "stuosmo", 1000u128),
+        ("channel-0", "stuatom", 1000u128),
+        ("channel-0", "stutia", 1000u128),
+    ];
+
+    let mut updates = Vec::new();
+    // Set IBC token shielded reward precisions
+    for (channel_id, base_token, precision) in IBC_TOKENS {
+        let ibc_denom = format!("transfer/{channel_id}/{base_token}");
+        let token_address = ibc_token(&ibc_denom).clone();
+
+        // The key holding the shielded reward precision of current token
+        let shielded_token_reward_precision_key =
+            masp_reward_precision_key::<Store<()>>(&token_address);
+
+        updates.push(migrations::DbUpdateType::Add {
+            key: shielded_token_reward_precision_key,
+            cf: DbColFam::SUBSPACE,
+            value: precision.into(),
+            force: false,
+        });
+    }
+
+    let changes = migrations::DbChanges {
+        changes: updates.into_iter().collect(),
+    };
+    std::fs::write(
+        "shielded_reward_precision_migration.json",
+        serde_json::to_string(&changes).unwrap(),
+    )
+    .unwrap();
+}
+
+// Generate various migrations
 fn main() {
-    example()
+    minted_balance_migration();
+    shielded_reward_precision_migration();
 }
