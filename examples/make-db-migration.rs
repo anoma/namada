300c34c79e12cced3b3f286d459ca372875c2786
Murisi Tarusenga <murisit@gmail.com>
2025-04-17 15:18:57 +0200

    Added a migration that sets the shielded reward parameters of specified tokens.

--- examples/make-db-migration.rs
+++ examples/make-db-migration.rs
@@ -7,6 +7,7 @@ use masp_primitives::ff::PrimeField;
 use masp_primitives::sapling::Node;
 use masp_primitives::transaction::components::I128Sum;
 use namada_core::borsh::BorshSerializeExt;
+use namada_core::dec::Dec;
 use namada_core::hash::Hash;
 use namada_core::masp::{Precision, encode_asset_type};
 use namada_core::storage::Key;
@@ -21,8 +22,9 @@ use namada_sdk::masp_primitives::sapling;
 use namada_sdk::migrations;
 use namada_sdk::storage::DbColFam;
 use namada_shielded_token::storage_key::{
-    masp_assets_hash_key, masp_conversion_key, masp_reward_precision_key,
-    masp_scheduled_base_native_precision_key,
+    masp_assets_hash_key, masp_conversion_key, masp_kd_gain_key,
+    masp_kp_gain_key, masp_locked_amount_target_key, masp_max_reward_rate_key,
+    masp_reward_precision_key, masp_scheduled_base_native_precision_key,
     masp_scheduled_reward_precision_key,
 };
 use namada_shielded_token::{ConversionLeaf, ConversionState, MaspEpoch};
@@ -40,6 +42,14 @@ pub type AddressBech32m = &'static str;
 pub type WasmHash = &'static str;
 /// Represents the bytes of a WASM binary
 pub type WasmBytes = &'static [u8];
+/// Represents a maximum reward rate
+pub type MaxRewardRate = &'static str;
+/// Represents a target locked amount
+pub type TargetLockedAmount = u64;
+/// Represents a nominal proportional gain
+pub type KpGain = &'static str;
+/// Represents a nominal derivative gain
+pub type KdGain = &'static str;
 /// The type hash of the conversion state structure in v0.31.9
 pub const OLD_CONVERSION_STATE_TYPE_HASH: &str =
     "05E2FD0BEBD54A05AAE349BBDE61F90893F09A72850EFD4F69060821EC5DE65F";
@@ -99,24 +109,41 @@ pub fn minted_balance_migration(updates: &mut Vec<migrations::DbUpdateType>) {
     ));
 }
 
+/// A convenience data structure to allow token addresses to be more readably
+/// expressed as a channel ID and base token instead of a raw Namada address.
+pub enum TokenAddress {
+    // Specify an IBC address. This can also be done more directly using the
+    // Self::Address variant.
+    Ibc(ChannelId, BaseToken),
+    // Directly specify a Namada address
+    Address(AddressBech32m),
+}
+
 /// Demonstrate how to set the shielded reward precision of IBC tokens using a
 /// migration
 pub fn shielded_reward_precision_migration(
     updates: &mut Vec<migrations::DbUpdateType>,
 ) {
-    const IBC_TOKENS: [(ChannelId, BaseToken, Precision); 6] = [
-        ("channel-1", "uosmo", 1000u128),
-        ("channel-2", "uatom", 1000u128),
-        ("channel-3", "utia", 1000u128),
-        ("channel-0", "stuosmo", 1000u128),
-        ("channel-0", "stuatom", 1000u128),
-        ("channel-0", "stutia", 1000u128),
+    const TOKENS: [(TokenAddress, Precision); 6] = [
+        (TokenAddress::Ibc("channel-1", "uosmo"), 1000u128),
+        (TokenAddress::Ibc("channel-2", "uatom"), 1000u128),
+        (TokenAddress::Ibc("channel-3", "utia"), 1000u128),
+        (TokenAddress::Ibc("channel-0", "stuosmo"), 1000u128),
+        (TokenAddress::Ibc("channel-0", "stuatom"), 1000u128),
+        (TokenAddress::Ibc("channel-0", "stutia"), 1000u128),
     ];
 
     // Set IBC token shielded reward precisions
-    for (channel_id, base_token, precision) in IBC_TOKENS {
-        let ibc_denom = format!("transfer/{channel_id}/{base_token}");
-        let token_address = ibc_token(&ibc_denom).clone();
+    for (token_address, precision) in TOKENS {
+        // Compute the Namada address
+        let token_address = match token_address {
+            TokenAddress::Ibc(channel_id, base_token) => {
+                let ibc_denom = format!("transfer/{channel_id}/{base_token}");
+                ibc_token(&ibc_denom).clone()
+            }
+            TokenAddress::Address(addr) => Address::from_str(addr)
+                .expect("unable to construct token address"),
+        };
 
         // The key holding the shielded reward precision of current token
         let shielded_token_reward_precision_key =
@@ -129,16 +156,6 @@ pub fn shielded_reward_precision_migration(
             force: false,
         });
     }
-}
-
-/// A convenience data structure to allow token addresses to be more readably
-/// expressed as a channel ID and base token instead of a raw Namada address.
-pub enum TokenAddress {
-    // Specify an IBC address. This can also be done more directly using the
-    // Self::Address variant.
-    Ibc(ChannelId, BaseToken),
-    // Directly specify a Namada address
-    Address(AddressBech32m),
 }
 
 /// Demonstrate clearing MASP rewards for the given IBC tokens by overwriting
@@ -495,17 +512,15 @@ pub fn wasm_migration(updates: &mut Vec<migrations::DbUpdateType>) {
         (
             "83afcbf97c35188991ae2e73db2f48cb8d019c4295fe5323d9c3dfebcd5dbec0",
             "tx_transfer.wasm",
-            // include_bytes!("tx_transfer.
-            // 5c7e44e61c00df351fa7c497cd2e186d71909f1a18db0c8d362dff36057e0fbf.
-            // wasm"),
+            // The following bytes are just an example. Usually the following
+            // line will be: include_bytes!("<path to Wasm binary>"),
             &[0xDE, 0xAD, 0xBE, 0xEF],
         ),
         (
             "6ff3c2a2ebc65061a9b89abd15fb37851ca77e162b42b7989889bd537e802b09",
             "tx_ibc.wasm",
-            // include_bytes!("tx_ibc.
-            // ae9b900edd6437461addd1fe1c723c4b1a8ac8d2fce30e1e4c417ef34f299f73.
-            // wasm"),
+            // The following bytes are just an example. Usually the following
+            // line will be: include_bytes!("<path to Wasm binary>"),
             &[0xDE, 0xAD, 0xBE, 0xEF],
         ),
     ];
@@ -607,6 +622,118 @@ pub fn wasm_migration(updates: &mut Vec<migrations::DbUpdateType>) {
     });
 }
 
+/// Demonstrate how to set the shielded reward parameters of IBC tokens using a
+/// migration
+pub fn shielded_reward_parameters_migration(
+    updates: &mut Vec<migrations::DbUpdateType>,
+) {
+    const TOKENS: [(
+        Denomination,
+        TokenAddress,
+        MaxRewardRate,
+        TargetLockedAmount,
+        KpGain,
+        KdGain,
+    ); 6] = [
+        (
+            Denomination(0),
+            TokenAddress::Ibc("channel-1", "uosmo"),
+            "0.01",
+            1_000_000,
+            "120000",
+            "120000",
+        ),
+        (
+            Denomination(0),
+            TokenAddress::Ibc("channel-2", "uatom"),
+            "0.01",
+            1_000_000,
+            "120000",
+            "120000",
+        ),
+        (
+            Denomination(0),
+            TokenAddress::Ibc("channel-3", "utia"),
+            "0.01",
+            1_000_000,
+            "120000",
+            "120000",
+        ),
+        (
+            Denomination(0),
+            TokenAddress::Ibc("channel-0", "stuosmo"),
+            "0.01",
+            1_000_000,
+            "120000",
+            "120000",
+        ),
+        (
+            Denomination(0),
+            TokenAddress::Ibc("channel-0", "stuatom"),
+            "0.01",
+            1_000_000,
+            "120000",
+            "120000",
+        ),
+        (
+            Denomination(0),
+            TokenAddress::Ibc("channel-0", "stutia"),
+            "0.01",
+            1_000_000,
+            "120000",
+            "120000",
+        ),
+    ];
+
+    // Set IBC token shielded reward parameters
+    for (denomination, token_addr, max_reward, lock_target, kp, kd) in TOKENS {
+        // Compute the Namada address
+        let token_address = match token_addr {
+            TokenAddress::Ibc(channel_id, base_token) => {
+                let ibc_denom = format!("transfer/{channel_id}/{base_token}");
+                ibc_token(&ibc_denom).clone()
+            }
+            TokenAddress::Address(addr) => Address::from_str(addr)
+                .expect("unable to construct token address"),
+        };
+
+        // The keys holding the shielded reward parameters of current token
+        let shielded_token_max_rewards_key =
+            masp_max_reward_rate_key::<Store<()>>(&token_address);
+        let shielded_token_target_locked_amount_key =
+            masp_locked_amount_target_key::<Store<()>>(&token_address);
+        let shielded_token_kp_gain_key =
+            masp_kp_gain_key::<Store<()>>(&token_address);
+        let shielded_token_kd_gain_key =
+            masp_kd_gain_key::<Store<()>>(&token_address);
+
+        updates.push(migrations::DbUpdateType::Add {
+            key: shielded_token_max_rewards_key,
+            cf: DbColFam::SUBSPACE,
+            value: Dec::from_str(max_reward).unwrap().into(),
+            force: false,
+        });
+        updates.push(migrations::DbUpdateType::Add {
+            key: shielded_token_target_locked_amount_key,
+            cf: DbColFam::SUBSPACE,
+            value: Amount::from_uint(lock_target, denomination).unwrap().into(),
+            force: false,
+        });
+        updates.push(migrations::DbUpdateType::Add {
+            key: shielded_token_kp_gain_key,
+            cf: DbColFam::SUBSPACE,
+            value: Dec::from_str(kp).unwrap().into(),
+            force: false,
+        });
+        updates.push(migrations::DbUpdateType::Add {
+            key: shielded_token_kd_gain_key,
+            cf: DbColFam::SUBSPACE,
+            value: Dec::from_str(kd).unwrap().into(),
+            force: false,
+        });
+    }
+}
+
 /// Generate various migrations
 pub fn main() {
     // Write an example migration that updates minted balances
@@ -658,6 +785,15 @@ pub fn main() {
     std::fs::write(
         "pre_phase4_migration.json",
         serde_json::to_string(&pre_phase4_changes).unwrap(),
+    )
+    .unwrap();
+    // Write an example migration that sets shielded reward parameters
+    let mut reward_parameter_changes =
+        migrations::DbChanges { changes: vec![] };
+    shielded_reward_parameters_migration(&mut reward_parameter_changes.changes);
+    std::fs::write(
+        "reward_parameters_migration.json",
+        serde_json::to_string(&reward_parameter_changes).unwrap(),
     )
     .unwrap();
 }
