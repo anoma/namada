7399e2abea4c6903d7966e28aa45344d3f8afc82
Tiago Carvalho <sugoiuguu@tfwno.gf>
2025-04-30 13:26:08 +0100

    Refactor FMD borsh impls

--- crates/core/src/masp/fmd.rs
+++ crates/core/src/masp/fmd.rs
@@ -104,54 +104,87 @@ impl Default for FlagCiphertext {
     }
 }
 
-impl BorshSerialize for FlagCiphertext {
-    #[inline]
-    fn serialize<W: io::Write>(&self, writer: &mut W) -> io::Result<()> {
+mod borsh_impls {
+    use super::*;
+
+    macro_rules! borsh_derive_from_bincode {
+        ($($type:ty),+) => {
+            $(
+                impl BorshSerialize for $type {
+                    #[inline]
+                    fn serialize<W: io::Write>(
+                        &self,
+                        writer: &mut W,
+                    ) -> io::Result<()> {
+                        bincode_as_borsh_serialize(writer, self)
+                    }
+                }
+
+                impl BorshDeserialize for $type {
+                    #[inline]
+                    fn deserialize_reader<R: io::Read>(
+                        reader: &mut R,
+                    ) -> io::Result<Self> {
+                        bincode_as_borsh_deserialize(reader)
+                    }
+                }
+
+                impl BorshSchema for $type {
+                    fn add_definitions_recursively(
+                        definitions: &mut BTreeMap<String, Definition>,
+                    ) {
+                        let def = {
+                            <Vec<u8>>::add_definitions_recursively(definitions);
+                            definitions.get(&<Vec<u8>>::declaration()).unwrap().clone()
+                        };
+
+                        definitions.insert(Self::declaration(), def);
+                    }
+
+                    fn declaration() -> String {
+                        std::any::type_name::<Self>().into()
+                    }
+                }
+            )+
+        };
+    }
+
+    fn bincode_as_borsh_serialize<W: io::Write, T: Serialize>(
+        writer: &mut W,
+        data: &T,
+    ) -> io::Result<()> {
         // NOTE: serialize the size. borsh will only see an
         // opaque vector of bytes
-        let size: u32 = bincode::serialized_size(&self.inner)
+        let size: u32 = bincode::serialized_size(data)
             .map_err(from_bincode_err)?
             .try_into()
             .map_err(io::Error::other)?;
         writer.write_all(&size.to_le_bytes())?;
 
-        bincode::serialize_into(writer, &self.inner).map_err(from_bincode_err)
+        bincode::serialize_into(writer, data).map_err(from_bincode_err)
     }
-}
 
-impl BorshDeserialize for FlagCiphertext {
-    #[inline]
-    fn deserialize_reader<R: io::Read>(reader: &mut R) -> io::Result<Self> {
+    fn bincode_as_borsh_deserialize<
+        R: io::Read,
+        T: for<'de> Deserialize<'de>,
+    >(
+        reader: &mut R,
+    ) -> io::Result<T> {
         // NOTE: skip the length of the fake vector of bytes
         reader.read_exact(&mut [0u8; 4])?;
 
         bincode::deserialize_from(reader).map_err(from_bincode_err)
     }
-}
 
-impl BorshSchema for FlagCiphertext {
-    fn add_definitions_recursively(
-        definitions: &mut BTreeMap<String, Definition>,
-    ) {
-        let def = {
-            <Vec<u8>>::add_definitions_recursively(definitions);
-            definitions.get(&<Vec<u8>>::declaration()).unwrap().clone()
-        };
-
-        definitions.insert(Self::declaration(), def);
+    #[allow(clippy::boxed_local)]
+    fn from_bincode_err(err: bincode::Error) -> io::Error {
+        match *err {
+            bincode::ErrorKind::Io(err) => err,
+            other => io::Error::other(other),
+        }
     }
 
-    fn declaration() -> String {
-        std::any::type_name::<Self>().into()
-    }
-}
-
-#[allow(clippy::boxed_local)]
-fn from_bincode_err(err: bincode::Error) -> io::Error {
-    match *err {
-        bincode::ErrorKind::Io(err) => err,
-        other => io::Error::other(other),
-    }
+    borsh_derive_from_bincode!(FlagCiphertext);
 }
 
 #[cfg(test)]
