cc36377dfbad464c35cb9321e37b1ee10c501008
Jacob Turner <jacob.turner870@gmail.com>
2025-06-04 11:33:30 +0200

    Update crates/node/src/bench_utils.rs
    Co-authored-by: Tiago Carvalho <sugoiuguu@tfwno.gf>

--- crates/node/src/bench_utils.rs
+++ crates/node/src/bench_utils.rs
@@ -804,12 +804,14 @@ impl ShieldedUtils for BenchShieldedUtils {
                 ContextSyncStatus::Speculative => SPECULATIVE_FILE_NAME,
             }
         };
-        let Ok(mut ctx_file) =
-            File::open(self.context_dir.0.path().to_path_buf().join(file_name))
-        else {
-            // a missing file means there is nothing to load.
-            return Ok(());
-        };
+        let mut ctx_file =
+            File::open(self.context_dir.0.path().to_path_buf().join(file_name)).or_else(|err| {
+                if err.kind() == std::io::ErrorKind::NotFound {
+                    Ok(())
+                } else {
+                    Err(err)
+                }
+            })?;
         let mut bytes = Vec::new();
         ctx_file.read_to_end(&mut bytes)?;
         // Fill the supplied context with the deserialized object
